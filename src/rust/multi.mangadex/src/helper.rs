102808500fd0fdaeafa8d19a0d3bcacb2a0b753d
Skitty <mrskittyblock@gmail.com>
2022-04-14 13:34:34 -0500

    Add initial MangaDex rust source

---
+++ src/rust/multi.mangadex/src/helper.rs
@@ -0,0 +1,232 @@
+use aidoku::{
+    error::Result, std::String, std::Vec, std::ObjectRef, Manga, MangaStatus, MangaContentRating, MangaViewer, Chapter
+};
+
+// Parse manga with title and cover
+pub fn parse_basic_manga(manga_object: ObjectRef) -> Result<Manga> {
+    let attributes = manga_object.get("attributes").as_object()?;
+    let id = manga_object.get("id").as_string()?.read();
+
+    // Title
+    let titles = attributes.get("title").as_object()?;
+    let title = match titles.get("en").as_string() { // try for english title
+        Ok(title) => title.read(),
+        Err(_) => match titles.values().get(0).as_string() { // get first title instead
+            Ok(title) => title.read(),
+            Err(_) => String::new(),
+        }
+    };
+
+    // Cover
+    let mut cover_file: String = String::new();
+    
+    let relationships = manga_object.get("relationships").as_array()?;
+    for relationship in relationships {
+        let relationship_obj = relationship.as_object()?;
+        let relation_type = relationship_obj.get("type").as_string()?.read();
+        if relation_type == "cover_art" {
+            let cover_attributes = relationship_obj.get("attributes").as_object()?;
+            cover_file = cover_attributes.get("fileName").as_string()?.read();
+            break;
+        }
+    }
+
+    let mut cover = String::from("https://uploads.mangadex.org/covers/");
+    cover.push_str(&id);
+    cover.push_str("/");
+    cover.push_str(&cover_file);
+    cover.push_str(".256.jpg");
+
+    Ok(Manga {
+        id,
+        cover,
+        title,
+        author: String::new(),
+        artist: String::new(),
+        description: String::new(),
+        url: String::new(),
+        categories: Vec::new(),
+        status: MangaStatus::Unknown,
+        nsfw: MangaContentRating::Safe,
+        viewer: MangaViewer::Default,
+    })
+}
+
+// Parse complete manga info
+pub fn parse_full_manga(manga_object: ObjectRef) -> Result<Manga> {
+    let attributes = manga_object.get("attributes").as_object()?;
+    let id = manga_object.get("id").as_string()?.read();
+
+    // Title
+    let titles = attributes.get("title").as_object()?;
+    let title = match titles.get("en").as_string() { // try for english title
+        Ok(title) => title.read(),
+        Err(_) => match titles.values().get(0).as_string() { // get first title instead
+            Ok(title) => title.read(),
+            Err(_) => String::new(),
+        }
+    };
+
+    // Cover, author, artist
+    let mut cover_file: String = String::new();
+    let mut author: String = String::new();
+    let mut artist: String = String::new();
+    
+    let relationships = manga_object.get("relationships").as_array()?;
+    for relationship in relationships {
+        let relationship_obj = relationship.as_object()?;
+        let relation_type = relationship_obj.get("type").as_string()?.read();
+        if let Ok(relationship_attributes) = relationship_obj.get("attributes").as_object() {
+            if relation_type == "cover_art" {
+                cover_file = relationship_attributes.get("fileName").as_string()?.read();
+            } else if relation_type == "author" {
+                author = relationship_attributes.get("name").as_string()?.read();
+            } else if relation_type == "artist" {
+                artist = relationship_attributes.get("name").as_string()?.read();
+            }
+        }
+    }
+
+    let mut cover = String::from("https://uploads.mangadex.org/covers/");
+    cover.push_str(&id);
+    cover.push_str("/");
+    cover.push_str(&cover_file);
+    cover.push_str(".256.jpg");
+
+    // Description
+    let description = match attributes.get("description").as_object() {
+		Ok(descriptions) => match descriptions.get("en").as_string() { // try for english desc
+			Ok(desc) => desc.read(),
+			Err(_) => match descriptions.values().get(0).as_string() { // get first desc instead
+				Ok(desc) => desc.read(),
+				Err(_) => String::new(),
+			}
+		},
+		Err(_) => String::new(),
+	};
+
+    // URL
+    let mut url = String::from("https://mangadex.org/title/");
+    url.push_str(&id);
+
+    // Tags
+    let mut categories: Vec<String> = Vec::new();
+    if let Ok(tags) = attributes.get("tags").as_array() {
+		for tag in tags {
+			let tag_obj = tag.as_object()?;
+			let tag_attributes = tag_obj.get("attributes").as_object()?;
+			let names = tag_attributes.get("name").as_object()?;
+			categories.push( match names.get("en").as_string() {
+				Ok(name) => name.read(),
+				Err(_) => match names.values().get(0).as_string() {
+					Ok(name) => name.read(),
+					Err(_) => String::new(),
+				}
+			});
+		}
+	}
+
+    // Status
+    let status_string = attributes.get("status").as_string()?.read();
+    let status = match status_string.as_str() {
+        "ongoing" => MangaStatus::Ongoing,
+        "completed" => MangaStatus::Completed,
+        "hiatus" => MangaStatus::Hiatus,
+        "cancelled" => MangaStatus::Cancelled,
+        _ => MangaStatus::Unknown,
+    };
+
+    // Content rating
+    let nsfw = match attributes.get("contentRating").as_string() {
+		Ok(string) => match string.read().as_str() {
+			"suggestive" => MangaContentRating::Suggestive,
+			"erotica" => MangaContentRating::Nsfw,
+			"pornographic" => MangaContentRating::Nsfw,
+			_ => MangaContentRating::Safe,
+		},
+		Err(_) => MangaContentRating::Safe,
+	};
+
+    // Viewer
+    let viewer = match attributes.get("originalLanguage").as_string() {
+		Ok(string) => match string.read().as_str() {
+			"ja" => MangaViewer::Rtl,
+			"zh" => MangaViewer::Scroll,
+			"ko" => MangaViewer::Scroll,
+			_ => MangaViewer::Rtl,
+		},
+		Err(_) => MangaViewer::Rtl,
+    };
+
+    Ok(Manga {
+        id,
+        cover,
+        title,
+        author,
+        artist,
+        description,
+        url,
+        categories,
+        status,
+        nsfw,
+        viewer,
+    })
+}
+
+// Parse chapter info
+pub fn parse_chapter(chapter_object: ObjectRef) -> Result<Chapter> {
+	let attributes = chapter_object.get("attributes").as_object()?;
+
+	let pages = attributes.get("pages").as_int()?;
+
+	if pages <= 0 { 
+		return Err(aidoku::error::AidokuError { reason: aidoku::error::AidokuErrorKind::Unimplemented });
+	}
+
+	let id = chapter_object.get("id").as_string()?.read();
+	let title = attributes.get("title").as_string()?.read();
+
+	let volume = match attributes.get("volume").as_float() {
+		Ok(volume) => volume as f32,
+		Err(_) => -1.0,
+	};
+	
+	let chapter = match attributes.get("chapter").as_float() {
+		Ok(chapter) => chapter as f32,
+		Err(_) => -1.0,
+	};
+
+	let date_updated = match attributes.get("publishAt").as_date("yyyy-MM-dd'T'HH:mm:ss+ss:ss") {
+		Ok(date) => date,
+		Err(_) => -1.0,
+	};
+
+	let mut scanlator = String::new();
+
+	let relationships = chapter_object.get("relationships").as_array()?;
+	for relationship in relationships {
+		let relationship_object = relationship.as_object()?;
+		let relationship_type = relationship_object.get("type").as_string()?.read();
+		if relationship_type == "scanlation_group" {
+			let relationship_attributes = relationship_object.get("attributes").as_object()?;
+			scanlator = relationship_attributes.get("name").as_string()?.read();
+			break;
+		}
+	}
+
+	let mut url = String::from("https://mangadex.org/chapter/");
+	url.push_str(&id);
+
+	let lang = attributes.get("translatedLanguage").as_string()?.read();
+
+	Ok(Chapter {
+		id,
+		title,
+		volume,
+		chapter,
+		date_updated,
+		scanlator,
+		url,
+		lang,
+	})
+}
---
+++ src/rust/multi.mangadex/src/lib.rs
@@ -0,0 +1,341 @@
+#![no_std]
+use aidoku::{
+    prelude::*, error::Result, std::String, std::Vec, std::ObjectRef, std::net::Request, std::net::HttpMethod,
+    Filter, FilterType, Listing, Manga, MangaPageResult, Chapter, Page,
+};
+
+mod helper;
+
+#[link(wasm_import_module = "env")]
+extern "C" {
+    fn print(message: *const u8, len: usize);
+}
+
+fn println(message: &str) {
+    unsafe {
+        print(message.as_ptr(), message.len());
+    }
+}
+
+fn urlencode(string: String) -> String {
+    let mut result: Vec<u8> = Vec::with_capacity(string.len() * 3);
+    let hex = "0123456789abcdef".as_bytes();
+    let bytes = string.as_bytes();
+    
+    for byte in bytes {
+        let curr = *byte;
+        if (b'a' <= curr && curr <= b'z')
+            || (b'A' <= curr && curr <= b'Z')
+            || (b'0' <= curr && curr <= b'9') {
+                result.push(curr);
+        } else {
+            result.push(b'%');
+            result.push(hex[curr as usize >> 4]);
+            result.push(hex[curr as usize & 15]);
+        }
+    }
+
+    String::from_utf8(result).unwrap_or(String::new())
+}
+
+fn i32_to_string(mut integer: i32) -> String {
+    if integer == 0 {
+        return String::from("0");
+    }
+    let mut string = String::with_capacity(11);
+    let pos = if integer < 0 {
+        string.insert(0, '-');
+        1
+    } else {
+        0
+    };
+    while integer != 0 {
+        let mut digit = integer % 10;
+        if pos == 1 {
+            digit *= -1;
+        }
+        string.insert(pos, char::from_u32((digit as u32) + ('0' as u32)).unwrap());
+        integer /= 10;
+    }
+    return string;
+}
+
+#[get_manga_list]
+fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
+    let offset = (page - 1) * 20;
+    let mut url = String::from("https://api.mangadex.org/manga/?includes[]=cover_art&limit=20&offset=");
+    url.push_str(&i32_to_string(offset));
+
+    for filter in filters {
+        match filter.kind {
+            FilterType::Title => {
+                url.push_str("&title=");
+                let encoded = urlencode(filter.value.as_string()?.read());
+                url.push_str(&encoded);
+            },
+            FilterType::Author => {
+                url.push_str("&author=");
+                let encoded = urlencode(filter.value.as_string()?.read());
+                url.push_str(&encoded);
+            },
+            FilterType::Check => {
+                if filter.value.as_int().unwrap_or(-1) <= 0 {
+                    continue;
+                }
+                match filter.name.as_str() {
+                    // Original Language
+                    "Japanese (Manga)" => url.push_str("&originalLanguage[]=ja"),
+                    "Chinese (Manhua)" => url.push_str("&originalLanguage[]=zh"),
+                    "Korean (Manhwa)" => url.push_str("&originalLanguage[]=ko"),
+                    // Demographic
+                    "None" => url.push_str("&publicationDemographic[]=none"),
+                    "Shounen" => url.push_str("&publicationDemographic[]=shounen"),
+                    "Shoujo" => url.push_str("&publicationDemographic[]=shoujo"),
+                    "Seinen" => url.push_str("&publicationDemographic[]=seinen"),
+                    "Josei" => url.push_str("&publicationDemographic[]=josei"),
+                    // Content Rating
+                    "Safe" => url.push_str("&contentRating[]=safe"),
+                    "Suggestive" => url.push_str("&contentRating[]=suggestive"),
+                    "Erotica" => url.push_str("&contentRating[]=erotica"),
+                    "Pornographic" => url.push_str("&contentRating[]=pornographic"),
+                    // Status
+                    "Ongoing" => url.push_str("&status[]=ongoing"),
+                    "Completed" => url.push_str("&status[]=completed"),
+                    "Hiatus" => url.push_str("&status[]=hiatus"),
+                    "Cancelled" => url.push_str("&status[]=cancelled"),
+                    _ => continue,
+                }
+            },
+            FilterType::Genre => {
+                // https://api.mangadex.org/manga/tag
+                let tag = match filter.name.as_str() {
+                    "Action" => "391b0423-d847-456f-aff0-8b0cfc03066b",
+                    "Adaptation" => "f4122d1c-3b44-44d0-9936-ff7502c39ad3",
+                    "Adventure" => "87cc87cd-a395-47af-b27a-93258283bbc6",
+                    "Aliens" => "e64f6742-c834-471d-8d72-dd51fc02b835",
+                    "Animals" => "3de8c75d-8ee3-48ff-98ee-e20a65c86451",
+                    "Anthology" => "51d83883-4103-437c-b4b1-731cb73d786c",
+                    "Award Winning" => "0a39b5a1-b235-4886-a747-1d05d216532d",
+                    "Boy's Love" => "5920b825-4181-4a17-beeb-9918b0ff7a30",
+                    "Comedy" => "4d32cc48-9f00-4cca-9b5a-a839f0764984",
+                    "Cooking" => "ea2bc92d-1c26-4930-9b7c-d5c0dc1b6869",
+                    "Crime" => "5ca48985-9a9d-4bd8-be29-80dc0303db72",
+                    "Crossdressing" => "9ab53f92-3eed-4e9b-903a-917c86035ee3",
+                    "Delinquents" => "da2d50ca-3018-4cc0-ac7a-6b7d472a29ea",
+                    "Demons" => "39730448-9a5f-48a2-85b0-a70db87b1233",
+                    "Doujinshi" => "b13b2a48-c720-44a9-9c77-39c9979373fb",
+                    "Drama" => "b9af3a63-f058-46de-a9a0-e0c13906197a",
+                    "Fan Colored" => "7b2ce280-79ef-4c09-9b58-12b7c23a9b78",
+                    "Fantasy" => "cdc58593-87dd-415e-bbc0-2ec27bf404cc",
+                    "4-Koma" => "b11fda93-8f1d-4bef-b2ed-8803d3733170",
+                    "Full Color" => "f5ba408b-0e7a-484d-8d49-4e9125ac96de",
+                    "Genderswap" => "2bd2e8d0-f146-434a-9b51-fc9ff2c5fe6a",
+                    "Ghosts" => "3bb26d85-09d5-4d2e-880c-c34b974339e9",
+                    "Girl's Love" => "a3c67850-4684-404e-9b7f-c69850ee5da6",
+                    "Gore" => "b29d6a3d-1569-4e7a-8caf-7557bc92cd5d",
+                    "Gyaru" => "fad12b5e-68ba-460e-b933-9ae8318f5b65",
+                    "Harem" => "aafb99c1-7f60-43fa-b75f-fc9502ce29c7",
+                    "Historical" => "33771934-028e-4cb3-8744-691e866a923e",
+                    "Horror" => "cdad7e68-1419-41dd-bdce-27753074a640",
+                    "Incest" => "5bd0e105-4481-44ca-b6e7-7544da56b1a3",
+                    "Isekai" => "ace04997-f6bd-436e-b261-779182193d3d",
+                    "Loli" => "2d1f5d56-a1e5-4d0d-a961-2193588b08ec",
+                    "Long Strip" => "3e2b8dae-350e-4ab8-a8ce-016e844b9f0d",
+                    "Mafia" => "85daba54-a71c-4554-8a28-9901a8b0afad",
+                    "Magic" => "a1f53773-c69a-4ce5-8cab-fffcd90b1565",
+                    "Magical Girls" => "81c836c9-914a-4eca-981a-560dad663e73",
+                    "Martial Arts" => "799c202e-7daa-44eb-9cf7-8a3c0441531e",
+                    "Mecha" => "50880a9d-5440-4732-9afb-8f457127e836",
+                    "Medical" => "c8cbe35b-1b2b-4a3f-9c37-db84c4514856",
+                    "Military" => "ac72833b-c4e9-4878-b9db-6c8a4a99444a",
+                    "Monster Girls" => "dd1f77c5-dea9-4e2b-97ae-224af09caf99",
+                    "Monsters" => "36fd93ea-e8b8-445e-b836-358f02b3d33d",
+                    "Music" => "f42fbf9e-188a-447b-9fdc-f19dc1e4d685",
+                    "Mystery" => "ee968100-4191-4968-93d3-f82d72be7e46",
+                    "Ninja" => "489dd859-9b61-4c37-af75-5b18e88daafc",
+                    "Office Workers" => "92d6d951-ca5e-429c-ac78-451071cbf064",
+                    "Official Colored" => "320831a8-4026-470b-94f6-8353740e6f04",
+                    "Oneshot" => "0234a31e-a729-4e28-9d6a-3f87c4966b9e",
+                    "Philosophical" => "b1e97889-25b4-4258-b28b-cd7f4d28ea9b",
+                    "Police" => "df33b754-73a3-4c54-80e6-1a74a8058539",
+                    "Post-Apocalyptic" => "9467335a-1b83-4497-9231-765337a00b96",
+                    "Psychological" => "3b60b75c-a2d7-4860-ab56-05f391bb889c",
+                    "Reincarnation" => "0bc90acb-ccc1-44ca-a34a-b9f3a73259d0",
+                    "Reverse Harem" => "65761a2a-415e-47f3-bef2-a9dababba7a6",
+                    "Romance" => "423e2eae-a7a2-4a8b-ac03-a8351462d71d",
+                    "Samurai" => "81183756-1453-4c81-aa9e-f6e1b63be016",
+                    "School Life" => "caaa44eb-cd40-4177-b930-79d3ef2afe87",
+                    "Sci-Fi" => "256c8bd9-4904-4360-bf4f-508a76d67183",
+                    "Sexual Violence" => "97893a4c-12af-4dac-b6be-0dffb353568e",
+                    "Shota" => "ddefd648-5140-4e5f-ba18-4eca4071d19b",
+                    "Slice of Life" => "e5301a23-ebd9-49dd-a0cb-2add944c7fe9",
+                    "Sports" => "69964a64-2f90-4d33-beeb-f3ed2875eb4c",
+                    "Superhero" => "7064a261-a137-4d3a-8848-2d385de3a99c",
+                    "Supernatural" => "eabc5b4c-6aff-42f3-b657-3e90cbd00b75",
+                    "Survival" => "5fff9cde-849c-4d78-aab0-0d52b2ee1d25",
+                    "Thriller" => "07251805-a27e-4d59-b488-f0bfbec15168",
+                    "Time Travel" => "292e862b-2d17-4062-90a2-0356caa4ae27",
+                    "Tragedy" => "f8f62932-27da-4fe4-8ee1-6779a8c5edba",
+                    "Traditional Games" => "31932a7e-5b8e-49a6-9f12-2afa39dc544c",
+                    "User Created" => "891cf039-b895-47f0-9229-bef4c96eccd4",
+                    "Vampires" => "d7d1730f-6eb0-4ba6-9437-602cac38664c",
+                    "Video Games" => "9438db5a-7e2a-4ac0-b39e-e0d95a34b8a8",
+                    "Villainess" => "d14322ac-4d6f-4e9b-afd9-629d5f4d8a41",
+                    "Virtual Reality" => "8c86611e-fab7-4986-9dec-d1a2f44acdd5",
+                    "Web Comic" => "e197df38-d0e7-43b5-9b09-2842d0c326dd",
+                    "Wuxia" => "acc803a4-c95a-4c22-86fc-eb6b582d82a2",
+                    "Zombies" => "631ef465-9aba-4afb-b0fc-ea10efe274a8",
+                    _ => continue,
+                };
+                match filter.value.as_int().unwrap_or(-1) {
+                    0 => url.push_str("&excludedTags[]="),
+                    1 => url.push_str("&includedTags[]="),
+                    _ => continue,
+                }
+                url.push_str(tag);
+            },
+            FilterType::Sort => {
+                let value = match filter.value.as_object() {
+                    Ok(value) => value,
+                    Err(_) => continue,
+                };
+                let index = value.get("index").as_int().unwrap_or(0);
+                let ascending = value.get("ascending").as_bool().unwrap_or(false);
+                let option = match index {
+                    0 => "latestUploadedChapter",
+                    1 => "relevance",
+                    2 => "followedCount",
+                    3 => "createdAt",
+                    4 => "updatedAt",
+                    5 => "title",
+                    _ => continue,
+                };
+                url.push_str("&order[");
+                url.push_str(&option);
+                url.push_str("]=");
+                url.push_str(&if ascending { "asc" } else { "desc" });
+            },
+            _ => continue,
+        }
+    }
+
+    let json = Request::new(&url, HttpMethod::Get).json().as_object()?;
+    println(&url);
+
+    let data = json.get("data").as_array()?;
+
+    let mut manga_arr: Vec<Manga> = Vec::new();
+
+    for manga in data {
+        let manga_obj = manga.as_object()?;
+        if let Ok(manga) = helper::parse_basic_manga(manga_obj) {
+            manga_arr.push(manga);
+        }
+    }
+
+    let total = json.get("total").as_int()? as i32;
+
+    Ok(MangaPageResult {
+        manga: manga_arr,
+        has_more: offset + 20 < total,
+    })
+}
+
+#[get_manga_listing]
+fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
+    let mut filters: Vec<Filter> = Vec::new();
+
+    let mut selection = ObjectRef::new();
+
+    if listing.name == "Popular" {
+        selection.set("index", 2i32.into());
+        selection.set("ascending", false.into());
+        filters.push(Filter {
+            kind: FilterType::Sort,
+            name: String::from("Sort"),
+            value: selection.0,
+        });
+    } else if listing.name == "Latest" {
+        selection.set("index", 0i32.into());
+        selection.set("ascending", false.into());
+        filters.push(Filter {
+            kind: FilterType::Sort,
+            name: String::from("Sort"),
+            value: selection.0,
+        });
+    }
+
+    let result = get_manga_list(filters, page);
+
+    result
+}
+
+#[get_manga_details]
+fn get_manga_details(id: String) -> Result<Manga> {
+    let mut url = String::from("https://api.mangadex.org/manga/");
+    url.push_str(&id);
+    url.push_str("/?includes[]=cover_art&includes[]=author&includes[]=artist");
+    let json = Request::new(&url, HttpMethod::Get).json().as_object()?;
+
+    let data = json.get("data").as_object()?;
+    
+    helper::parse_full_manga(data)
+}
+
+#[get_chapter_list]
+fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
+    let mut url = String::from("https://api.mangadex.org/manga/");
+    url.push_str(&id);
+    url.push_str("/feed/?order[volume]=desc&order[chapter]=desc&limit=500&contentRating[]=pornographic&contentRating[]=erotica&contentRating[]=suggestive&contentRating[]=safe&includes[]=scanlation_group&translatedLanguage[]=en");
+    let json = Request::new(&url, HttpMethod::Get).json().as_object()?;
+
+    let data = json.get("data").as_array()?;
+
+    let mut chapters: Vec<Chapter> = Vec::new();
+
+    for chapter in data {
+        let chapter_obj = chapter.as_object()?;
+        if let Ok(chapter) = helper::parse_chapter(chapter_obj) {
+            chapters.push(chapter);
+        }
+    }
+
+    Ok(chapters)
+}
+
+#[get_page_list]
+fn get_page_list(id: String) -> Result<Vec<Page>> {
+    let mut url = String::from("https://api.mangadex.org/at-home/server/");
+    url.push_str(&id);
+    let json = Request::new(&url, HttpMethod::Get).json().as_object()?;
+
+    let chapter = json.get("chapter").as_object()?;
+    let data = chapter.get("data").as_array()?;
+
+    let base_url = json.get("baseUrl").as_string()?.read();
+    let hash = chapter.get("hash").as_string()?.read();
+
+    let mut pages: Vec<Page> = Vec::new();
+
+    let mut i = 0;
+    for page in data {
+        let data_string = page.as_string()?.read();
+        let mut url = String::new();
+        url.push_str(&base_url);
+        url.push_str("/data/");
+        url.push_str(&hash);
+        url.push_str("/");
+        url.push_str(&data_string);
+
+        pages.push(Page {
+            index: i,
+            url,
+            base64: String::new(),
+            text: String::new(),
+        });
+
+        i += 1;
+    }
+
+    Ok(pages)
+}
