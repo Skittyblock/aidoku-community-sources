ea22ab0da0a0ea29d471a17974fd143376a7ef28
pandeynmn <namanpandey23@gmail.com>
2022-05-14 21:43:46 -0700

    Mangapill: removed print statement

--- src/rust/en.mangapill/src/lib.rs
+++ src/rust/en.mangapill/src/lib.rs
@@ -13,7 +13,6 @@ fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
 
     let mut url = String::new();
     parser::get_filtered_url(filters, page, &mut url);
-    println!("{}", url.as_str());
     let html = Request::new(url.as_str(), HttpMethod::Get).html();
 
     if url.contains("search") {
