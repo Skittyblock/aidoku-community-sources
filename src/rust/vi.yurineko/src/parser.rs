c8dcb298b60f7e8ee7f920c981f65846fdfa6529
beerpiss <lacvtg.a1.2023@gmail.com>
2022-05-16 14:51:44 +0700

    feat: add description parsed from html

--- src/rust/vi.yurineko/src/parser.rs
+++ src/rust/vi.yurineko/src/parser.rs
@@ -1,5 +1,5 @@
 use aidoku::{
-	error::Result, std::String, std::Vec, std::ObjectRef,
+	error::Result, std::{String, html::Node}, std::Vec, std::ObjectRef,
 	Manga, MangaContentRating, MangaViewer, Chapter,
 };
 
@@ -9,50 +9,56 @@ pub fn parse_manga(manga_object: ObjectRef) -> Result<Manga> {
     let id = manga_object.get("id").as_int().unwrap_or(-1);
     let title = manga_object.get("originalName").as_string()?.read();
     let cover = manga_object.get("thumbnail").as_string()?.read();
+
     let authors = manga_object.get("author").as_array()?;
-    let author_string = authors.map(|author| {
+    let author = authors.map(|author| {
         let author_object = author.as_object()?;
         return Ok(author_object.get("name").as_string()?.read());
     })
         .map(|a: Result<String>| a.unwrap_or(String::from("")))
         .collect::<Vec<String>>()
         .join(", ");
+
     let mut url = String::from("https://yurineko.net/manga/");
     url.push_str(&i32_to_string(id as i32));
 
+    let description_html = manga_object.get("description").as_string()?.read();
+    let description_node = Node::new_fragment(description_html.as_bytes());
+    let description = description_node.text().0.as_string()?.read();
+
     let tags = manga_object.get("tag").as_array()?;
-    let tags_array = tags.map(|tag| {
+    let categories = tags.map(|tag| {
         let tag_object = tag.as_object()?;
         Ok(tag_object.get("name").as_string()?.read())
     })
         .map(|a: Result<String>| a.unwrap_or(String::from("")))
         .collect::<Vec<String>>();
 
-    let mut content_rating: MangaContentRating = MangaContentRating::Safe;
-    let mut view_direction: MangaViewer = MangaViewer::Rtl;
-    for tag in &tags_array {
+    let mut nsfw: MangaContentRating = MangaContentRating::Safe;
+    let mut viewer: MangaViewer = MangaViewer::Rtl;
+    for tag in &categories {
         if tag.contains("sex") || tag.contains("NSFW") || tag.contains("R18") {
-            content_rating = MangaContentRating::Nsfw;
+            nsfw = MangaContentRating::Nsfw;
         }
         else if tag.contains("Ecchi") {
-            content_rating = MangaContentRating::Suggestive;
+            nsfw = MangaContentRating::Suggestive;
         }
         if tag.contains(">") || tag.contains("Manhua") || tag.contains("Manhwa") {
-            view_direction = MangaViewer::Ltr;
+            viewer = MangaViewer::Ltr;
         }
     }
     Ok(Manga {
         id: i32_to_string(id as i32),
         cover,
         title,
-        author: author_string,
+        author,
         artist: String::new(),
-        description: String::new(), // todo: parse html,
+        description,
         url,
-        categories: tags_array,
+        categories,
         status: status_map(manga_object.get("status").as_int().unwrap_or(-1)),
-        nsfw: content_rating,
-		viewer: view_direction,
+        nsfw,
+		viewer,
     })
 }
 
