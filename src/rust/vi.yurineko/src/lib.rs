6ee993ea8638c1a58086b7e009321ef1aca9f126
beerpiss <lacvtg.a1.2023@gmail.com>
2022-05-16 16:31:26 +0700

    replace .push_str() with format!()

--- src/rust/vi.yurineko/src/lib.rs
+++ src/rust/vi.yurineko/src/lib.rs
@@ -6,7 +6,7 @@ use aidoku::{
 	std::net::Request, std::net::HttpMethod, Filter, FilterType, Listing, Manga, 
 	MangaPageResult, Chapter, Page, DeepLink,
 };
-use helper::{get_tag_id, get_search_url, urlencode, listing_map, i32_to_string};
+use helper::{get_tag_id, get_search_url, urlencode, listing_map};
 use parser::{parse_manga, parse_chapter};
 
 #[get_manga_list]
@@ -48,10 +48,7 @@ pub fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult
 
 #[get_manga_listing]
 pub fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
-	let mut url = String::from("https://api.yurineko.net/");
-	url.push_str(&listing_map(listing.name));
-	url.push_str("?page=");
-	url.push_str(&i32_to_string(page));
+	let url = format!("https://api.yurineko.net/{}?page={}", listing_map(listing.name), page);
 
 	let result = Request::new(url.as_str(), HttpMethod::Get).json().as_array()?;
 	let mut manga_arr: Vec<Manga> = Vec::new();
@@ -70,16 +67,14 @@ pub fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult>
 
 #[get_manga_details]
 fn get_manga_details(id: String) -> Result<Manga> {
-	let mut url = String::from("https://api.yurineko.net/manga/");
-	url.push_str(id.as_str());
+	let url = format!("https://api.yurineko.net/manga/{}", id);
 	let json = Request::new(url.as_str(), HttpMethod::Get).json().as_object()?;
 	parse_manga(json)
 }
 
 #[get_chapter_list]
 fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
-	let mut url = String::from("https://api.yurineko.net/manga/");
-	url.push_str(id.as_str());
+	let url = format!("https://api.yurineko.net/manga/{}", id);
 
 	let json = Request::new(url.as_str(), HttpMethod::Get).json().as_object()?;
 	let chapters = json.get("chapters").as_array()?;
@@ -104,8 +99,7 @@ fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
 
 #[get_page_list]
 fn get_page_list(id: String) -> Result<Vec<Page>> {
-	let mut url = String::from("https://api.yurineko.net/read/");
-	url.push_str(id.as_str());
+	let url = format!("https://api.yurineko.net/read/{}", id);
 
 	let json = Request::new(url.as_str(), HttpMethod::Get).json().as_object()?;
 	let pages = json.get("url").as_array()?;
@@ -141,8 +135,7 @@ pub fn handle_url(url: String) -> Result<DeepLink> {
 		let manga_id = &id[..end];
 		let manga = get_manga_details(String::from(manga_id))?;
 
-		let mut api_url = String::from("https://api.yurineko.net/read/");
-		api_url.push_str(id);
+		let api_url = format!("https://api.yurineko.net/read/{}", id);
 		let json = Request::new(api_url.as_str(), HttpMethod::Get).json().as_object()?;
 		let chapter_info = json.get("chapterInfo").as_object()?;
 		let chapter = parse_chapter(String::from(""), chapter_info)?;
--- src/rust/vi.yurineko/src/parser.rs
+++ src/rust/vi.yurineko/src/parser.rs
@@ -1,6 +1,6 @@
 use aidoku::{
 	error::Result, std::{String, html::Node}, std::Vec, std::ObjectRef,
-	Manga, MangaContentRating, MangaViewer, Chapter,
+	Manga, MangaContentRating, MangaViewer, Chapter, prelude::format
 };
 
 use crate::helper::{status_map, i32_to_string, extract_f32_from_string};
@@ -9,6 +9,7 @@ pub fn parse_manga(manga_object: ObjectRef) -> Result<Manga> {
     let id = manga_object.get("id").as_int().unwrap_or(-1);
     let title = manga_object.get("originalName").as_string()?.read();
     let cover = manga_object.get("thumbnail").as_string()?.read();
+    let url = format!("https://yurineko.net/manga/{}", id);
 
     let authors = manga_object.get("author").as_array()?;
     let author = authors.map(|author| {
@@ -18,9 +19,6 @@ pub fn parse_manga(manga_object: ObjectRef) -> Result<Manga> {
         .map(|a: Result<String>| a.unwrap_or(String::from("")))
         .collect::<Vec<String>>()
         .join(", ");
-
-    let mut url = String::from("https://yurineko.net/manga/");
-    url.push_str(&i32_to_string(id as i32));
 
     let description_html = manga_object.get("description").as_string()?.read();
     let description_node = Node::new_fragment(description_html.as_bytes());
@@ -65,18 +63,13 @@ pub fn parse_manga(manga_object: ObjectRef) -> Result<Manga> {
 pub fn parse_chapter(scanlator: String, chapter_object: ObjectRef) -> Result<Chapter> {
     let id = chapter_object.get("id").as_int().unwrap_or(-1);
     let manga_id = chapter_object.get("mangaID").as_int().unwrap_or(-1);
+    let chapter_id = format!("{}/{}", manga_id, id);
+    let url = format!("https://yurineko.net/read/{}", chapter_id);
     let title = chapter_object.get("name").as_string()?.read();
-    let date_string = chapter_object.get("date").as_string()?;
     let chapter_number = extract_f32_from_string(String::from("-"), String::from(&title));
 
+    let date_string = chapter_object.get("date").as_string()?;
     let date_object = date_string.0.as_date("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Some("en_US"), Some("UTC")).unwrap_or(-1.0);
-
-    let mut url = String::from("https://yurineko.net/read/");
-    let mut chapter_id = String::new();
-    chapter_id.push_str(&i32_to_string(manga_id as i32));
-    chapter_id.push_str("/");
-    chapter_id.push_str(&i32_to_string(id as i32));
-    url.push_str(chapter_id.as_str());
 
     Ok(Chapter {
         id: chapter_id,
