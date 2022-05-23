465f99a58ccfffe4059145640998097210d3e4b5
beerpiss <lacvtg.a1.2023@gmail.com>
2022-05-24 06:08:06 +0700

    fix(extract_f32): deal with cases like Vol.1 and Ch.1

--- src/rust/vi.blogtruyen/src/helper.rs
+++ src/rust/vi.blogtruyen/src/helper.rs
@@ -4,16 +4,22 @@ use aidoku::{
 	MangaContentRating, MangaStatus, MangaViewer,
 };
 pub fn extract_f32_from_string(title: String, text: String) -> Vec<f32> {
+	let mut last_char_was_digit: bool = false;
 	text.replace(&title, "")
-		.replace("Ch.", "") // Really dumb special case
 		.chars()
-		.filter(|a| (*a >= '0' && *a <= '9') || *a == ' ' || *a == '.' || *a == '+')
+		.filter(|a| {
+			if (*a).is_ascii_digit() {
+				last_char_was_digit = true;
+				return true;
+			} else if *a == '.' && last_char_was_digit || *a == '+' || *a == ' ' {
+				last_char_was_digit = false;
+				return true;
+			}
+			false
+		})
 		.collect::<String>()
 		.split(' ')
-		.collect::<Vec<&str>>()
-		.into_iter()
-		.map(|a| a.parse::<f32>().unwrap_or(-1.0))
-		.filter(|a| *a >= 0.0)
+		.filter_map(|a| a.parse::<f32>().ok())
 		.collect::<Vec<f32>>()
 }
 
