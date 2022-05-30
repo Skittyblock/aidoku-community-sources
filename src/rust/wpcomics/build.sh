c9e63be43265a503efd73aef0e2f1ba075f82f7e
beerpiss <lacvtg.a1.2023@gmail.com>
2022-05-30 21:19:58 +0700

    fix: copy default filter template

--- src/rust/wpcomics/build.sh
+++ src/rust/wpcomics/build.sh
@@ -7,6 +7,7 @@ if [ "$1" != "-a" ] && [ "$1" != "" ]; then
 	
 	echo "packaging $1";
 	mkdir -p target/wasm32-unknown-unknown/release/Payload
+	cp res/* target/wasm32-unknown-unknown/release/Payload
 	cp sources/$1/res/* target/wasm32-unknown-unknown/release/Payload
 	cd target/wasm32-unknown-unknown/release
 	cp $1.wasm Payload/main.wasm
@@ -24,6 +25,7 @@ else
 		echo "packaging $dir";
 
 		mkdir -p target/wasm32-unknown-unknown/release/Payload
+		cp res/* target/wasm32-unknown-unknown/release/Payload
 		cp sources/$dir/res/* target/wasm32-unknown-unknown/release/Payload
 		cd target/wasm32-unknown-unknown/release
 		cp $dir.wasm Payload/main.wasm
