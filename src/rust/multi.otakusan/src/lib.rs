96a4ed2ce35fd590f2a94157d5d3330a7f84012f
beerpiss <lacvtg.a1.2023@gmail.com>
2022-06-08 11:25:29 +0700

    feat: add Wallpaper and Cosplay listings

--- src/rust/multi.otakusan/src/lib.rs
+++ src/rust/multi.otakusan/src/lib.rs
@@ -10,7 +10,7 @@ use aidoku::{
 		html::Node,
 		json,
 		net::{HttpMethod, Request},
-		String, Vec,
+		String, Vec
 	},
 	Chapter, DeepLink, Filter, FilterType, Listing, Manga, MangaContentRating, MangaPageResult,
 	MangaStatus, MangaViewer, Page,
@@ -100,227 +100,374 @@ fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
 
 #[get_manga_listing]
 fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
+	let wallpaper_url = format!(
+		"https://otakusan.net/WallPaper/Newest?type=Newest&offset={}",
+		(page - 1) * 18
+	);
+	let cosplay_url = format!(
+		"https://otakusan.net/Cosplay/Newest?type=Newest&offset={}",
+		(page - 1) * 18
+	);
 	let url = match listing.name.as_str() {
 		"Completed" => "https://otakusan.net/Manga/CompletedNewest",
 		"New Titles" => "https://otakusan.net/Manga/NewTitleNewest",
 		"For Boys" => "https://otakusan.net/Manga/ForBoyNewest",
 		"For Girls" => "https://otakusan.net/Manga/ForGirlNewest",
 		"Ecchi Land" => "https://otakusan.net/Manga/EcchiNewest",
+		"Wallpaper" => wallpaper_url.as_str(),
+		"Cosplay" => cosplay_url.as_str(),
 		_ => {
 			return Err(AidokuError {
 				reason: AidokuErrorKind::Unimplemented,
 			})
 		}
 	};
-	let request = format!(
-		"Lang={}&Page={page}&Type=Include&Dir=NewPostedDate",
-		get_lang_code()
-	);
-	let resp = &Request::new(url, HttpMethod::Post)
-		.body(request.as_bytes())
-		.header(
-			"Content-Type",
-			"application/x-www-form-urlencoded; charset=UTF-8",
-		)
-		.header("X-Requested-With", "XMLHttpRequest")
-		.header("Referer", "https://otakusan.net/")
-		.header("Origin", "https://otakusan.net")
-		.html();
-	let (manga, has_more) = parse_manga_list!(resp.select("div.mdl-card").array());
-	Ok(MangaPageResult { manga, has_more })
+	match listing.name.as_str() {
+		"Completed" | "New Titles" | "For Boys" | "For Girls" | "Ecchi Land" => {
+			let request = format!(
+				"Lang={}&Page={page}&Type=Include&Dir=NewPostedDate",
+				get_lang_code()
+			);
+			let resp = &Request::new(url, HttpMethod::Post)
+				.body(request.as_bytes())
+				.header(
+					"Content-Type",
+					"application/x-www-form-urlencoded; charset=UTF-8",
+				)
+				.header("X-Requested-With", "XMLHttpRequest")
+				.header("Referer", "https://otakusan.net/")
+				.header("Origin", "https://otakusan.net")
+				.html();
+			let (manga, has_more) = parse_manga_list!(resp.select("div.mdl-card").array());
+			Ok(MangaPageResult { manga, has_more })
+		}
+		"Wallpaper" | "Cosplay" => {
+			let resp = &Request::new(url, HttpMethod::Get)
+				.header(
+					"Content-Type",
+					"application/x-www-form-urlencoded; charset=UTF-8",
+				)
+				.header("X-Requested-With", "XMLHttpRequest")
+				.header("Referer", "https://otakusan.net/")
+				.header("Origin", "https://otakusan.net")
+				.html();
+			let node = resp.select("div.picture-mason");
+			let elems = node.array();
+			let has_more = elems.len() > 0;
+			let mut manga: Vec<Manga> = Vec::with_capacity(elems.len());
+			for elem in elems {
+				let node = elem.as_node();
+				let id = node.select("a").attr("href").read();
+				let url = format!("https://otakusan.net{id}");
+				let cover = node
+					.select("img")
+					.attr("data-src")
+					.read()
+					.replace("http:", "https:");
+				let title = node.select("a").attr("title").read();
+				manga.push(Manga {
+					id,
+					cover,
+					title,
+					author: String::new(),
+					artist: String::new(),
+					description: String::new(),
+					url,
+					categories: Vec::new(),
+					status: MangaStatus::Unknown,
+					nsfw: MangaContentRating::Nsfw,
+					viewer: MangaViewer::Ltr,
+				})
+			}
+			Ok(MangaPageResult { manga, has_more })
+		}
+		_ => Err(AidokuError {
+			reason: AidokuErrorKind::Unimplemented,
+		}),
+	}
 }
 
 #[get_manga_details]
 fn get_manga_details(id: String) -> Result<Manga> {
 	cache_manga_page(&id);
-	let html = unsafe { Node::new(&CACHED_MANGA.clone().unwrap()) };
 	let url = format!("https://otakusan.net{id}");
-	let title = capitalize_first_letter(String::from(
-		html.select("h1.title.text-lg-left.text-overflow-2-line")
+	let html = unsafe { Node::new(&CACHED_MANGA.clone().unwrap()) };
+	if id.contains("manga-detail") {
+		let title = capitalize_first_letter(String::from(
+			html.select("h1.title.text-lg-left.text-overflow-2-line")
+				.text()
+				.read()
+				.trim(),
+		));
+		let cover = html
+			.select("div.container-3-4.background-contain img")
+			.attr("src")
+			.read()
+			.replace("http:", "https:");
+		let description = text_with_newlines(html.select("div.summary p"));
+		let author = capitalize_first_letter(String::from(
+			html.select("tr:contains(Tác Giả) a.capitalize[href*=Author]")
+				.attr("title")
+				.read()
+				.trim(),
+		));
+		let categories = html
+			.select("div.genres a")
+			.array()
+			.map(|val| val.as_node().text().read())
+			.collect::<Vec<String>>();
+		let status = match html
+			.select("tr:contains(Tình Trạng) td")
+			.array()
+			.get(0)
+			.as_node()
 			.text()
 			.read()
-			.trim(),
-	));
-	let cover = html
-		.select("div.container-3-4.background-contain img")
-		.attr("src")
-		.read()
-		.replace("http:", "https:");
-	let description = text_with_newlines(html.select("div.summary p"));
-	let author = capitalize_first_letter(String::from(
-		html.select("tr:contains(Tác Giả) a.capitalize[href*=Author]")
-			.attr("title")
+			.trim()
+		{
+			"Ongoing" => MangaStatus::Ongoing,
+			"Done" => MangaStatus::Completed,
+			_ => MangaStatus::Unknown,
+		};
+		let viewer = match html
+			.select("tr:contains(Loại Truyện) td")
+			.array()
+			.get(0)
+			.as_node()
+			.text()
 			.read()
-			.trim(),
-	));
-	let categories = html
-		.select("div.genres a")
-		.array()
-		.map(|val| val.as_node().text().read())
-		.collect::<Vec<String>>();
-	let status = match html
-		.select("tr:contains(Tình Trạng) td")
-		.array()
-		.get(0)
-		.as_node()
-		.text()
-		.read()
-		.trim()
-	{
-		"Ongoing" => MangaStatus::Ongoing,
-		"Done" => MangaStatus::Completed,
-		_ => MangaStatus::Unknown,
-	};
-	let viewer = match html
-		.select("tr:contains(Loại Truyện) td")
-		.array()
-		.get(0)
-		.as_node()
-		.text()
-		.read()
-		.trim()
-	{
-		"Manhua" | "Manhwa" => MangaViewer::Scroll,
-		"VnComic" => MangaViewer::Ltr,
-		_ => MangaViewer::Rtl,
-	};
-	let (mut nsfw, _) = category_parser(&categories);
-	if html.select("div.alert:contains(18 tuổi)").array().len() > 0 {
-		nsfw = MangaContentRating::Nsfw;
+			.trim()
+		{
+			"Manhua" | "Manhwa" => MangaViewer::Scroll,
+			"VnComic" => MangaViewer::Ltr,
+			_ => MangaViewer::Rtl,
+		};
+		let (mut nsfw, _) = category_parser(&categories);
+		if html.select("div.alert:contains(18 tuổi)").array().len() > 0 {
+			nsfw = MangaContentRating::Nsfw;
+		}
+		Ok(Manga {
+			id,
+			cover,
+			title: String::from(title.trim()),
+			author,
+			artist: String::new(),
+			description: String::from(description.trim()),
+			url,
+			categories,
+			status,
+			nsfw,
+			viewer,
+		})
+	} else if id.contains("wallpaper") || id.contains("Cosplay") {
+		let author = if id.contains("wallpaper") {
+			capitalize_first_letter(String::from(
+				html.select("tr:contains(Artist) td span.capitalize")
+					.text()
+					.read()
+					.trim(),
+			))
+		} else {
+			String::new()
+		};
+		let cover = html.select("div#image_content img").attr("src").read();
+		let title = String::from(
+			html.select("h4.group-header")
+				.array()
+				.get(0)
+				.as_node()
+				.text()
+				.read()
+				.trim()
+				.split(" > ")
+				.collect::<Vec<_>>()[1],
+		);
+		Ok(Manga {
+			id,
+			cover,
+			title,
+			author,
+			artist: String::new(),
+			description: String::new(),
+			url,
+			categories: Vec::new(),
+			status: MangaStatus::Unknown,
+			nsfw: MangaContentRating::Nsfw,
+			viewer: MangaViewer::Ltr,
+		})
+	} else {
+		Err(AidokuError {
+			reason: AidokuErrorKind::Unimplemented,
+		})
 	}
-	Ok(Manga {
-		id,
-		cover,
-		title: String::from(title.trim()),
-		author,
-		artist: String::new(),
-		description: String::from(description.trim()),
-		url,
-		categories,
-		status,
-		nsfw,
-		viewer,
-	})
 }
 
 #[get_chapter_list]
 fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
 	cache_manga_page(&id);
 	let html = unsafe { Node::new(&CACHED_MANGA.clone().unwrap()) };
-	let scanlator = html
-		.select("tr:contains(Nhóm Dịch) a")
-		.attr("title")
-		.read()
-		.replace(" collections", "");
-	let mut lang = html
-		.select("h1.title img.flag")
-		.attr("src")
-		.read()
-		.replace("https://ipdata.co/flags/", "")
-		.replace(".png", "");
-	lang = String::from(match lang.as_str() {
-		"us" => "en",
-		"vn" => "vi",
-		_ => lang.as_str(),
-	});
+	if id.contains("manga-detail") {
+		let scanlator = html
+			.select("tr:contains(Nhóm Dịch) a")
+			.attr("title")
+			.read()
+			.replace(" collections", "");
+		let mut lang = html
+			.select("h1.title img.flag")
+			.attr("src")
+			.read()
+			.replace("https://ipdata.co/flags/", "")
+			.replace(".png", "");
+		lang = String::from(match lang.as_str() {
+			"us" => "en",
+			"vn" => "vi",
+			_ => lang.as_str(),
+		});
 
-	let node = html.select("tr.chapter");
-	let elems = node.array();
-	let mut chapters: Vec<Chapter> = Vec::with_capacity(elems.len());
-	for elem in elems {
-		let elem_node = elem.as_node();
-		let cells_node = elem_node.select("td");
-		let cells = cells_node.array();
-		let mut chapter: Chapter = Chapter {
-			id: String::new(),
-			title: String::new(),
+		let node = html.select("tr.chapter");
+		let elems = node.array();
+		let mut chapters: Vec<Chapter> = Vec::with_capacity(elems.len());
+		for elem in elems {
+			let elem_node = elem.as_node();
+			let cells_node = elem_node.select("td");
+			let cells = cells_node.array();
+			let mut chapter: Chapter = Chapter {
+				id: String::new(),
+				title: String::new(),
+				volume: -1.0,
+				chapter: -1.0,
+				date_updated: -1.0,
+				scanlator: if scanlator.as_str() == "Unknown" {
+					String::new()
+				} else {
+					scanlator.clone()
+				},
+				url: String::new(),
+				lang: lang.clone(),
+			};
+			for (idx, cell) in cells.enumerate() {
+				let node = cell.as_node();
+				match idx {
+					0 => {
+						chapter.chapter =
+							extract_f32_from_string(String::new(), node.text().read());
+					}
+					1 => {
+						let anchor = node.select("a");
+						chapter.id = anchor.attr("href").read();
+						chapter.title = String::from(anchor.text().read().trim());
+						chapter.url = format!("https://otakusan.net{}", chapter.id);
+					}
+					3 => {
+						chapter.date_updated =
+							convert_time(String::from(node.text().read().trim()));
+					}
+					_ => continue,
+				}
+			}
+			chapters.push(chapter);
+		}
+		Ok(chapters)
+	} else if id.contains("wallpaper") || id.contains("Cosplay") {
+		let url = format!("https://otakusan.net{id}");
+		let mut chapters: Vec<Chapter> = Vec::with_capacity(1);
+		chapters.push(Chapter {
+			id: format!("{id}/image"),
+			title: String::from("View image"),
 			volume: -1.0,
 			chapter: -1.0,
-			date_updated: -1.0,
-			scanlator: if scanlator.as_str() == "Unknown" {
-				String::new()
-			} else {
-				scanlator.clone()
-			},
-			url: String::new(),
-			lang: lang.clone(),
-		};
-		for (idx, cell) in cells.enumerate() {
-			let node = cell.as_node();
-			match idx {
-				0 => {
-					chapter.chapter = extract_f32_from_string(String::new(), node.text().read());
-				}
-				1 => {
-					let anchor = node.select("a");
-					chapter.id = anchor.attr("href").read();
-					chapter.title = String::from(anchor.text().read().trim());
-					chapter.url = format!("https://otakusan.net{}", chapter.id);
-				}
-				3 => {
-					chapter.date_updated = convert_time(String::from(node.text().read().trim()));
-				}
-				_ => continue,
-			}
-		}
-		chapters.push(chapter);
+			date_updated: convert_time(String::from(
+				html.select("tr:contains(Ngày Đăng) td span:not(.nav)")
+					.text()
+					.read()
+					.trim(),
+			)),
+			lang: String::new(),
+			scanlator: String::new(),
+			url,
+		});
+		Ok(chapters)
+	} else {
+		Err(AidokuError {
+			reason: AidokuErrorKind::Unimplemented,
+		})
 	}
-	Ok(chapters)
 }
 
 #[get_page_list]
 fn get_page_list(id: String) -> Result<Vec<Page>> {
-	let resp = Request::new(
-		format!("https://otakusan.net{id}").as_str(),
-		HttpMethod::Get,
-	)
-	.html();
-	let vi = resp.select("#dataip").attr("value").read();
-	let numeric_id = resp.select("#inpit-c").attr("data-chapter-id").read();
-	let json = Request::new(
-		"https://otakusan.net/Manga/CheckingAlternate",
-		HttpMethod::Post,
-	)
-	.body(format!("chapId={numeric_id}").as_bytes())
-	.header(
-		"Content-Type",
-		"application/x-www-form-urlencoded; charset=UTF-8",
-	)
-	.header("X-Requested-With", "XMLHttpRequest")
-	.header("Referer", format!("https://otakusan.net{id}").as_str())
-	.header("Origin", "https://otakusan.net")
-	.json();
-	let json_object = json.as_object()?;
-	let raw_pages_arr_value = json_object.get("Content");
-	let raw_pages_arr = if raw_pages_arr_value.is_none() {
-		let json = Request::new("https://otakusan.net/Manga/UpdateView", HttpMethod::Post)
-			.body(format!("chapId={numeric_id}").as_bytes())
-			.header(
-				"Content-Type",
-				"application/x-www-form-urlencoded; charset=UTF-8",
-			)
-			.header("X-Requested-With", "XMLHttpRequest")
-			.header("Referer", format!("https://otakusan.net{id}").as_str())
-			.header("Origin", "https://otakusan.net")
-			.json();
+	if id.contains("chapter") {
+		let resp = Request::new(
+			format!("https://otakusan.net{id}").as_str(),
+			HttpMethod::Get,
+		)
+		.html();
+		let vi = resp.select("#dataip").attr("value").read();
+		let numeric_id = resp.select("#inpit-c").attr("data-chapter-id").read();
+		let json = Request::new(
+			"https://otakusan.net/Manga/CheckingAlternate",
+			HttpMethod::Post,
+		)
+		.body(format!("chapId={numeric_id}").as_bytes())
+		.header(
+			"Content-Type",
+			"application/x-www-form-urlencoded; charset=UTF-8",
+		)
+		.header("X-Requested-With", "XMLHttpRequest")
+		.header("Referer", format!("https://otakusan.net{id}").as_str())
+		.header("Origin", "https://otakusan.net")
+		.json();
 		let json_object = json.as_object()?;
-		let raw_pages_arr_value = json_object.get("view");
-		raw_pages_arr_value.as_string()?.read()
-	} else {
-		raw_pages_arr_value.as_string()?.read()
-	};
-	let pages = json::parse(raw_pages_arr.as_bytes()).as_array()?;
-	let mut page_arr: Vec<Page> = Vec::with_capacity(pages.len());
-	for (index, page) in pages.enumerate() {
-		let url = parser::url_replacer(page.as_string()?.read(), vi.clone());
+		let raw_pages_arr_value = json_object.get("Content");
+		let raw_pages_arr = if raw_pages_arr_value.is_none() {
+			let json = Request::new("https://otakusan.net/Manga/UpdateView", HttpMethod::Post)
+				.body(format!("chapId={numeric_id}").as_bytes())
+				.header(
+					"Content-Type",
+					"application/x-www-form-urlencoded; charset=UTF-8",
+				)
+				.header("X-Requested-With", "XMLHttpRequest")
+				.header("Referer", format!("https://otakusan.net{id}").as_str())
+				.header("Origin", "https://otakusan.net")
+				.json();
+			let json_object = json.as_object()?;
+			let raw_pages_arr_value = json_object.get("view");
+			raw_pages_arr_value.as_string()?.read()
+		} else {
+			raw_pages_arr_value.as_string()?.read()
+		};
+		let pages = json::parse(raw_pages_arr.as_bytes()).as_array()?;
+		let mut page_arr: Vec<Page> = Vec::with_capacity(pages.len());
+		for (index, page) in pages.enumerate() {
+			let url = parser::url_replacer(page.as_string()?.read(), vi.clone());
+			page_arr.push(Page {
+				index: index as i32,
+				url,
+				base64: String::new(),
+				text: String::new(),
+			});
+		}
+		Ok(page_arr)
+	} else if id.contains("wallpaper") || id.contains("Cosplay") {
+		let html = Request::new(
+			format!("https://otakusan.net{}", id.replace("/image", "")).as_str(),
+			HttpMethod::Get,
+		)
+		.html();
+		let mut page_arr: Vec<Page> = Vec::with_capacity(1);
+		let url = html.select("div#image_content img").attr("src").read();
 		page_arr.push(Page {
-			index: index as i32,
+			index: 0,
 			url,
 			base64: String::new(),
 			text: String::new(),
 		});
+		Ok(page_arr)
+	} else {
+		Err(AidokuError {
+			reason: AidokuErrorKind::Unimplemented,
+		})
 	}
-	Ok(page_arr)
 }
 
 #[modify_image_request]
@@ -332,7 +479,7 @@ fn modify_image_request(request: Request) {
 
 #[handle_url]
 fn handle_url(url: String) -> Result<DeepLink> {
-	if url.contains("manga-detail") {
+	if url.contains("manga-detail") || url.contains("Cosplay") || url.contains("wallpaper") {
 		let id = String::from(&url[20..]);
 		Ok(DeepLink {
 			manga: Some(get_manga_details(id)?),
