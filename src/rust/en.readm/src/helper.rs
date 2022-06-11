25e13898644fff124bb9bdc0be9945ecd7528914
Skitty <mrskittyblock@gmail.com>
2022-06-11 09:04:56 -0500

    Format rust files
    cargo +nightly fmt

--- src/rust/en.readm/src/helper.rs
+++ src/rust/en.readm/src/helper.rs
@@ -1,135 +1,142 @@
 use aidoku::{
-    std::{Vec, net::Request, current_date, html::Node},
-    std::{String, net::HttpMethod},
-    MangaStatus, prelude::format,
+	prelude::format,
+	std::{current_date, html::Node, net::Request, Vec},
+	std::{net::HttpMethod, String},
+	MangaStatus,
 };
 
 use crate::BASE_URL;
 pub fn get_chapter_number(id: String) -> f32 {
-    let values: Vec<&str> = id.split("/").collect();
-    return values[5].parse::<f32>().unwrap();
+	let values: Vec<&str> = id.split("/").collect();
+	return values[5].parse::<f32>().unwrap();
 }
 
 pub fn get_manga_id(raw_id: String) -> String {
-    if raw_id.contains("all-pages"){
-        let mut m_id = String::new();
-        let first_part = &raw_id[7..];
-        let last_part = &first_part[..first_part.find("/").unwrap_or(first_part.len())];
-        m_id.push_str(&raw_id[..7]);
-        m_id.push_str(last_part);
-        return m_id;
-    } 
-    else {
-        return raw_id;
-    }
+	if raw_id.contains("all-pages") {
+		let mut m_id = String::new();
+		let first_part = &raw_id[7..];
+		let last_part = &first_part[..first_part.find("/").unwrap_or(first_part.len())];
+		m_id.push_str(&raw_id[..7]);
+		m_id.push_str(last_part);
+		return m_id;
+	} else {
+		return raw_id;
+	}
 }
 
 pub fn get_date(time_ago: String) -> f64 {
-    let number = time_ago.split_whitespace().next().unwrap_or("").parse::<f64>().unwrap_or(0.0);
-    let date = match time_ago.to_uppercase().split_whitespace().last().unwrap_or(""){
-        "YEAR" | "YEARS" => current_date() - (number * 31556926.0),
-        "MONTH" | "MONTHS" => current_date() - (number * 2629743.0),
-        "WEEK" | "WEEKS" => current_date() - (number * 604800.0),
-        "YESTERDAY" | "DAYS" => current_date() - (number * 86400.0),
-        _ => current_date(),
-
-    };
-    return date;
+	let number = time_ago
+		.split_whitespace()
+		.next()
+		.unwrap_or("")
+		.parse::<f64>()
+		.unwrap_or(0.0);
+	let date = match time_ago
+		.to_uppercase()
+		.split_whitespace()
+		.last()
+		.unwrap_or("")
+	{
+		"YEAR" | "YEARS" => current_date() - (number * 31556926.0),
+		"MONTH" | "MONTHS" => current_date() - (number * 2629743.0),
+		"WEEK" | "WEEKS" => current_date() - (number * 604800.0),
+		"YESTERDAY" | "DAYS" => current_date() - (number * 86400.0),
+		_ => current_date(),
+	};
+	return date;
 }
 
 pub fn manga_status(status: String) -> MangaStatus {
-    if status == "ONGOING" {
-        return MangaStatus::Ongoing;
-    } else if status == "COMPLETED" {
-        return MangaStatus::Completed;
-    } else if status == "HIATUS" {
-        return MangaStatus::Hiatus;
-    } else if status == "CANCELLED" {
-        return MangaStatus::Cancelled;
-    } else {
-        return MangaStatus::Unknown;
-    }
+	if status == "ONGOING" {
+		return MangaStatus::Ongoing;
+	} else if status == "COMPLETED" {
+		return MangaStatus::Completed;
+	} else if status == "HIATUS" {
+		return MangaStatus::Hiatus;
+	} else if status == "CANCELLED" {
+		return MangaStatus::Cancelled;
+	} else {
+		return MangaStatus::Unknown;
+	}
 }
 
 pub fn get_page_number(id: String) -> i32 {
-    let html = Request::new(id.clone().as_str(), HttpMethod::Get).html();
-    let mut url = String::new();
-    for manga in html.select(".ui.pagination.menu a").array() {
-        let manga_node = manga.as_node();
-        let last_page_string = manga_node.text().read();
-        if last_page_string == "Last"{
-            url =  String::from(manga_node.attr("href").read().as_str());
-        }
-    }
-    let values: Vec<&str> = url.split("/").collect();
-    return String::from(values[2]).parse::<i32>().unwrap_or(1);
+	let html = Request::new(id.clone().as_str(), HttpMethod::Get).html();
+	let mut url = String::new();
+	for manga in html.select(".ui.pagination.menu a").array() {
+		let manga_node = manga.as_node();
+		let last_page_string = manga_node.text().read();
+		if last_page_string == "Last" {
+			url = String::from(manga_node.attr("href").read().as_str());
+		}
+	}
+	let values: Vec<&str> = url.split("/").collect();
+	return String::from(values[2]).parse::<i32>().unwrap_or(1);
 }
 
-pub fn get_full_url(id: String) -> String{
-    if id.contains("readm.org"){
-        return id;
-    }
-    else{
-        return format!("{}{}", BASE_URL, id);
-    }
+pub fn get_full_url(id: String) -> String {
+	if id.contains("readm.org") {
+		return id;
+	} else {
+		return format!("{}{}", BASE_URL, id);
+	}
 }
 
 pub fn genres() -> [&'static str; 45] {
-        [
-            "all",
-            "action",
-            "adventure",
-            "comedy",
-            "cooking",
-            "doujinshi",
-            "drama",
-            "ecchi",
-            "fantasy",
-            "gender Bender",
-            "harem",
-            "historical",
-            "horror",
-            "isekai",
-            "josei",
-            "lolicon",
-            "magic",
-            "manga",
-            "manhua",
-            "manhwa",
-            "martial Arts",
-            "mature",
-            "mecha",
-            "mind Game",
-            "mystery",
-            "none",
-            "one Shot",
-            "psychological",
-            "recarnation",
-            "romance",
-            "school Life",
-            "sci-fi",
-            "seinen",
-            "shotacon",
-            "shoujo Ai",
-            "shoujo",
-            "shounen Ai",
-            "shounen",
-            "slice of Life",
-            "sports",
-            "supernatural",
-            "tragedy",
-            "uncategorized",
-            "yaoi",
-            "yuri"
-        ]
+	[
+		"all",
+		"action",
+		"adventure",
+		"comedy",
+		"cooking",
+		"doujinshi",
+		"drama",
+		"ecchi",
+		"fantasy",
+		"gender Bender",
+		"harem",
+		"historical",
+		"horror",
+		"isekai",
+		"josei",
+		"lolicon",
+		"magic",
+		"manga",
+		"manhua",
+		"manhwa",
+		"martial Arts",
+		"mature",
+		"mecha",
+		"mind Game",
+		"mystery",
+		"none",
+		"one Shot",
+		"psychological",
+		"recarnation",
+		"romance",
+		"school Life",
+		"sci-fi",
+		"seinen",
+		"shotacon",
+		"shoujo Ai",
+		"shoujo",
+		"shounen Ai",
+		"shounen",
+		"slice of Life",
+		"sports",
+		"supernatural",
+		"tragedy",
+		"uncategorized",
+		"yaoi",
+		"yuri",
+	]
 }
 
-pub fn get_image_src(node:Node) -> String{
-    let image = node.select("img").first().attr("src").read();
-    if image.starts_with("data") || image == "" {
-        return node.select("img").first().attr("data-src").read();
-    }else {
-        return image;
-    }
+pub fn get_image_src(node: Node) -> String {
+	let image = node.select("img").first().attr("src").read();
+	if image.starts_with("data") || image == "" {
+		return node.select("img").first().attr("data-src").read();
+	} else {
+		return image;
+	}
 }
-
--- src/rust/en.readm/src/lib.rs
+++ src/rust/en.readm/src/lib.rs
@@ -1,48 +1,56 @@
 #![no_std]
 use aidoku::{
-    error::Result,
-    prelude::*,
-    std::{String, Vec},
-    Chapter, Listing, Manga, MangaPageResult, Page, std::net::Request, Filter
+	error::Result,
+	prelude::*,
+	std::net::Request,
+	std::{String, Vec},
+	Chapter, Filter, Listing, Manga, MangaPageResult, Page,
 };
 
-pub mod parser;
 pub mod helper;
+pub mod parser;
 
 const BASE_URL: &'static str = "https://readm.org";
 
 #[get_manga_list]
 fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
-    parser::parse_manga_list(String::from(BASE_URL), filters, page)
+	parser::parse_manga_list(String::from(BASE_URL), filters, page)
 }
 
 #[get_manga_listing]
 fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
-    let url = match listing.name.as_str() {
-        "Latest" => format!("{}/latest-releases/{}", BASE_URL, page),
-        "Popular" => format!("{}/popular-manga/{}", BASE_URL,page),
-        _ => String::from(BASE_URL),
-    };
-    parser::parse_manga_listing(String::from(BASE_URL),String::from(url), String::from(listing.name))
+	let url = match listing.name.as_str() {
+		"Latest" => format!("{}/latest-releases/{}", BASE_URL, page),
+		"Popular" => format!("{}/popular-manga/{}", BASE_URL, page),
+		_ => String::from(BASE_URL),
+	};
+	parser::parse_manga_listing(
+		String::from(BASE_URL),
+		String::from(url),
+		String::from(listing.name),
+	)
 }
 
 #[get_manga_details]
 fn get_manga_details(id: String) -> Result<Manga> {
-    parser::parse_manga_details(String::from(BASE_URL), id)
+	parser::parse_manga_details(String::from(BASE_URL), id)
 }
 
 #[get_chapter_list]
 fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
-    parser::parse_chapter_list(String::from(BASE_URL), String::from("MMM dd,yyyy HH:mm"),id)
+	parser::parse_chapter_list(
+		String::from(BASE_URL),
+		String::from("MMM dd,yyyy HH:mm"),
+		id,
+	)
 }
 
 #[get_page_list]
 fn get_page_list(id: String) -> Result<Vec<Page>> {
-    parser::parse_page_list(String::from(BASE_URL),id)
-
+	parser::parse_page_list(String::from(BASE_URL), id)
 }
 
-#[modify_image_request] 
+#[modify_image_request]
 fn modify_image_request(request: Request) {
 	parser::modify_image_request(String::from(BASE_URL), request)
 }
