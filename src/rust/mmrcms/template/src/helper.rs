46f1f133c8f750cc31e610c538210af880d5efcc
beerpiss <lacvtg.a1.2023@gmail.com>
2022-06-13 16:01:31 +0700

    new source: komikid

--- src/rust/mmrcms/template/src/helper.rs
+++ src/rust/mmrcms/template/src/helper.rs
@@ -2,6 +2,7 @@ use aidoku::{prelude::format, std::html::Node, std::String, std::Vec};
 
 pub fn extract_f32_from_string(title: String, text: String) -> f32 {
 	text.replace(&title, "")
+		.replace(',', ".")
 		.chars()
 		.filter(|a| (*a >= '0' && *a <= '9') || *a == ' ' || *a == '.')
 		.collect::<String>()
