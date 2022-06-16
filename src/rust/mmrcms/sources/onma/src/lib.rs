da5aa2d7bff3a248cf165b90e9746905cad2da5c
beerpiss <lacvtg.a1.2023@gmail.com>
2022-06-17 06:34:40 +0700

    fix email_unprotected

--- src/rust/mmrcms/sources/onma/src/lib.rs
+++ src/rust/mmrcms/sources/onma/src/lib.rs
@@ -8,7 +8,7 @@ use aidoku::{
 };
 use lazy_static::lazy_static;
 use mmrcms_template::{
-	helper::text_with_newlines,
+	helper::{email_unprotected, text_with_newlines},
 	template::{cache_manga_page, MMRCMSSource, CACHED_MANGA},
 };
 
@@ -46,7 +46,7 @@ fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
 fn get_manga_details(id: String) -> Result<Manga> {
 	let url = format!("{}/{}/{}", INSTANCE.base_url, INSTANCE.manga_path, id);
 	cache_manga_page(&url);
-	let html = unsafe { CACHED_MANGA.clone().unwrap() };
+	let html = email_unprotected(unsafe { CACHED_MANGA.clone().unwrap() });
 
 	let title = html.select("div.panel-heading").text().read();
 	let cover = html.select("img.img-thumbnail").attr("abs:src").read();
@@ -82,7 +82,7 @@ fn get_manga_details(id: String) -> Result<Manga> {
 		}
 	}
 	(manga.nsfw, manga.viewer) = (INSTANCE.category_parser)(&html, manga.categories.clone());
-	if html.select("div.alert.alert-danger").array().len() > 0 {
+	if !html.select("div.alert.alert-danger").array().is_empty() {
 		manga.nsfw = MangaContentRating::Nsfw;
 	}
 	Ok(manga)
