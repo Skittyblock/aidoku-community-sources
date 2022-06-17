4a316e74a69ff3448059993bc4ad07cba2374408
Trzeth <Trzeth@outlook.com>
2022-06-17 08:31:08 +0800

    init

---
+++ src/rust/zh.dmzj/src/helper.rs
@@ -0,0 +1,132 @@
+use aidoku::{
+    error::Result, prelude::*, std::net::HttpMethod, std::net::Request, std::ArrayRef, std::String,
+    std::ValueRef, std::Vec,
+};
+
+use prost::bytes::Bytes;
+use prost::Message;
+
+use base64ct::{Base64, Encoding};
+use rsa::{pkcs8::DecodePrivateKey, RsaPrivateKey, RsaPublicKey};
+
+pub mod protobuf {
+    include!(concat!(env!("OUT_DIR"), "/dmzj.comic.rs"));
+}
+
+// 地区
+pub fn type_list() -> [i32; 7] {
+    [0, 2304, 2305, 2306, 2307, 2308, 8453]
+}
+
+// 读者
+pub fn reader_list() -> [i32; 4] {
+    [0, 3262, 3263, 3264]
+}
+
+// 连载状态
+pub fn status_list() -> [i32; 3] {
+    [0, 2309, 2310]
+}
+
+// 分类
+pub fn genre_list() -> [i32; 42] {
+    [
+        0, 4, 3243, 3242, 17, 3244, 3245, 3249, 3248, 3246, 16, 14, 7, 6, 5, 8, 9, 13, 12, 11, 10,
+        3250, 3251, 5806, 5345, 5077, 5848, 6316, 7900, 7568, 6437, 4518, 4459, 3254, 3253, 3252,
+        3255, 6219, 3328, 3365, 3326, 3325,
+    ]
+}
+
+pub fn encodeURI(string: &String) -> String {
+    let mut result: Vec<u8> = Vec::with_capacity(string.len() * 3);
+    let hex = "0123456789abcdef".as_bytes();
+    let bytes = string.as_bytes();
+
+    for byte in bytes {
+        let curr = *byte;
+        if (b'a' <= curr && curr <= b'z')
+            || (b'A' <= curr && curr <= b'Z')
+            || (b'0' <= curr && curr <= b'9')
+            || (curr == b';'
+                || curr == b','
+                || curr == b'/'
+                || curr == b'?'
+                || curr == b':'
+                || curr == b'@'
+                || curr == b'&'
+                || curr == b'='
+                || curr == b'+'
+                || curr == b'$')
+            || (curr == b'-'
+                || curr == b'_'
+                || curr == b'.'
+                || curr == b'!'
+                || curr == b'~'
+                || curr == b'*'
+                || curr == b'\''
+                || curr == b'('
+                || curr == b')')
+            || (curr == b'#')
+        {
+            result.push(curr);
+        } else {
+            result.push(b'%');
+            result.push(hex[curr as usize >> 4]);
+            result.push(hex[curr as usize & 15]);
+        }
+    }
+
+    String::from_utf8(result).unwrap_or(String::new())
+}
+
+pub fn i32_to_string(mut integer: i32) -> String {
+    if integer == 0 {
+        return String::from("0");
+    }
+    let mut string = String::with_capacity(11);
+    let pos = if integer < 0 {
+        string.insert(0, '-');
+        1
+    } else {
+        0
+    };
+    while integer != 0 {
+        let mut digit = integer % 10;
+        if pos == 1 {
+            digit *= -1;
+        }
+        string.insert(pos, char::from_u32((digit as u32) + ('0' as u32)).unwrap());
+        integer /= 10;
+    }
+    return string;
+}
+
+pub fn GET(url: String) -> Request {
+    let mut req = Request::new(&url, HttpMethod::Get);
+    req = req.header("Referer", "https://www.dmzj.com/");
+    return req.header("User-Agent",
+    "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.93 Mobile Safari/537.36 Aidoku/1.0");
+}
+
+const KEY:&str = "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAK8nNR1lTnIfIes6oRWJNj3mB6OssDGx0uGMpgpbVCpf6+VwnuI2stmhZNoQcM417Iz7WqlPzbUmu9R4dEKmLGEEqOhOdVaeh9Xk2IPPjqIu5TbkLZRxkY3dJM1htbz57d/roesJLkZXqssfG5EJauNc+RcABTfLb4IiFjSMlTsnAgMBAAECgYEAiz/pi2hKOJKlvcTL4jpHJGjn8+lL3wZX+LeAHkXDoTjHa47g0knYYQteCbv+YwMeAGupBWiLy5RyyhXFoGNKbbnvftMYK56hH+iqxjtDLnjSDKWnhcB7089sNKaEM9Ilil6uxWMrMMBH9v2PLdYsqMBHqPutKu/SigeGPeiB7VECQQDizVlNv67go99QAIv2n/ga4e0wLizVuaNBXE88AdOnaZ0LOTeniVEqvPtgUk63zbjl0P/pzQzyjitwe6HoCAIpAkEAxbOtnCm1uKEp5HsNaXEJTwE7WQf7PrLD4+BpGtNKkgja6f6F4ld4QZ2TQ6qvsCizSGJrjOpNdjVGJ7bgYMcczwJBALvJWPLmDi7ToFfGTB0EsNHZVKE66kZ/8Stx+ezueke4S556XplqOflQBjbnj2PigwBN/0afT+QZUOBOjWzoDJkCQClzo+oDQMvGVs9GEajS/32mJ3hiWQZrWvEzgzYRqSf3XVcEe7PaXSd8z3y3lACeeACsShqQoc8wGlaHXIJOHTcCQQCZw5127ZGs8ZDTSrogrH73Kw/HvX55wGAeirKYcv28eauveCG7iyFR0PFB/P/EDZnyb+ifvyEFlucPUI0+Y87F";
+
+pub fn DECODE(base64: &String) -> protobuf::ComicDetailResponse {
+    let keyByte = Base64::decode_vec(KEY).unwrap();
+    let privateKey = rsa::RsaPrivateKey::from_pkcs8_der(&keyByte).unwrap();
+    let r = Base64::decode_vec(&base64).unwrap();
+    const BLOCK_SIZE: usize = 128;
+    let mut iter = r.chunks(BLOCK_SIZE);
+
+    let mut rr = Vec::new();
+    while let Some(ptr) = iter.next() {
+        rr = [
+            rr,
+            privateKey
+                .decrypt(rsa::PaddingScheme::PKCS1v15Encrypt, ptr)
+                .unwrap(),
+        ]
+        .concat();
+    }
+
+    return prost::Message::decode(Bytes::from(rr)).unwrap();
+}
---
+++ src/rust/zh.dmzj/src/lib.rs
@@ -0,0 +1,414 @@
+/*	Created by reference to https://github.com/tachiyomiorg/tachiyomi-extensions/tree/master/src/zh/dmzj
+ *	All credit goes to their outstanding work.
+ */
+
+#![no_std]
+use core::ops::Deref;
+
+use aidoku::{
+    error::Result,
+    prelude::*,
+    std::net::HttpMethod,
+    std::net::Request,
+    std::ObjectRef,
+    std::{defaults::defaults_get, format},
+    std::{json, ArrayRef},
+    std::{String, StringRef, Vec},
+    Chapter, DeepLink, Filter, FilterType, Listing, Manga, MangaContentRating, MangaPageResult,
+    MangaStatus, MangaViewer, Page,
+};
+
+mod helper;
+
+const BASE_URL: &str = "https://m.dmzj.com";
+const V3_API_URL: &str = "https://v3api.dmzj.com";
+const V3_API_CHAPTER_URL: &str = "https://nnv3api.muwai.com";
+// v3api now shutdown the functionality to fetch manga detail and chapter list,
+// so move these logic to v4api
+const V4_API_URL: &str = "https://nnv4api.muwai.com"; // https://v4api.dmzj1.com
+const API_URL: &str = "https://api.dmzj.com";
+const API_PAGELIST_OLD_URL: &str = "https://api.m.dmzj.com";
+const API_PAGELIST_WEBVIEW_URL: &str = "https://m.dmzj.com/chapinfo";
+const IMAGE_URL: &str = "https://images.dmzj.com";
+const IMAGE_SMALL_URL: &str = "https://imgsmall.dmzj.com";
+
+const FILTER_GENRE: [i32; 42] = [
+    0, 4, 3243, 3242, 17, 3244, 3245, 3249, 3248, 3246, 16, 14, 7, 6, 5, 8, 9, 13, 12, 11, 10,
+    3250, 3251, 5806, 5345, 5077, 5848, 6316, 7900, 7568, 6437, 4518, 4459, 3254, 3253, 3252, 3255,
+    6219, 3328, 3365, 3326, 3325,
+];
+
+const FILTER_STATUS: [i32; 3] = [0, 2309, 2310];
+const FILTER_READER: [i32; 4] = [0, 3262, 3263, 3264];
+const FILTER_TYPE: [i32; 7] = [0, 2304, 2305, 2306, 2307, 2308, 8453];
+
+#[get_manga_list]
+pub fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
+    let mut is_keyword: bool = false;
+    let mut keyword: String = String::new();
+    let mut sort: i32 = 0;
+    let mut filters_list: Vec<i32> = Vec::new();
+
+    for filter in filters {
+        match filter.kind {
+            FilterType::Title => {
+                let title = filter.value.as_string()?.read();
+                is_keyword = !title.is_empty();
+
+                if is_keyword {
+                    keyword = title.clone();
+                    break;
+                }
+            }
+            FilterType::Select => {
+                let index = filter.value.as_int()? as usize;
+                let id = match filter.name.as_str() {
+                    "连载状态" => FILTER_STATUS[index],
+                    "读者" => FILTER_READER[index],
+                    "地区" => FILTER_TYPE[index],
+                    "分类" => FILTER_GENRE[index],
+                    _ => continue,
+                };
+
+                if id != 0 {
+                    filters_list.push(id);
+                }
+            }
+            FilterType::Sort => {
+                let value = match filter.value.as_object() {
+                    Ok(value) => value,
+                    Err(_) => continue,
+                };
+                sort = value.get("index").as_int()? as i32;
+            }
+            _ => continue,
+        }
+    }
+
+    let mut manga_arr: Vec<Manga> = Vec::new();
+
+    if is_keyword {
+        let url = format!(
+            "http://s.acg.dmzj.com/comicsum/search.php?s={}",
+            &helper::encodeURI(&keyword)
+        );
+
+        let data = {
+            let req = helper::GET(url);
+            let r = req.string();
+
+            let r = r
+                .strip_prefix("var g_search_data = ")
+                .unwrap()
+                .strip_suffix(";")
+                .unwrap();
+
+            json::parse(r.as_bytes().clone()).as_array()?
+        };
+
+        for it in data {
+            let it = it.as_object()?;
+            let id = helper::i32_to_string(it.get("id").as_int()? as i32);
+            let author = it.get("comic_author").as_string()?.read();
+            let title = it.get("comic_name").as_string()?.read();
+            let cover = it.get("comic_cover").as_string()?.read();
+
+            manga_arr.push(Manga {
+                id,
+                cover,
+                title,
+                author: String::new(),
+                artist: author,
+                description: String::new(),
+                url: String::new(),
+                categories: Vec::new(),
+                status: MangaStatus::Unknown,
+                nsfw: MangaContentRating::Safe,
+                viewer: MangaViewer::Rtl,
+            });
+        }
+    } else {
+        let mut filters_query: String = String::from("0");
+        if filters_list.len() != 0 {
+            for i in filters_list {
+                filters_query.push_str(&helper::i32_to_string(i));
+                filters_query.push('-');
+            }
+            filters_query.pop();
+        }
+
+        let url = format!(
+            "{}/classify/{}/{}/{}.json",
+            V3_API_URL,
+            filters_query,
+            helper::i32_to_string(sort),
+            helper::i32_to_string(page)
+        );
+
+        let data = Request::new(&url, HttpMethod::Get).json().as_array()?;
+
+        for it in data {
+            let it = it.as_object()?;
+            let id = helper::i32_to_string(it.get("id").as_int()? as i32);
+            let author = it.get("authors").as_string()?.read();
+            let title = it.get("title").as_string()?.read();
+            let cover = it.get("cover").as_string()?.read();
+            let status = match it.get("status").as_string()?.read().as_str() {
+                "连载中" => MangaStatus::Ongoing,
+                "已完结" => MangaStatus::Completed,
+                _ => MangaStatus::Unknown,
+            };
+            manga_arr.push(Manga {
+                id,
+                cover,
+                title,
+                author: String::new(),
+                artist: author,
+                description: String::new(),
+                url: String::new(),
+                categories: Vec::new(),
+                status: status,
+                nsfw: MangaContentRating::Safe,
+                viewer: MangaViewer::Rtl,
+            });
+        }
+    }
+
+    let len = manga_arr.len();
+    Ok(MangaPageResult {
+        manga: manga_arr,
+        has_more: if is_keyword { false } else { len != 0 },
+    })
+}
+
+#[get_manga_details]
+fn get_manga_details(id: String) -> Result<Manga> {
+    let url = format!(
+        "{}/comic/detail/{}?channel=android&version=3.0.0&timestamp={}",
+        V4_API_URL,
+        &id,
+        aidoku::std::current_date() as i64
+    );
+
+    let pb = helper::DECODE(&helper::GET(url).string());
+    if pb.errno == 0 {
+        let pbData = pb.data.unwrap();
+        return Ok(Manga {
+            id: id.clone(),
+            cover: pbData.cover,
+            title: pbData.title,
+            author: String::from(""),
+            artist: String::from(""),
+            description: pbData.description,
+            url: format!("{}/info/{}.html", BASE_URL, id),
+            categories: pbData
+                .types
+                .iter()
+                .map(|s| String::from(s.tag_name.clone()))
+                .collect(),
+            status: match pbData.status[0].tag_name.as_str() {
+                "连载中" => MangaStatus::Ongoing,
+                "已完结" => MangaStatus::Completed,
+                _ => MangaStatus::Unknown,
+            },
+            nsfw: MangaContentRating::Safe,
+            viewer: MangaViewer::Ltr,
+        });
+    } else {
+        // Try old api
+
+        let url = format!("{}/dynamic/comicinfo/{}.json", API_URL, id);
+        let req = helper::GET(url);
+
+        let info = req
+            .json()
+            .as_object()?
+            .get("data")
+            .as_object()?
+            .get("info")
+            .as_object()?;
+
+        return Ok(Manga {
+            id: id.clone(),
+            cover: info.get("cover").as_string()?.read(),
+            title: info.get("title").as_string()?.read(),
+            author: info.get("authors").as_string()?.read(),
+            artist: String::from(""),
+            description: info.get("description").as_string()?.read(),
+            url: format!("{}/info/{}.html", BASE_URL, id),
+            categories: info
+                .get("types")
+                .as_string()?
+                .read()
+                .split("/")
+                .collect::<Vec<_>>()
+                .iter()
+                .map(|s| String::from(s.deref()))
+                .collect(),
+            status: match info.get("status").as_string()?.read().as_str() {
+                "连载中" => MangaStatus::Ongoing,
+                "已完结" => MangaStatus::Completed,
+                _ => MangaStatus::Unknown,
+            },
+            nsfw: MangaContentRating::Safe,
+            viewer: MangaViewer::Ltr,
+        });
+    }
+}
+
+#[get_chapter_list]
+fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
+    let url = format!(
+        "{}/comic/detail/{}?channel=android&version=3.0.0&timestamp={}",
+        V4_API_URL,
+        &id,
+        aidoku::std::current_date() as i64
+    );
+
+    let pb = helper::DECODE(&helper::GET(url).string());
+
+    let mut chapters = Vec::new();
+
+    if pb.errno == 0 && pb.data.as_ref().unwrap().chapters.len() != 0 {
+        let pbData = pb.data.unwrap();
+        let mut volume = 0;
+        let hasMultiChapter = pbData.chapters.len() >= 2;
+        for chapterList in pbData.chapters {
+            volume = volume + 1;
+            for chapter in chapterList.data {
+                chapters.push(Chapter {
+                    id: format!("{}/{}", pbData.id, chapter.chapter_id),
+                    title: format!("{}: {}", chapterList.title, chapter.chapter_title),
+                    volume: if hasMultiChapter { volume as f32 } else { -1.0 },
+                    chapter: (chapter.chapter_order / 10) as f32,
+                    date_updated: chapter.updatetime as f64,
+                    scanlator: String::new(),
+                    url: String::new(),
+                    lang: String::from("zh"),
+                });
+            }
+        }
+    } else {
+        let url = format!("{}/dynamic/comicinfo/{}.json", API_URL, id);
+        println!("{}", url);
+        let req = helper::GET(url);
+
+        let list = req
+            .json()
+            .as_object()?
+            .get("data")
+            .as_object()?
+            .get("list")
+            .clone()
+            .as_array()?;
+
+        for chapter in list {
+            let data = chapter.as_object()?;
+
+            chapters.push(Chapter {
+                id: format!("{}/{}", id.clone(), data.get("id").as_string()?.read()),
+                title: data.get("chapter_name").as_string()?.read(),
+                volume: -1.0,
+                chapter: (data.get("chapter_order").as_int()? / 10) as f32,
+                date_updated: data.get("updatetime").as_int()? as f64,
+                scanlator: String::new(),
+                url: String::new(),
+                lang: String::from("zh"),
+            });
+        }
+    }
+    return Ok(chapters);
+}
+
+#[get_page_list]
+fn get_page_list(id: String) -> Result<Vec<Page>> {
+    // Not Tested
+    // Maybe only use the first one.
+
+    let url = [
+        format!("{}/{}.html", API_PAGELIST_WEBVIEW_URL, &id),
+        format!(
+            "{}/chapter/{}.json?channel=android&version=3.0.0&timestamp={}",
+            V3_API_CHAPTER_URL,
+            &id,
+            aidoku::std::current_date() as i64
+        ),
+        format!("{}/comic/chapter/{}.html", API_PAGELIST_OLD_URL, &id),
+    ];
+    let mut index = 0;
+
+    let arr = loop {
+        if index > 2 {
+            break ArrayRef::new();
+        }
+
+        let req = helper::GET(url[index].clone());
+
+        let req = req.json();
+        let r = match index {
+            0 | 1 => req.as_object()?.get("page_url").clone().as_array().ok(),
+            2 => req
+                .as_object()?
+                .get("chapter")
+                .as_object()?
+                .get("page_url")
+                .clone()
+                .as_array()
+                .ok(),
+            _ => None,
+        };
+        match r {
+            Some(r) => break r,
+            _ => index = index + 1,
+        };
+    };
+
+    let mut pages = Vec::new();
+
+    let mut index = 0;
+
+    for r in arr {
+        let mut imageUrl = r.as_string()?.read();
+        imageUrl = imageUrl
+            .replace("http:", "https:")
+            .replace("dmzj1.com", "dmzj.com");
+
+        let thumbUrl = {
+            if id != "" {
+                let initial = imageUrl
+                    .strip_prefix("https://images.dmzj.com/")
+                    .unwrap()
+                    .get(0..1)
+                    .unwrap();
+
+                format!("{}/{}/{}/{}.jpg", IMAGE_SMALL_URL, initial, id, index)
+            } else {
+                String::new()
+            }
+        };
+
+        pages.push(Page {
+            index,
+            url: helper::encodeURI(&imageUrl),
+            base64: String::new(),
+            text: String::new(),
+        });
+
+        index += 1;
+    }
+
+    Ok(pages)
+}
+
+// Doesn't work
+#[modify_image_request]
+fn modify_image_request(request: Request) {
+    request.header("Referer", "https://www.dmzj.com/");
+}
+
+#[handle_url]
+pub fn handle_url(url: String) -> Result<DeepLink> {
+    Err(aidoku::error::AidokuError {
+        reason: aidoku::error::AidokuErrorKind::Unimplemented,
+    })
+}
