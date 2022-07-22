67125e06edd69c64b58516b252da2f09b5d0904a
Jim Phieffer <phiefferj24@columbusacademy.org>
2022-07-21 22:12:17 -0400

    skitty's changes

--- src/rust/multi.nhentai/src/lib.rs
+++ src/rust/multi.nhentai/src/lib.rs
@@ -83,8 +83,7 @@ fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
 
 	if is_sauce_code {
 		let url = helper::get_details_url(sauce_code);
-		let request = Request::new(&url, HttpMethod::Get);
-		let request = request.header("User-Agent", "Aidoku");
+		let request = Request::new(&url, HttpMethod::Get).header("User-Agent", "Aidoku");
 		let json = request.json().as_object()?;
 
 		let id = helper::get_id(json.get("id"))?;
@@ -116,8 +115,7 @@ fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
 		url.push_str("&sort=");
 		url.push_str(&helper::urlencode(sort));
 
-		let request = Request::new(&url, HttpMethod::Get);
-		let request = request.header("User-Agent", "Aidoku");
+		let request = Request::new(&url, HttpMethod::Get).header("User-Agent", "Aidoku");
 		let json = request.json().as_object()?;
 
 		let data = json.get("result").as_array()?;
@@ -181,8 +179,7 @@ fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
 
 #[get_manga_details]
 fn get_manga_details(id: String) -> Result<Manga> {
-	let request = Request::new(helper::get_details_url(id).as_str(), HttpMethod::Get);
-	let request = request.header("User-Agent", "Aidoku");
+	let request = Request::new(helper::get_details_url(id).as_str(), HttpMethod::Get).header("User-Agent", "Aidoku");
 	let json = request.json().as_object()?;
 
 	let id = helper::get_id(json.get("id"))?;
@@ -222,10 +219,8 @@ fn get_manga_details(id: String) -> Result<Manga> {
 
 #[get_chapter_list]
 fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
-	let request = Request::new(helper::get_details_url(id.clone()).as_str(), HttpMethod::Get);
-	let request = request.header("User-Agent", "Aidoku");
+	let request = Request::new(helper::get_details_url(id.clone()).as_str(), HttpMethod::Get).header("User-Agent", "Aidoku");
 	let json = request.json().as_object()?;
-	// let id = helper::get_id(json.get("id"))?;
 
 	let mut url = String::from("https://nhentai.net/g/");
 	url.push_str(&id);
@@ -258,8 +253,7 @@ fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
 
 #[get_page_list]
 fn get_page_list(id: String) -> Result<Vec<Page>> {
-	let request = Request::new(helper::get_details_url(id).as_str(), HttpMethod::Get);
-	let request = request.header("User-Agent", "Aidoku");
+	let request = Request::new(helper::get_details_url(id).as_str(), HttpMethod::Get).header("User-Agent", "Aidoku");
 	let json = request.json().as_object()?;
 
 	let images = json.get("images").as_object()?;
