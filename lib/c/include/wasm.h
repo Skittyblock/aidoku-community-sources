3dac6111ee9cd00a59799f854bfbdbafde3d7b3b
Skitty <mrskittyblock@gmail.com>
2022-11-20 22:26:38 -0600

    Remove C build support
    Opting towards rust-only for community sources.

--- lib/c/include/wasm.h
+++
@@ -1,3 +0,0 @@
-#define WASM_IMPORT(x, y) __attribute__((import_module(x))) __attribute__((import_name(y))) 
-#define WASM_EXPORT __attribute__((visibility("default")))
-#define WASM_EXPORT_AS __attribute__((export_name(x)))
