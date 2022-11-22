526455486502219cdc33aad6cc806a91470c0c49
johntyty912 <johntyty912@gmail.com>
2022-11-22 11:08:56 +0800

    remove unused index

--- src/rust/zh.manhuagui/src/parser.rs
+++ src/rust/zh.manhuagui/src/parser.rs
@@ -200,7 +200,6 @@ pub fn parse_manga_details(html: Node, manga_id: String) -> Result<Manga> {
 
 pub fn get_chapter_list(html: Node) -> Result<Vec<Chapter>> {
 	let mut chapters: Vec<Chapter> = Vec::new();
-	let mut index = 1.0;
 
 	let mut div = html.clone();
 	let hidden = html.html().read().contains("__VIEWSTATE");
@@ -257,7 +256,6 @@ pub fn get_chapter_list(html: Node) -> Result<Vec<Chapter>> {
 				};
 
 				chapters.push(chapter);
-				index += 1.0;
 			}
 		}
 	}
