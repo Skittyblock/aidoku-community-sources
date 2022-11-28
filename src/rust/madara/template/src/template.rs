95639a9d7e7b03dcc0f432cc252f55bee25c4735
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-11-28 16:46:34 -0500

    feat(template): allow the base_id selector to be overridden

--- src/rust/madara/template/src/template.rs
+++ src/rust/madara/template/src/template.rs
@@ -31,6 +31,7 @@ pub struct MadaraSiteData {
 	pub genre_selector: String,
 	pub description_selector: String,
 	pub chapter_selector: String,
+	pub base_id_selector: String,
 
 	pub status_filter_ongoing: String,
 	pub status_filter_completed: String,
@@ -69,6 +70,8 @@ impl Default for MadaraSiteData {
 			description_selector: String::from("div.description-summary div p"),
 			// selector for chapter list
 			chapter_selector: String::from("li.wp-manga-chapter"),
+			// a to get the base id from requests to admin-ajax.php
+			base_id_selector: String::from("h3.h5 > a"),
 			// div to select images from a chapter
 			image_selector: String::from("div.page-break > img"),
 			// div to select all the genres
@@ -198,7 +201,7 @@ pub fn get_series_page(data: MadaraSiteData, listing: &str, page: i32) -> Result
 			continue;
 		}
 
-		let base_id = obj.select("h3.h5 > a").attr("href").read();
+		let base_id = obj.select(&data.base_id_selector).attr("href").read();
 		let final_path = base_id
 			.strip_prefix(&data.base_url)
 			.unwrap_or(&base_id)
@@ -213,7 +216,7 @@ pub fn get_series_page(data: MadaraSiteData, listing: &str, page: i32) -> Result
 			.unwrap_or(final_path)
 			.to_string();
 
-		let title = obj.select("h3.h5 > a").text().read();
+		let title = obj.select(&data.base_id_selector).text().read();
 
 		let cover = get_image_url(obj.select("img"));
 
