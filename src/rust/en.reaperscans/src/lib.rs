8041a2b601302384385997b6aa13215add5747af
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-12-09 19:15:01 -0500

    mock(reaperscans): get_manga_listing implemented

--- src/rust/en.reaperscans/src/lib.rs
+++ src/rust/en.reaperscans/src/lib.rs
@@ -51,7 +51,80 @@ fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
 
 #[get_manga_listing]
 fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
-	todo!()
+	let url = match listing.name.as_str() {
+		"Latest" => format!("{}/latest/comics?page={}", BASE_URL, page),
+		_ => format!("{}/comics/{}", BASE_URL, page),
+	};
+
+	let mut mangas: Vec<Manga> = Vec::new();
+	let mut has_more = false;
+
+	if &listing.name == "Latest" {
+		let html = Request::new(&url, HttpMethod::Get)
+			.html()
+			.expect("i brokey");
+		for manga in html
+			.select("main div[wire:id] div.grid > div.relative")
+			.array()
+		{
+			let manga_node = manga.as_node().expect("now i brokey");
+			let title = String::from(manga_node.select("p.text-sm a").text().read().trim());
+			let id = manga_node.select("a").attr("href").read();
+			let cover = manga_node.select("img").attr("src").read();
+			// let id = get_manga_id(manga_node.select("a").attr("href").read());
+			// let cover = base_url.clone() + &get_image_src(manga_node);
+			mangas.push(Manga {
+				id,
+				cover,
+				title,
+				author: String::new(),
+				artist: String::new(),
+				description: String::new(),
+				url: String::new(),
+				categories: Vec::new(),
+				status: MangaStatus::Unknown,
+				nsfw: MangaContentRating::Safe,
+				viewer: MangaViewer::Scroll,
+			});
+		}
+
+		let last_page = html.select("main div[wire:id] div nav span").text().read();
+		has_more = last_page.contains("Next");
+	} else {
+		let html = Request::new(&url, HttpMethod::Get)
+			.html()
+			.expect("i brokey");
+		let mut mangas: Vec<Manga> = Vec::new();
+		for manga in html.select("main div[wire:id] div > li > div").array() {
+			let manga_node = manga.as_node().expect("now i brokey");
+			let title = String::from(manga_node.select("a.text-sm").text().read().trim());
+			let id = manga_node.select("a").attr("href").read();
+			let cover = manga_node.select("img").attr("src").read();
+			// let id = get_manga_id(manga_node.select("a").attr("href").read());
+			// let cover = base_url.clone() + &get_image_src(manga_node);
+			mangas.push(Manga {
+				id,
+				cover,
+				title,
+				author: String::new(),
+				artist: String::new(),
+				description: String::new(),
+				url: String::new(),
+				categories: Vec::new(),
+				status: MangaStatus::Unknown,
+				nsfw: MangaContentRating::Safe,
+				viewer: MangaViewer::Scroll,
+			});
+		}
+
+		let last_page = html.select("main div[wire:id] div nav span").text().read();
+		has_more = last_page.contains("Next");
+	}
+
+	Ok(MangaPageResult {
+		manga: mangas,
+		has_more,
+	})
 }
 
 #[get_manga_details]
