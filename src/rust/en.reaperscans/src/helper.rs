c2dc469ccab6b46b768ecc78b4c28b92023e48ee
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-12-11 12:29:53 -0500

    feat(helper): add get_date function

--- src/rust/en.reaperscans/src/helper.rs
+++ src/rust/en.reaperscans/src/helper.rs
@@ -1,6 +1,6 @@
 use aidoku::{
 	prelude::format,
-	std::{format, html::Node, String, Vec},
+	std::{current_date, format, html::Node, String, Vec},
 };
 
 /// Returns an array of f32s contained within a string.
@@ -136,4 +136,28 @@ pub fn get_chapter_url(chapter_id: String, manga_id: String, base_url: String) -
 	// return "https://reaperscans.com/comics/4921-demonic-emperor/chapters/64343350-chapter-324"
 
 	format!("{}/comics/{}/chapters/{}", base_url, manga_id, chapter_id)
+}
+
+pub fn get_date(time_ago: String) -> f64 {
+	let cleaned_time_ago = String::from(time_ago.replace("Released", "").replace("ago", "").trim());
+
+	let number = cleaned_time_ago
+		.split_whitespace()
+		.next()
+		.unwrap_or("")
+		.parse::<f64>()
+		.unwrap_or(0.0);
+
+	match cleaned_time_ago
+		.to_uppercase()
+		.split_whitespace()
+		.last()
+		.unwrap_or("")
+	{
+		"YEAR" | "YEARS" => current_date() - (number * 31556926.0),
+		"MONTH" | "MONTHS" => current_date() - (number * 2629743.0),
+		"WEEK" | "WEEKS" => current_date() - (number * 604800.0),
+		"DAY" | "DAYS" => current_date() - (number * 86400.0),
+		_ => current_date(),
+	}
 }
