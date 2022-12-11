93bee682a364ecef294cbb89d4de0884a271f9cc
KaBankz <20971850+KaBankz@users.noreply.github.com>
2022-12-11 11:28:11 -0500

    refactor(reaperscans): clean up get_manga_listing

--- src/rust/en.reaperscans/src/parser.rs
+++ src/rust/en.reaperscans/src/parser.rs
@@ -6,8 +6,8 @@ use aidoku::{
 	std::net::Request,
 	std::Vec,
 	std::{print, String},
-	Chapter, DeepLink, Filter, FilterType, Manga, MangaContentRating, MangaPageResult, MangaStatus,
-	MangaViewer, Page,
+	Chapter, DeepLink, Filter, FilterType, Listing, Manga, MangaContentRating, MangaPageResult,
+	MangaStatus, MangaViewer, Page,
 };
 
 use crate::helper::*;
@@ -30,6 +30,56 @@ pub fn parse_manga_list(
 		let id = get_manga_id(manga_node.select("a").attr("href").read());
 		let cover = manga_node.select("img").attr("src").read();
 		let title = String::from(manga_node.select("a.text-sm").text().read().trim());
+		let url = manga_node.select("a").attr("href").read();
+
+		mangas.push(Manga {
+			id,
+			cover,
+			title,
+			author: String::new(),
+			artist: String::new(),
+			description: String::new(),
+			url,
+			categories: Vec::new(),
+			status: MangaStatus::Unknown,
+			nsfw: MangaContentRating::Safe,
+			viewer: MangaViewer::Scroll,
+		});
+	}
+
+	let pagination = html.select("main div[wire:id] div nav span").text().read();
+	let has_more = pagination.contains("Next");
+
+	Ok(MangaPageResult {
+		manga: mangas,
+		has_more,
+	})
+}
+
+pub fn parse_manga_listing(
+	base_url: String,
+	listing: Listing,
+	page: i32,
+) -> Result<MangaPageResult> {
+	// The only alternate listing that reaper has is latest
+	let url = match listing.name.as_str() {
+		_ => format!("{}/latest/comics?page={}", base_url, page),
+	};
+
+	let html = Request::new(&url, HttpMethod::Get)
+		.html()
+		.expect("Failed to get html");
+
+	let mut mangas: Vec<Manga> = Vec::new();
+
+	for manga in html
+		.select("main div[wire:id] div.grid > div.relative")
+		.array()
+	{
+		let manga_node = manga.as_node().expect("Failed to get manga node");
+		let id = get_manga_id(manga_node.select("a").attr("href").read());
+		let cover = manga_node.select("img").attr("src").read();
+		let title = String::from(manga_node.select("p.text-sm a").text().read().trim());
 		let url = manga_node.select("a").attr("href").read();
 
 		mangas.push(Manga {
