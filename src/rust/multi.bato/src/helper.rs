785beee90ad4021d89fbd19ff8b64e3fd09fba7e
NMN <namanpandey23@gmail.com>
2022-12-23 03:56:22 -0700

    Removed helper functions from parser into helper.rs
    Removed helper functions for i32_to_string and urlencode from the end of parser.rs into helper.rs since the file now exists.

--- src/rust/multi.bato/src/helper.rs
+++ src/rust/multi.bato/src/helper.rs
@@ -1,4 +1,48 @@
-use aidoku::std::String;
+use aidoku::std::{String, Vec};
+
+pub fn i32_to_string(mut integer: i32) -> String {
+	if integer == 0 {
+		return String::from("0");
+	}
+	let mut string = String::with_capacity(11);
+	let pos = if integer < 0 {
+		string.insert(0, '-');
+		1
+	} else {
+		0
+	};
+	while integer != 0 {
+		let mut digit = integer % 10;
+		if pos == 1 {
+			digit *= -1;
+		}
+		string.insert(pos, char::from_u32((digit as u32) + ('0' as u32)).unwrap());
+		integer /= 10;
+	}
+	string
+}
+
+pub fn urlencode(string: String) -> String {
+	let mut result: Vec<u8> = Vec::with_capacity(string.len() * 3);
+	let hex = "0123456789abcdef".as_bytes();
+	let bytes = string.as_bytes();
+
+	for byte in bytes {
+		let curr = *byte;
+		if (b'a'..=b'z').contains(&curr)
+			|| (b'A'..=b'Z').contains(&curr)
+			|| (b'0'..=b'9').contains(&curr)
+		{
+			result.push(curr);
+		} else {
+			result.push(b'%');
+			result.push(hex[curr as usize >> 4]);
+			result.push(hex[curr as usize & 15]);
+		}
+	}
+
+	String::from_utf8(result).unwrap_or_default()
+}
 
 pub fn lang_encoder(lang: String) -> String {
 	let lang = match lang.to_lowercase().as_str() {
--- src/rust/multi.bato/src/parser.rs
+++ src/rust/multi.bato/src/parser.rs
@@ -6,7 +6,7 @@ use aidoku::{
 };
 
 use crate::crypto::batojs_decrypt;
-use crate::helper::lang_encoder;
+use crate::helper::{i32_to_string, lang_encoder, urlencode};
 use crate::substring::Substring;
 // use alloc::string::String;
 
@@ -329,50 +329,4 @@ pub fn parse_incoming_url(url: String) -> String {
 	}
 
 	manga_id
-}
-
-// HELPER FUNCTIONS
-
-pub fn i32_to_string(mut integer: i32) -> String {
-	if integer == 0 {
-		return String::from("0");
-	}
-	let mut string = String::with_capacity(11);
-	let pos = if integer < 0 {
-		string.insert(0, '-');
-		1
-	} else {
-		0
-	};
-	while integer != 0 {
-		let mut digit = integer % 10;
-		if pos == 1 {
-			digit *= -1;
-		}
-		string.insert(pos, char::from_u32((digit as u32) + ('0' as u32)).unwrap());
-		integer /= 10;
-	}
-	string
-}
-
-pub fn urlencode(string: String) -> String {
-	let mut result: Vec<u8> = Vec::with_capacity(string.len() * 3);
-	let hex = "0123456789abcdef".as_bytes();
-	let bytes = string.as_bytes();
-
-	for byte in bytes {
-		let curr = *byte;
-		if (b'a'..=b'z').contains(&curr)
-			|| (b'A'..=b'Z').contains(&curr)
-			|| (b'0'..=b'9').contains(&curr)
-		{
-			result.push(curr);
-		} else {
-			result.push(b'%');
-			result.push(hex[curr as usize >> 4]);
-			result.push(hex[curr as usize & 15]);
-		}
-	}
-
-	String::from_utf8(result).unwrap_or_default()
 }
