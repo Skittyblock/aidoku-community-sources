c27b5ab07c5341404665efaf5bed9471f81b8c49
Deepak Patil <dspatil11599@gmail.com>
2022-12-31 13:05:58 +0530

    feat: pr changes

--- src/rust/multi.comick/build.sh
+++ src/rust/multi.comick/build.sh
@@ -4,3 +4,4 @@ cp res/* target/wasm32-unknown-unknown/release/Payload
 cp target/wasm32-unknown-unknown/release/*.wasm target/wasm32-unknown-unknown/release/Payload/main.wasm
 cd target/wasm32-unknown-unknown/release ; zip -r package.aix Payload
 mv package.aix ../../../package.aix
+aidoku serve -p 9000 ../../../package.aix
