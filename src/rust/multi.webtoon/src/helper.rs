3202134e3933e2cd1d50ee523e653935df1411e4
KaBankz <20971850+KaBankz@users.noreply.github.com>
2023-01-15 17:34:00 -0500

    feat(webtoon)!: complete rewrite of webtoon

---
+++ src/rust/multi.webtoon/src/helper.rs
@@ -0,0 +1,193 @@
+use aidoku::{
+	prelude::format,
+	std::defaults::defaults_get,
+	std::net::{HttpMethod, Request},
+	std::{String, Vec},
+	Filter, FilterType,
+};
+
+/// Returns the correct url for the selected language
+pub fn get_base_url(mobile: bool) -> String {
+	let desktop_base_url = "https://www.webtoons.com";
+	let mobile_base_url = "https://m.webtoons.com";
+	let lang = get_lang_code().unwrap_or(String::from("en"));
+
+	if mobile {
+		format!("{}/{}", mobile_base_url, lang)
+	} else {
+		format!("{}/{}", desktop_base_url, lang)
+	}
+}
+
+/// Returns the currently selected language
+pub fn get_lang_code() -> Option<String> {
+	if let Ok(languages) = defaults_get("languages")
+		.expect("Could not read language")
+		.as_array()
+	{
+		if let Ok(language) = languages.get(0).as_string() {
+			return Some(language.read());
+		}
+	}
+	None
+}
+
+/// Returns a useragent string
+pub fn get_user_agent(mobile: bool) -> String {
+	if mobile {
+		String::from("Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Mobile/15E148 Safari/604.1")
+	} else {
+		String::from("Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15")
+	}
+}
+
+/// Request wrapper to set cookies for each request
+pub fn request(url: &String, mobile: bool) -> Request {
+	let locale = get_lang_code().unwrap_or(String::from("en"));
+	let age_gate_pass = true;
+	let need_gdpr = true;
+	let need_ccpa = true;
+	let need_coppa = false;
+	let user_agent = get_user_agent(mobile);
+
+	let cookie_string = format!(
+		"locale={}; ageGatePass={}; needGDPR={}; needCCPA={}; needCOPPA={}",
+		locale, age_gate_pass, need_gdpr, need_ccpa, need_coppa
+	);
+
+	Request::new(url, HttpMethod::Get)
+		.header("Referer", &get_base_url(false))
+		.header("Cookie", &cookie_string)
+		.header("User-Agent", &user_agent)
+}
+
+/// Percent-encode any non-ASCII characters in a string.
+pub fn urlencode(string: String) -> String {
+	let mut result: Vec<u8> = Vec::with_capacity(string.len() * 3);
+	let hex = "0123456789ABCDEF".as_bytes();
+	let bytes = string.as_bytes();
+
+	for byte in bytes {
+		let curr = *byte;
+		if curr.is_ascii_alphanumeric() {
+			result.push(curr);
+		} else {
+			result.push(b'%');
+			result.push(hex[curr as usize >> 4]);
+			result.push(hex[curr as usize & 15]);
+		}
+	}
+	String::from_utf8(result).unwrap_or_default()
+}
+
+/// Returns the ID of a manga from a URL.
+// *NOTE: This was written by GitHub Copilot.
+pub fn get_manga_id(url: String) -> String {
+	// Example Url: https://www.webtoons.com/en/action/ultra-alternate-character/list?title_no=3581
+	// Example Url: https://www.webtoons.com/episodeList?titleNo=3581
+	// parse "3581" from the url
+
+	if url.contains("title_no") || url.contains("titleNo") {
+		let split_url = {
+			if url.contains("title_no") {
+				url.split("title_no=").collect::<Vec<&str>>()
+			} else if url.contains("titleNo") {
+				url.split("titleNo=").collect::<Vec<&str>>()
+			} else {
+				Vec::new()
+			}
+		};
+
+		if !split_url.is_empty() {
+			let manga_id = split_url[1];
+			String::from(manga_id)
+		} else {
+			String::new()
+		}
+	} else {
+		String::new()
+	}
+}
+
+/// Returns the ID of a chapter from a URL.
+// *NOTE: This was written by GitHub Copilot.
+pub fn get_chapter_id(url: String) -> String {
+	// Example Url: https://www.webtoons.com/en/action/ultra-alternate-character/ep-1-healer-servant/viewer?title_no=3581&episode_no=1
+	// Example Url: https://www.webtoons.com/viewer?titleNo=3581&episodeNo=1
+	// parse "1" from the url
+
+	if url.contains("episode_no") || url.contains("episodeNo") {
+		let split_url = {
+			if url.contains("episode_no") {
+				url.split("episode_no=").collect::<Vec<&str>>()
+			} else if url.contains("episodeNo") {
+				url.split("episodeNo=").collect::<Vec<&str>>()
+			} else {
+				Vec::new()
+			}
+		};
+
+		if !split_url.is_empty() {
+			let chapter_id = split_url[1];
+			String::from(chapter_id)
+		} else {
+			String::new()
+		}
+	} else {
+		String::new()
+	}
+}
+
+/// Returns full URL of a manga from a manga ID.
+// *NOTE: This was written by GitHub Copilot.
+pub fn get_manga_url(manga_id: String, base_url: String) -> String {
+	// Example manga id: 3581
+	// return "https://www.webtoons.com/episodeList?titleNo=3581"
+
+	// Removing the language tag from the url, because it is not required
+	let mut split_url = base_url.split('/').collect::<Vec<&str>>();
+	split_url.pop();
+
+	let base_url = split_url.join("/");
+
+	format!("{}/episodeList?titleNo={}", base_url, manga_id)
+}
+
+/// Returns full URL of a chapter from a chapter ID and manga ID.
+// *NOTE: This was written by GitHub Copilot.
+pub fn get_chapter_url(chapter_id: String, manga_id: String, base_url: String) -> String {
+	// Example chapter id: 1
+	// Example manga id: 3581
+	// return "https://www.webtoons.com/viewer?titleNo=3581&episodeNo=1"
+
+	// Removing the language tag from the url, because it is not required
+	let mut split_url = base_url.split('/').collect::<Vec<&str>>();
+	split_url.pop();
+
+	let base_url = split_url.join("/");
+
+	format!(
+		"{}/viewer?titleNo={}&episodeNo={}",
+		base_url, manga_id, chapter_id
+	)
+}
+
+/// Returns the search status as a boolean and the search string if there is one
+pub fn check_for_search(filters: Vec<Filter>) -> (String, bool) {
+	let mut search_string = String::new();
+	let mut search = false;
+
+	for filter in filters {
+		match filter.kind {
+			FilterType::Title => {
+				if let Ok(filter_value) = filter.value.as_string() {
+					search_string.push_str(urlencode(filter_value.read().to_lowercase()).as_str());
+					search = true;
+					break;
+				}
+			}
+			_ => continue,
+		}
+	}
+	(search_string, search)
+}
---
+++ src/rust/multi.webtoon/src/parser.rs
@@ -0,0 +1,343 @@
+use aidoku::{
+	error::Result,
+	prelude::format,
+	std::defaults::defaults_get,
+	std::net::Request,
+	std::{String, Vec},
+	Chapter, DeepLink, Filter, Listing, Manga, MangaContentRating, MangaPageResult, MangaStatus,
+	MangaViewer, Page,
+};
+
+use crate::{get_manga_details, helper::*};
+
+pub fn parse_manga_list(base_url: String, filters: Vec<Filter>) -> Result<MangaPageResult> {
+	let (query, search) = check_for_search(filters);
+
+	let url = {
+		if search {
+			format!("{}/search?keyword={}", base_url, query)
+		} else {
+			// This is to handle parse_manga_listing as it passes in full a url,
+			// not just the base
+			if base_url.contains("genre") {
+				base_url
+			} else {
+				format!("{}/genre", base_url)
+			}
+		}
+	};
+
+	let html = request(&url, false).html().expect("Failed to get html");
+
+	let mut mangas: Vec<Manga> = Vec::new();
+
+	for manga in html
+		.select("#content > div.card_wrap > ul > li > a")
+		.array()
+	{
+		let manga_node = manga.as_node().expect("Failed to get manga node");
+		let id = get_manga_id(manga_node.attr("href").read());
+		let cover = manga_node.select("img").attr("src").read();
+		let title = manga_node.select(".info > .subj").text().read();
+
+		let author_artist = manga_node.select(".info > .author").text().read();
+		let author_artist = author_artist.split('/').collect::<Vec<&str>>();
+
+		let author = String::from(author_artist[0].trim());
+		let mut artist = String::new();
+
+		if author_artist.len() > 1 {
+			artist = String::from(author_artist[1].trim());
+		}
+
+		let url = manga_node.attr("href").read();
+
+		mangas.push(Manga {
+			id,
+			cover,
+			title,
+			author,
+			artist,
+			description: String::new(),
+			url,
+			categories: Vec::new(),
+			status: MangaStatus::Unknown,
+			nsfw: MangaContentRating::Safe,
+			viewer: MangaViewer::Scroll,
+		});
+	}
+
+	Ok(MangaPageResult {
+		manga: mangas,
+		has_more: false,
+	})
+}
+
+pub fn parse_manga_listing(base_url: String, listing: Listing) -> Result<MangaPageResult> {
+	let url = {
+		match listing.name.as_str() {
+			"Latest" => format!("{}/genre?sortOrder=UPDATE", base_url),
+			"Popular" => format!("{}/genre?sortOrder=READ_COUNT", base_url),
+			"Top" => format!("{}/genre?sortOrder=LIKEIT", base_url),
+			_ => format!("{}/genre", base_url),
+		}
+	};
+
+	parse_manga_list(url, Vec::new())
+}
+
+pub fn parse_manga_details(base_url: String, manga_id: String) -> Result<Manga> {
+	let url = get_manga_url(manga_id.clone(), base_url);
+
+	let html = request(&url, false).html().expect("Failed to get html");
+
+	let cover = {
+		// Cover is set as background image using inline css
+		// Example: background:#fff url(https://webtoon-phinf.pstatic.net/20211111_3/1636569655006lMyqV_JPEG/6UltraAlternateCharacter_desktop_thumbnail.jpg?type=a306) no-repeat 100% 100%;background-size:306px
+		// This parses the styles and gets the image url
+		let style_attr = html
+			.select("#content > div.cont_box > div.detail_body.banner")
+			.attr("style")
+			.read();
+
+		let split_styles = style_attr.split(['(', ')']).collect::<Vec<&str>>();
+		String::from(split_styles[1])
+	};
+
+	let title = html
+		.select("#content > div.cont_box > div.detail_header > div.info > .subj")
+		.text()
+		.read();
+
+	let author_artist = html
+		.select("#content > div.cont_box > div.detail_header > div.info > .author_area")
+		.text()
+		.read()
+		.replace("author info", "");
+	let author_artist = author_artist.split(',').collect::<Vec<&str>>();
+
+	let author = String::from(author_artist[0].trim());
+	let mut artist = String::new();
+
+	if author_artist.len() > 1 {
+		artist = String::from(author_artist[1].trim());
+	}
+
+	let description = html.select("#_asideDetail > .summary").text().read();
+
+	let status = {
+		let status_text = html
+			.select("#_asideDetail > .day_info")
+			.text()
+			.read()
+			.to_lowercase();
+
+		let series_note = html
+			.select("#content > div.cont_box > div.detail_body div.detail_paywall")
+			.text()
+			.read()
+			.to_lowercase();
+
+		// Even if a series is on hiatus it will have "every x" in the status text
+		// So we have to check the series note for hiatus before checking for ongoing
+		if status_text.contains("completed") {
+			MangaStatus::Completed
+		} else if series_note.contains("will return") {
+			MangaStatus::Hiatus
+		} else if status_text.contains("every") {
+			MangaStatus::Ongoing
+		} else {
+			MangaStatus::Unknown
+		}
+	};
+
+	let categories = [html
+		.select("#content > div.cont_box > div.detail_header > div.info > .genre")
+		.text()
+		.read()]
+	.to_vec();
+
+	Ok(Manga {
+		id: manga_id,
+		cover,
+		title,
+		author,
+		artist,
+		description,
+		url,
+		categories,
+		status,
+		nsfw: MangaContentRating::Safe,
+		viewer: MangaViewer::Scroll,
+	})
+}
+
+pub fn parse_chapter_list(base_url: String, manga_id: String) -> Result<Vec<Chapter>> {
+	let url = get_manga_url(manga_id, base_url);
+
+	let html = request(&url, true).html().expect("Failed to get html");
+
+	let mut chapters: Vec<Chapter> = Vec::new();
+
+	for chapter in html.select("._episodeItem").array() {
+		let chapter_node = chapter.as_node().expect("Failed to get chapter node");
+
+		let mut volume = -1.0;
+
+		let title = {
+			let raw_title = chapter_node.select(".sub_title > .ellipsis").text().read();
+			let mut title = raw_title.split_whitespace().collect::<Vec<&str>>();
+
+			// Remove leading volume text and set volume accordingly
+			// This is for titles like "(S1) Chapter 1 - PeePeePooPoo"
+			// or for titles like "(T1) Chapter 1 - PeePeePooPoo"
+			if !title.is_empty() {
+				let title_chars = title[0].chars().collect::<Vec<char>>();
+
+				if title_chars.len() >= 2
+					&& (title_chars[1] == 'S' || title_chars[1] == 'T')
+					&& String::from(title_chars[2]).parse::<f64>().is_ok()
+				{
+					volume = String::from(title_chars[2]).parse::<f32>().unwrap_or(-1.0);
+					title.remove(0);
+				}
+
+				// Remove leading episode text
+				// This is for titles like "Ep.1 - PeePeePooPoo"
+				if title_chars.len() >= 3
+					&& (title_chars[0] == 'E'
+						&& (title_chars[1] == 'p' || title_chars[1] == 'P')
+						&& title_chars[2] == '.')
+					&& title_chars[3..]
+						.iter()
+						.collect::<String>()
+						.parse::<f64>()
+						.is_ok()
+				{
+					title.remove(0);
+				}
+			}
+
+			// Remove leading season text and set volume accordingly
+			// This is for titles like "[Season 1] Chapter 1 - PeePeePooPoo"
+			if title.len() >= 2
+				&& (title[0] == "[Season")
+				&& title[1].replace(']', "").parse::<f64>().is_ok()
+			{
+				volume = title[1].replace(']', "").parse::<f32>().unwrap_or(-1.0);
+				title.remove(0);
+				title.remove(0);
+			}
+
+			// Remove leading chapter/episode text
+			if title.len() >= 2
+				&& (title[0] == "Chapter"
+					|| title[0] == "Episode"
+					|| title[0] == "Ch." || title[0] == "CH."
+					|| title[0] == "Ep." || title[0] == "EP"
+					|| title[0] == "EP.")
+				&& title[1].replace(':', "").parse::<f64>().is_ok()
+			{
+				title.remove(0);
+				title.remove(0);
+			}
+
+			// Remove leading symbols
+			if !title.is_empty() && (title[0] == "-" || title[0] == ":") {
+				title.remove(0);
+			}
+
+			title.join(" ")
+		};
+
+		let chapter_number = {
+			let raw_chapter_number = chapter_node.attr("data-episode-no").read();
+			raw_chapter_number
+				.parse::<f32>()
+				.expect("Failed to parse chapter number")
+		};
+
+		let chapter_id = get_chapter_id(chapter_node.select("a").attr("href").read());
+		let chapter_url = chapter_node.select("a").attr("href").read();
+
+		let lang = get_lang_code().unwrap_or(String::from("en"));
+
+		let date_updated = {
+			let date = chapter_node.select(".sub_info > .date");
+
+			match lang.as_str() {
+				"en" => date.text().as_date("MMM dd, yyyy", Some("en-US"), None),
+				"zh-hant" => date.text().as_date("yyyy/mm/dd", Some("zh-Hant"), None),
+				// FIXME: Can't parse thai dates
+				"th" => date.text().as_date("dd MM yyyy", Some("th-TH"), None),
+				"id" => date.text().as_date("yyyy MMM dd", Some("id-ID"), None),
+				"es" => date.text().as_date("dd-MMM-yyyy", Some("es-ES"), None),
+				"fr" => date.text().as_date("dd MMM yyyy", Some("fr-FR"), None),
+				"de" => date.text().as_date("dd.mm.yyyy", Some("de-DE"), None),
+				_ => date.text().as_date("MMM dd, yyyy", None, None),
+			}
+		};
+
+		chapters.push(Chapter {
+			id: chapter_id,
+			title,
+			volume,
+			chapter: chapter_number,
+			date_updated,
+			scanlator: String::new(),
+			url: chapter_url,
+			lang,
+		});
+	}
+
+	Ok(chapters)
+}
+
+pub fn parse_page_list(
+	base_url: String,
+	manga_id: String,
+	chapter_id: String,
+) -> Result<Vec<Page>> {
+	let url = get_chapter_url(chapter_id, manga_id, base_url);
+
+	let html = request(&url, false).html().expect("Failed to get html");
+
+	let mut pages: Vec<Page> = Vec::new();
+
+	// Optional pages contain "?type=opti" at the end of the url
+	let optional_pages = defaults_get("optionalPages")?.as_bool().unwrap_or(true);
+
+	for (index, page) in html.select("div#_imageList > img").array().enumerate() {
+		let page_node = page.as_node().expect("Failed to get page node");
+
+		let url = page_node.attr("data-url").read();
+
+		// Skip optional pages if optionalPages is false
+		if url.ends_with("?type=opti") && !optional_pages {
+			continue;
+		}
+
+		pages.push(Page {
+			index: index as i32,
+			url,
+			base64: String::new(),
+			text: String::new(),
+		});
+	}
+
+	Ok(pages)
+}
+
+pub fn modify_image_request(base_url: String, request: Request) {
+	request.header("Referer", &base_url);
+}
+
+pub fn handle_url(url: String) -> Result<DeepLink> {
+	let manga_id = get_manga_id(url);
+	let parsed = get_manga_details(manga_id);
+
+	Ok(DeepLink {
+		manga: parsed.ok(),
+		..Default::default()
+	})
+}
