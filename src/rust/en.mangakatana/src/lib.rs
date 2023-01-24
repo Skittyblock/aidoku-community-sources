1bd585f7a5bd8226f1abfaf5d8fb4494975f3642
KaBankz <20971850+KaBankz@users.noreply.github.com>
2023-01-23 19:30:42 -0500

    feat(mangakatana): implement filtering

--- src/rust/en.mangakatana/src/lib.rs
+++ src/rust/en.mangakatana/src/lib.rs
@@ -8,17 +8,103 @@ use aidoku::{
 	prelude::*,
 	std::net::{HttpMethod, Request},
 	std::{String, Vec},
-	Chapter, DeepLink, Filter, Listing, Manga, MangaPageResult, Page,
+	Chapter, DeepLink, Filter, FilterType, Listing, Manga, MangaPageResult, Page,
 };
 
-use helper::{get_chapter_id, get_chapter_url, get_manga_id, get_manga_url};
+use helper::*;
 use parser::*;
 
 const URL: &str = "https://mangakatana.com";
 
 #[get_manga_list]
 fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
-	let url = format!("{}/manga/page/{}", URL, page);
+	// filter=1 enables filtering
+	let mut url = format!("{}/manga/page/{}?filter=1", URL, page);
+
+	let mut searching = false;
+	let mut include_generes = Vec::new();
+	let mut exclude_generes = Vec::new();
+
+	for filter in filters {
+		match filter.kind {
+			FilterType::Title => {
+				if let Ok(value) = filter.value.as_string() {
+					searching = true;
+					let query = urlencode(value.read());
+					url = format!("{}/page/{}?search={}&search_by=book_name", URL, page, query);
+				}
+			}
+			FilterType::Author => {
+				if let Ok(value) = filter.value.as_string() {
+					searching = true;
+					let query = urlencode(value.read());
+					url = format!("{}/page/{}?search={}&search_by=author", URL, page, query);
+				}
+			}
+			FilterType::Genre => {
+				if let Ok(id) = filter.object.get("id").as_string() {
+					match filter.value.as_int().unwrap_or(-1) {
+						0 => exclude_generes.push(id.read()),
+						1 => include_generes.push(id.read()),
+						_ => continue,
+					}
+				}
+			}
+			FilterType::Select => match filter.name.as_str() {
+				"Status" => match filter.value.as_int().unwrap_or(-1) {
+					0 => continue,
+					1 => url.push_str("&status=1"),
+					2 => url.push_str("&status=2"),
+					3 => url.push_str("&status=0"),
+					_ => continue,
+				},
+				"Genre Inclusion Mode" => match filter.value.as_int().unwrap_or(-1) {
+					0 => url.push_str("&include_mode=and"),
+					1 => url.push_str("&include_mode=or"),
+					_ => url.push_str("&include_mode=and"),
+				},
+				"Chapter Count" => match filter.value.as_int().unwrap_or(-1) {
+					0 => url.push_str("&chapters=e1"),
+					1 => url.push_str("&chapters=1"),
+					2 => url.push_str("&chapters=5"),
+					3 => url.push_str("&chapters=10"),
+					4 => url.push_str("&chapters=20"),
+					5 => url.push_str("&chapters=30"),
+					6 => url.push_str("&chapters=50"),
+					7 => url.push_str("&chapters=100"),
+					8 => url.push_str("&chapters=150"),
+					9 => url.push_str("&chapters=200"),
+					_ => url.push_str("&chapters=1"),
+				},
+				_ => continue,
+			},
+			FilterType::Sort => {
+				let value = match filter.value.as_object() {
+					Ok(value) => value,
+					Err(_) => continue,
+				};
+				let index = value.get("index").as_int().unwrap_or(0);
+				url.push_str(match index {
+					0 => "&order=az",
+					1 => "&order=latest",
+					2 => "&order=new",
+					3 => "&order=numc",
+					_ => "&order=latest",
+				});
+			}
+			_ => continue,
+		}
+	}
+
+	if !include_generes.is_empty() && !searching {
+		url.push_str("&include=");
+		url.push_str(&include_generes.join("_"));
+	}
+
+	if !exclude_generes.is_empty() && !searching {
+		url.push_str("&exclude=");
+		url.push_str(&exclude_generes.join("_"));
+	}
 
 	let html = Request::new(url, HttpMethod::Get)
 		.html()
@@ -29,6 +115,7 @@ fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
 
 #[get_manga_listing]
 fn get_manga_listing(_listing: Listing, page: i32) -> Result<MangaPageResult> {
+	// No listings exist on mangakatana it's all done through filters
 	get_manga_list(Vec::new(), page)
 }
 
