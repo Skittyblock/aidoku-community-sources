13e5882713fa563f63c69226a2b6658b995d3e2c
NMN <56101163+pandeynmn@users.noreply.github.com>
2023-03-06 00:06:07 -0700

    remove unnecessary  newline

--- src/rust/madara/template/src/template.rs
+++ src/rust/madara/template/src/template.rs
@@ -208,7 +208,6 @@ pub fn get_search_result(data: MadaraSiteData, url: String) -> Result<MangaPageR
 	for item in html.select(data.search_selector.as_str()).array() {
 		let obj = item.as_node();
 
-
 		let id = obj
 			.select("a")
 			.attr("href")
