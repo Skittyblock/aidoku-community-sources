b5db5c0b25560f1e75fb6db716df22344f7c8576
Aphronyx <aphronyx.su@gmail.com>
2023-05-13 15:40:48 +0800

    refactor(baozimanhua): improve code readability

--- src/rust/zh.baozimanhua/src/lib.rs
+++ src/rust/zh.baozimanhua/src/lib.rs
@@ -11,26 +11,28 @@ use parser::{API_URL, BASE_URL, USER_AGENT};
 
 #[get_manga_list]
 fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
-	let mut url = String::new();
-	parser::get_filtered_url(filters, page, &mut url);
+	let url = parser::get_filtered_url(filters, page);
 
-	let request = parser::request_get(url.clone());
 	if url.contains(API_URL) {
-		return parser::parse_home_page(request.json()?);
+		let json = parser::request_get(url).json()?;
+		return parser::parse_home_page(json);
 	}
-	parser::parse_search_page(request.html()?)
+	let html = parser::request_get(url).html()?;
+	parser::parse_search_page(html)
 }
 
 #[get_manga_details]
 fn get_manga_details(manga_id: String) -> Result<Manga> {
 	let url = format!("{}/comic/{}", BASE_URL, manga_id);
-	parser::get_manga_details(parser::request_get(url).html()?, manga_id)
+	let html = parser::request_get(url).html()?;
+	parser::get_manga_details(html, manga_id)
 }
 
 #[get_chapter_list]
 fn get_chapter_list(manga_id: String) -> Result<Vec<Chapter>> {
 	let url = format!("{}/comic/{}", BASE_URL, manga_id);
-	parser::get_chapter_list(parser::request_get(url).html()?, manga_id)
+	let html = parser::request_get(url).html()?;
+	parser::get_chapter_list(html, manga_id)
 }
 
 #[get_page_list]
@@ -39,7 +41,8 @@ fn get_page_list(manga_id: String, chapter_id: String) -> Result<Vec<Page>> {
 		"{}/comic/chapter/{}/0_{}.html",
 		BASE_URL, manga_id, chapter_id
 	);
-	parser::get_page_list(parser::request_get(url).html()?)
+	let html = parser::request_get(url).html()?;
+	parser::get_page_list(html)
 }
 
 #[modify_image_request]
@@ -51,8 +54,7 @@ fn modify_image_request(request: Request) {
 
 #[handle_url]
 fn handle_url(url: String) -> Result<DeepLink> {
-	let deep_link = url;
-	let (manga_id, chapter_id) = parser::parse_deep_link(deep_link);
+	let (manga_id, chapter_id) = parser::parse_deep_link(url);
 
 	let manga = (!manga_id.is_empty()).then(|| get_manga_details(manga_id).expect("Manga"));
 	let chapter = (!chapter_id.is_empty()).then(|| Chapter {
