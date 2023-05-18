dc20900bbee41132c2ecb1bb37b64eb9a1c80ebb
Aphronyx <aphronyx.su@gmail.com>
2023-05-18 18:55:13 +0800

    feat(myreadingmanga): create initial code

---
+++ src/rust/multi.myreadingmanga/src/lib.rs
@@ -0,0 +1,44 @@
+#![no_std]
+use aidoku::{
+	error::Result,
+	prelude::*,
+	std::{net::Request, String, Vec},
+	Chapter, DeepLink, Filter, Listing, Manga, MangaPageResult, Page,
+};
+
+mod parser;
+
+#[get_manga_list]
+fn get_manga_list(_filters: Vec<Filter>, _page: i32) -> Result<MangaPageResult> {
+	todo!()
+}
+
+#[get_manga_listing]
+fn get_manga_listing(_listing: Listing, _page: i32) -> Result<MangaPageResult> {
+	todo!()
+}
+
+#[get_manga_details]
+fn get_manga_details(_id: String) -> Result<Manga> {
+	todo!()
+}
+
+#[get_chapter_list]
+fn get_chapter_list(_id: String) -> Result<Vec<Chapter>> {
+	todo!()
+}
+
+#[get_page_list]
+fn get_page_list(_manga_id: String, _chapter_id: String) -> Result<Vec<Page>> {
+	todo!()
+}
+
+#[modify_image_request]
+fn modify_image_request(_request: Request) {
+	todo!()
+}
+
+#[handle_url]
+fn handle_url(_url: String) -> Result<DeepLink> {
+	todo!()
+}
