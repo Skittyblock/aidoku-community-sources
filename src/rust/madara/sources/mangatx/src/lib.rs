1d8f19ea97de0bcde5c938210454861b4cf266d4
Naufal <naufaljct48@gmail.com>
2023-07-17 01:02:32 +0700

    Fix: MangaTX (#399)

--- src/rust/madara/sources/mangatx/src/lib.rs
+++ src/rust/madara/sources/mangatx/src/lib.rs
@@ -8,6 +8,7 @@ use madara_template::template;
 fn get_data() -> template::MadaraSiteData {
 	let data: template::MadaraSiteData = template::MadaraSiteData {
 		base_url: String::from("https://mangatx.com"),
+		alt_ajax: true,
 		..Default::default()
 	};
 	data
