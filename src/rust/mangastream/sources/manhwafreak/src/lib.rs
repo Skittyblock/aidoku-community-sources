ef9e6ae457de781dfde46ba183066a3ace4c850d
KaBankz <20971850+KaBankz@users.noreply.github.com>
2023-07-21 18:24:08 +0600

    fix(manhwafreak): update selectors
    Closes #327

--- src/rust/mangastream/sources/manhwafreak/src/lib.rs
+++ src/rust/mangastream/sources/manhwafreak/src/lib.rs
@@ -9,6 +9,17 @@ use mangastream_template::template::MangaStreamSource;
 fn get_instance() -> MangaStreamSource {
 	MangaStreamSource {
 		base_url: String::from("https://manhwafreak.com"),
+		manga_selector: ".listupd .lastest-serie",
+		manga_details_title: ".wrapper .series h1.title",
+		manga_details_artist: ".wrapper .series #info div:contains(Artist) > p+p",
+		manga_details_author: ".wrapper .series #info div:contains(Author) > p+p",
+		manga_details_status: ".wrapper .series #info div:contains(Status) > p+p",
+		manga_details_description: ".wrapper .series #summary",
+		manga_details_categories: ".wrapper .series #info div:contains(Genre) > p+p",
+		manga_details_type: ".wrapper .series #info div:contains(Type) > p+p",
+		chapter_selector: ".chapter-li > a",
+		chapter_title: ".chapter-info > p:first-child",
+		chapter_date: ".chapter-info > p:nth-child(2)",
 		alt_pages: true,
 		..Default::default()
 	}
