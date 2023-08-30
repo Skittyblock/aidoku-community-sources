3521ccacbf1e83a6eb391babc2d104235dc19f06
KaBanks <20971850+KaBankz@users.noreply.github.com>
2023-08-29 20:35:33 -0400

    fix(setsuscans): remove get_manga_id override (#432)
    * fix(setsuscans): remove get_manga_id override
    
    * chore(madara): fix formatting

--- src/rust/madara/sources/setsuscans/src/lib.rs
+++ src/rust/madara/sources/setsuscans/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 use aidoku::{
-	error::Result, prelude::*, std::net::HttpMethod, std::net::Request, std::String, std::Vec,
-	Chapter, DeepLink, Filter, Listing, Manga, MangaPageResult, Page,
+	error::Result, prelude::*, std::String, std::Vec, Chapter, DeepLink, Filter, Listing, Manga,
+	MangaPageResult, Page,
 };
 
 use madara_template::template;
@@ -10,35 +10,10 @@ fn get_data() -> template::MadaraSiteData {
 	let data: template::MadaraSiteData = template::MadaraSiteData {
 		base_url: String::from("https://setsuscans.com"),
 		description_selector: String::from("div.summary_content_wrap div p"),
-		get_manga_id,
 		alt_ajax: true,
 		..Default::default()
 	};
 	data
-}
-
-// SetsuScans keeps its manga data in its src attribute as a base64 encoded
-// string so this function overrides the default get_manga_id function to handle
-// SetsuScans
-fn get_manga_id(manga_id: String, base_url: String, path: String) -> String {
-	let url = base_url + "/" + path.as_str() + "/" + manga_id.as_str();
-	if let Ok(html) = Request::new(url.as_str(), HttpMethod::Get).html() {
-		let id_html = html.select("script#wp-manga-js-extra").attr("src").read();
-		let id_html = id_html.replace("data:text/javascript;base64,", "");
-		let decoded_html = base64::decode(id_html).expect("Failed to decode base64");
-		let decoded_html =
-			String::from_utf8(decoded_html).expect("Failed to convert base64 to utf8");
-		let id = &decoded_html[decoded_html
-			.find("manga_id")
-			.expect("Failed to find manga_id")
-			+ 11
-			..decoded_html
-				.find("\"}")
-				.expect("Failed to find end of manga_id")];
-		String::from(id)
-	} else {
-		String::new()
-	}
 }
 
 #[get_manga_list]
