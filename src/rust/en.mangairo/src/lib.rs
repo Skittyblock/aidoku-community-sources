fbc9db438d8abe06f04fee01f0762c94cfe39c37
getBoolean <getboolean@gmail.com>
2023-09-04 03:22:29 -0500

    Add boilerplate parser for mangaairo

--- src/rust/en.mangairo/src/lib.rs
+++ src/rust/en.mangairo/src/lib.rs
@@ -6,42 +6,84 @@ use aidoku::{
 	Chapter, DeepLink, Filter, Listing, Manga, MangaPageResult, Page,
 };
 
-#[initialize]
-fn initialize() {
-	todo!()
-}
+mod parser;
+use parser::{BASE_URL, USER_AGENT};
 
 #[get_manga_list]
-fn get_manga_list(_filters: Vec<Filter>, _page: i32) -> Result<MangaPageResult> {
-	todo!()
-}
+fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
+	let mut result: Vec<Manga> = Vec::new();
 
-#[get_manga_listing]
-fn get_manga_listing(_listing: Listing, _page: i32) -> Result<MangaPageResult> {
-	todo!()
+	let mut url = String::new();
+	parser::get_filtered_url(filters, page, &mut url);
+	let html = Request::new(url.as_str(), HttpMethod::Get).html()?;
+
+	if url.contains("search") {
+		parser::parse_search(html, &mut result);
+	} else {
+		parser::parse_recents(html, &mut result);
+	}
+
+	if result.len() >= 50 {
+		Ok(MangaPageResult {
+			manga: result,
+			has_more: true,
+		})
+	} else {
+		Ok(MangaPageResult {
+			manga: result,
+			has_more: false,
+		})
+	}
 }
 
 #[get_manga_details]
-fn get_manga_details(_id: String) -> Result<Manga> {
-	todo!()
+fn get_manga_details(manga_id: String) -> Result<Manga> {
+	let url = format!("https://w.mangairo.com/{}", &manga_id);
+	let html = Request::new(url.as_str(), HttpMethod::Get).html()?;
+	parser::parse_manga(html, manga_id)
+}
+
+#[get_manga_listing]
+fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
+	let url = match listing.name.as_str() {
+		"Latest" => format!("{BASE_URL}/manga-list/type-latest/ctg-all/state-all/page-{page}"),
+		"Hot" => format!("{BASE_URL}/manga-list/type-topview/ctg-all/state-all/page-{page}"),
+		// TODO: Make "Completed" a filter
+		"Completed" => {
+			format!("{BASE_URL}/manga-list/type-latest/ctg-all/state-completed/page-{page}")
+		}
+		_ => String::from(BASE_URL),
+	};
+	parser::parse_manga_listing(url)
 }
 
 #[get_chapter_list]
-fn get_chapter_list(_id: String) -> Result<Vec<Chapter>> {
-	todo!()
+fn get_chapter_list(manga_id: String) -> Result<Vec<Chapter>> {
+	let url = format!("https://w.mangairo.com/{}", &manga_id);
+	let html = Request::new(url.as_str(), HttpMethod::Get).html()?;
+	parser::get_chapter_list(html)
 }
 
 #[get_page_list]
-fn get_page_list(_chapter_id: String, _manga_id: String) -> Result<Vec<Page>> {
-	todo!()
+fn get_page_list(chapter_id: String, manga_id: String) -> Result<Vec<Page>> {
+	let url = format!("https://w.mangairo.com/{}/{}", &manga_id, &chapter_id);
+	let html = Request::new(url.as_str(), HttpMethod::Get).html()?;
+	parser::get_page_list(html)
 }
 
 #[modify_image_request]
-fn modify_image_request(_request: Request) {
-	todo!()
+fn modify_image_request(request: Request) {
+	request
+		.header("Referer", BASE_URL)
+		.header("User-Agent", USER_AGENT);
 }
 
 #[handle_url]
-fn handle_url(_url: String) -> Result<DeepLink> {
-	todo!()
+fn handle_url(url: String) -> Result<DeepLink> {
+	let parsed_manga_id = parser::parse_incoming_url(url);
+
+	Ok(DeepLink {
+		manga: Some(get_manga_details(parsed_manga_id)?),
+		chapter: None,
+	})
 }
---
+++ src/rust/en.mangairo/src/parser.rs
@@ -0,0 +1,82 @@
+use aidoku::{
+	error::Result, prelude::*, std::html::Node, std::String, std::Vec, Chapter, Filter, FilterType,
+	Manga, MangaContentRating, MangaStatus, MangaViewer, Page,
+};
+
+pub const BASE_URL: &str = "https://w.mangairo.com";
+pub const USER_AGENT: &str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36";
+
+pub fn parse_recents(html: Node, result: &mut Vec<Manga>) {
+	todo!()
+}
+
+pub fn parse_search(html: Node, result: &mut Vec<Manga>) {
+	todo!()
+}
+
+pub fn parse_manga(obj: Node, id: String) -> Result<Manga> {
+	todo!()
+}
+
+pub fn get_chapter_list(obj: Node) -> Result<Vec<Chapter>> {
+	todo!()
+}
+
+pub fn parse_manga_listing(url: String) -> Result<MangaPageResult> {
+	todo!()
+}
+
+pub fn get_page_list(obj: Node) -> Result<Vec<Page>> {
+	todo!()
+}
+
+pub fn get_filtered_url(filters: Vec<Filter>, page: i32, url: &mut String) {
+	todo!()
+}
+
+pub fn parse_incoming_url(url: String) -> String {
+	todo!()
+}
+
+// HELPER FUNCTIONS
+
+pub fn i32_to_string(mut integer: i32) -> String {
+	if integer == 0 {
+		return String::from("0");
+	}
+	let mut string = String::with_capacity(11);
+	let pos = if integer < 0 {
+		string.insert(0, '-');
+		1
+	} else {
+		0
+	};
+	while integer != 0 {
+		let mut digit = integer % 10;
+		if pos == 1 {
+			digit *= -1;
+		}
+		string.insert(pos, char::from_u32((digit as u32) + ('0' as u32)).unwrap());
+		integer /= 10;
+	}
+	string
+}
+
+pub fn urlencode(string: String) -> String {
+	let mut result: Vec<u8> = Vec::with_capacity(string.len() * 3);
+	let hex = "0123456789abcdef".as_bytes();
+	let bytes = string.as_bytes();
+
+	for byte in bytes {
+		let curr = *byte;
+		if curr.is_ascii_lowercase() || curr.is_ascii_uppercase() || curr.is_ascii_digit() {
+			result.push(curr);
+		} else {
+			result.push(b'%');
+			result.push(hex[curr as usize >> 4]);
+			result.push(hex[curr as usize & 15]);
+		}
+	}
+
+	String::from_utf8(result).unwrap_or_default()
+}
