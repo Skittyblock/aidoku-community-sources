b4c1969302495d562551ede7fac2ac9bcc37241c
DthRazak <vovamilchan@gmail.com>
2023-10-15 14:04:06 +0200

    Revert "Fix 1stkissmanga get_chapter_list (#480)"
    This reverts commit 38e9680e9f5f0bb174448b49b1020b7c8cb554be.

--- src/rust/madara/sources/firstkissmanga/src/lib.rs
+++ src/rust/madara/sources/firstkissmanga/src/lib.rs
@@ -1,19 +1,14 @@
 #![no_std]
 use aidoku::{
-	error::Result, prelude::*, std::net::Request, std::net::HttpMethod, std::String, std::Vec,
-	Chapter, DeepLink, Filter, Listing, Manga, MangaPageResult, Page,
-	helpers::substring::Substring
+	error::Result, prelude::*, std::net::Request, std::String, std::Vec, Chapter, DeepLink, Filter,
+	Listing, Manga, MangaPageResult, Page,
 };
 
 use madara_template::template;
 
-extern crate base64;
-use base64::{Engine as _, engine::general_purpose};
-
 fn get_data() -> template::MadaraSiteData {
 	let data: template::MadaraSiteData = template::MadaraSiteData {
 		base_url: String::from("https://1st-kissmanga.net"),
-		get_manga_id: get_int_manga_id,
 		alt_ajax: true,
 		..Default::default()
 	};
@@ -53,22 +48,4 @@ pub fn handle_url(url: String) -> Result<DeepLink> {
 #[modify_image_request]
 fn modify_image_request(request: Request) {
 	template::modify_image_request(get_data().base_url, request)
-}
-
-fn get_int_manga_id(manga_id: String, base_url: String, path: String) -> String {
-	let url = base_url + "/" + path.as_str() + "/" + manga_id.as_str();
-	if let Ok(html) = Request::new(url.as_str(), HttpMethod::Get).html() {
-		// For this web page, the script is encoded in base64
-		let id_html_encoded = html.select("script#wp-manga-js-extra").attr("src").read();
-		let id_html_base64 = id_html_encoded
-			.substring_after("data:text/javascript;base64,").unwrap_or_default();
-		let id_html_decoded = general_purpose::STANDARD.decode(id_html_base64).unwrap();
-		let id_html = String::from_utf8(id_html_decoded).unwrap_or_default();
-
-		let id = &id_html[id_html.find("manga_id").expect("Could not find manga_id") + 11
-			..id_html.find("\"}").expect("Could not find end of manga_id")];
-		String::from(id)
-	} else {
-		String::new()
-	}
 }
