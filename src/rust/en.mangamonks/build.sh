845e39ddf050b8d2d891915eb1adc8b8ac39472f
Deepak Patil <dspatil11599@gmail.com>
2024-03-19 02:36:28 +0530

    fix: aidoku serve from build

--- src/rust/en.mangamonks/build.sh
+++ src/rust/en.mangamonks/build.sh
@@ -4,4 +4,3 @@ cp res/* target/wasm32-unknown-unknown/release/Payload
 cp target/wasm32-unknown-unknown/release/*.wasm target/wasm32-unknown-unknown/release/Payload/main.wasm
 cd target/wasm32-unknown-unknown/release ; zip -r package.aix Payload
 mv package.aix ../../../package.aix
-aidoku serve -p 9001 ../../../package.aix 
