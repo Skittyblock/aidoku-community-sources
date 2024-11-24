0908e3f47f129a2c18b7148f4339c1c5e0f82e26
Aeon <30298077+Aeonss@users.noreply.github.com>
2024-11-24 13:58:53 -0500

    Fixed reaperscans (#808)
    * Fixed reaperscans
    
    * Fix order of filters
    
    * Change order by from filter type sort to select
    
    * Fix legacy cover when viewing in manga details
    
    * .
    
    * Addressed comments

--- src/rust/en.reaperscans/src/lib.rs
+++ src/rust/en.reaperscans/src/lib.rs
@@ -3,23 +3,18 @@ use aidoku::{
 	error::Result,
 	prelude::*,
 	std::{net::Request, String, Vec},
-	Chapter, DeepLink, Filter, Listing, Manga, MangaPageResult, Page,
+	Chapter, Filter, Listing, Manga, MangaPageResult, Page,
 };
 
-mod helper;
 mod parser;
-mod request_helper;
 
+const USER_AGENT: &str = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) GSA/300.0.598994205 Mobile/15E148 Safari/604";
 const BASE_URL: &str = "https://reaperscans.com";
+const BASE_API_URL: &str = "https://api.reaperscans.com";
 
 #[get_manga_list]
 fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
-	let (query, search) = helper::check_for_search(filters);
-	if search {
-		parser::parse_search(String::from(BASE_URL), query)
-	} else {
-		parser::parse_manga_list(String::from(BASE_URL), page)
-	}
+	parser::parse_manga_list(String::from(BASE_URL), filters, page)
 }
 
 #[get_manga_listing]
@@ -29,7 +24,7 @@ fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
 
 #[get_manga_details]
 fn get_manga_details(manga_id: String) -> Result<Manga> {
-	parser::parse_manga_details(String::from(BASE_URL), manga_id)
+	parser::parse_manga_details(&String::from(BASE_URL), manga_id)
 }
 
 #[get_chapter_list]
@@ -44,32 +39,8 @@ fn get_page_list(manga_id: String, chapter_id: String) -> Result<Vec<Page>> {
 
 #[modify_image_request]
 fn modify_image_request(request: Request) {
-	parser::modify_image_request(String::from(BASE_URL), request)
-}
-
-#[handle_url]
-fn handle_url(url: String) -> Result<DeepLink> {
-	let manga_id = helper::get_manga_id(url.clone());
-	let chapter_id = helper::get_chapter_id(url);
-
-	let manga = {
-		if let Ok(manga) = parser::parse_manga_details(String::from(BASE_URL), manga_id) {
-			Some(manga)
-		} else {
-			None
-		}
-	};
-
-	let chapter = {
-		if !chapter_id.is_empty() {
-			Some(Chapter {
-				id: chapter_id,
-				..Default::default()
-			})
-		} else {
-			None
-		}
-	};
-
-	Ok(DeepLink { manga, chapter })
+	parser::modify_image_request(
+		String::from(BASE_URL),
+		request.header("User-Agent", USER_AGENT),
+	)
 }
--- src/rust/en.reaperscans/src/parser.rs
+++ src/rust/en.reaperscans/src/parser.rs
@@ -1,96 +1,71 @@
 use aidoku::{
-	error::Result,
-	helpers::uri::encode_uri_component,
-	prelude::format,
-	std::html::Node,
-	std::net::{HttpMethod, Request},
-	std::{String, Vec},
-	Chapter, Listing, Manga, MangaContentRating, MangaPageResult, MangaStatus, MangaViewer, Page,
+	error::Result, prelude::format, std::net::HttpMethod, std::net::Request, std::String, std::Vec,
+	Chapter, Filter, FilterType, Listing, Manga, MangaPageResult, MangaStatus, MangaViewer, Page,
 };
+
+use crate::BASE_API_URL;
 
 extern crate alloc;
 use alloc::string::ToString;
 
-use crate::helper::*;
-use crate::request_helper::*;
+pub fn parse_manga_list(
+	base_url: String,
+	filters: Vec<Filter>,
+	page: i32,
+) -> Result<MangaPageResult> {
+	let mut search_query = String::new();
+	let mut genres = String::new();
+	let mut status = String::new();
+	let mut order_by = String::new();
 
-pub fn parse_manga_list(base_url: String, page: i32) -> Result<MangaPageResult> {
-	let url = format!("{}/comics?page={}", base_url, page);
-
-	let html = Request::new(url, HttpMethod::Get)
-		.header("User-Agent", USER_AGENT)
-		.html()
-		.expect(
-		"ReaperScans: Could not display All listing. Check the website and your internet connection on https://reaperscans.com",
-		);
-
-	let mut mangas: Vec<Manga> = Vec::new();
-
-	for manga in html.select("main div[wire:id] div > li > div").array() {
-		let manga_node = manga
-			.as_node()
-			.expect("Reaperscans: Failed to parse a manga node. Title could not be displayed");
-		let id = get_manga_id(manga_node.select("a").attr("href").read());
-		let cover = manga_node.select("img").attr("src").read();
-		let title = String::from(manga_node.select("a:not(:has(img))").text().read().trim());
-		let url = manga_node.select("a").attr("href").read();
-
-		mangas.push(Manga {
-			id,
-			cover,
-			title,
-			url,
-			viewer: MangaViewer::Scroll,
-			..Default::default()
-		});
+	for filter in filters {
+		match filter.kind {
+			FilterType::Title => {
+				search_query = filter.value.as_string()?.read();
+			}
+			FilterType::Genre => {
+				if let Ok(filter_id) = filter.object.get("id").as_string() {
+					match filter.value.as_int().unwrap_or(-1) {
+						1 => {
+							genres.push_str(filter_id.read().as_str());
+							genres.push(',');
+						}
+						_ => continue,
+					}
+				}
+			}
+			FilterType::Select => match filter.name.as_str() {
+				"Status" => match filter.value.as_int().unwrap_or(-1) {
+					0 => status.push_str("All"),
+					1 => status.push_str("Ongoing"),
+					2 => status.push_str("Hiatus"),
+					3 => status.push_str("Dropped"),
+					4 => status.push_str("Completed"),
+					_ => status.push_str("All"),
+				},
+				"Order By" => match filter.value.as_int().unwrap_or(-1) {
+					0 => order_by.push_str("updated_at"),
+					1 => order_by.push_str("total_views"),
+					2 => order_by.push_str("title"),
+					3 => order_by.push_str("created_at"),
+					_ => order_by.push_str("updated_at"),
+				},
+				_ => continue,
+			},
+			_ => continue,
+		}
 	}
 
-	let pagination = html.select("main div[wire:id] div nav span").text().read();
-	let has_more = pagination.contains("Next");
-
-	Ok(MangaPageResult {
-		manga: mangas,
-		has_more,
-	})
-}
-
-pub fn parse_search(base_url: String, query: String) -> Result<MangaPageResult> {
-	let html: Node = create_search_request_object(String::from(&base_url), query)
-		.expect("Reaperscans: Search POST request was unsuccessful");
-
-	let mut mangas: Vec<Manga> = Vec::new();
-	for i in html.select("ul li").array() {
-		let item = i
-			.as_node()
-			.expect("Reaperscans: Failed to parse a manga node. Title could not be displayed");
-		// Search displays both Comics and Novels in that order.
-		if &item.text().read() == "Novels" {
-			break;
-		}
-		let id = item
-			.select("a")
-			.attr("href")
-			.read()
-			.replace(&base_url, "")
-			.replace("/comics/", "");
-		if id.is_empty() {
-			continue;
-		}
-		let cover = item.select("a img").attr("src").read();
-		let title = item.select("a img").attr("alt").read();
-		mangas.push(Manga {
-			id,
-			cover,
-			title,
-			viewer: MangaViewer::Scroll,
-			..Default::default()
-		});
+	if !genres.is_empty() {
+		genres.pop();
 	}
 
-	Ok(MangaPageResult {
-		manga: mangas,
-		has_more: false,
-	})
+	let url = format!("{}/query?page={}&perPage=20&series_type=Comic&query_string={}&order=desc&orderBy={}&adult=true&status={}&tags_ids=[{}]", BASE_API_URL, page, search_query, order_by, status, genres);
+	let json = Request::new(url, HttpMethod::Get);
+	let manga = parse_manga(&base_url, json)?;
+	let has_more = !manga.is_empty();
+
+	Ok(MangaPageResult { manga, has_more })
 }
 
 pub fn parse_manga_listing(
@@ -98,229 +73,111 @@ pub fn parse_manga_listing(
 	_listing: Listing,
 	page: i32,
 ) -> Result<MangaPageResult> {
-	// The only alternate listing that reaper has is latest
-	let url = format!("{}/latest/comics?page={}", base_url, page);
+	let url = format!("{}/query?page={}&perPage=20&series_type=Comic&query_string=&order=desc&orderBy=&adult=true&status=&tags_ids=[]", BASE_API_URL, page);
 
-	let html = Request::new(url, HttpMethod::Get)
-		.header("User-Agent", USER_AGENT)
-		.html()
-		.expect("ReaperScans: Could not display a listing. Check the website and your internet connection on https://reaperscans.com");
+	let json = Request::new(url, HttpMethod::Get);
+	let manga = parse_manga(&base_url, json)?;
+	let has_more = !manga.is_empty();
 
-	let mut mangas: Vec<Manga> = Vec::new();
-
-	for manga in html
-		.select("main div[wire:id] > div > div:not(:has(nav)) > div")
-		.array()
-	{
-		let manga_node = manga
-			.as_node()
-			.expect("Reaperscans: Failed to parse a manga node. Title could not be displayed");
-		let id = get_manga_id(manga_node.select("a").attr("href").read());
-		let cover = manga_node.select("img").attr("src").read();
-		let title = String::from(manga_node.select("p > a").text().read().trim());
-		let url = manga_node.select("a").attr("href").read();
-
-		mangas.push(Manga {
-			id,
-			cover,
-			title,
-			url,
-			viewer: MangaViewer::Scroll,
-			..Default::default()
-		});
-	}
-
-	let pagination = html.select("main div[wire:id] div nav span").text().read();
-	let has_more = pagination.contains("Next");
-
-	Ok(MangaPageResult {
-		manga: mangas,
-		has_more,
-	})
+	Ok(MangaPageResult { manga, has_more })
 }
 
-pub fn parse_manga_details(base_url: String, manga_id: String) -> Result<Manga> {
-	let url = get_manga_url(manga_id.clone(), base_url);
+pub fn parse_manga_details(base_url: &String, manga_id: String) -> Result<Manga> {
+	let url = format!("{}/series/{}", BASE_API_URL, manga_id);
+	let data = Request::new(url, HttpMethod::Get).json()?.as_object()?;
 
-	let html = Request::new(&url, HttpMethod::Get)
-		.header("User-Agent", USER_AGENT)
-		.html()
-		.expect("Reaperscans: Failed to access the comic page. Try accessing the website on a browser and check your internet connection");
-
-	let main_div = html.select("main > div:not(:has(script))");
-
-	let cover = main_div.select("div[tabindex] img[alt]").attr("src").read();
-
-	let title = String::from(
-		main_div
-			.select("div[tabindex] div:not(:has(dl)) h1")
-			.text()
-			.read()
-			.trim(),
-	);
-
-	let description = text_with_newlines(main_div.select("section div[tabindex] > div > p"));
-
-	let mut status = MangaStatus::Unknown;
-	let mut age_rating = String::new();
-	let mut nsfw = MangaContentRating::Safe;
-	let mut language = String::new();
-
-	for node in main_div
-		.select("section div[tabindex] > div dl > div")
-		.array()
-	{
-		let info = node
-			.as_node()
-			.expect("Reaperscans: Could not parse the comic's details")
-			.text()
-			.read();
-
-		if info.contains("Source Language") {
-			language = String::from(info.replace("Source Language", "").trim());
-		} else if info.contains("Age Rating") {
-			age_rating = info.replace("Age Rating", "");
-		} else if info.contains("Release Status") {
-			let release_status = info.replace("Release Status", "");
-
-			if release_status.contains("Ongoing") {
-				status = MangaStatus::Ongoing;
-			} else if release_status.contains("Complete") {
-				status = MangaStatus::Completed;
-			} else if release_status.contains("Dropped") {
-				status = MangaStatus::Cancelled;
-			} else if release_status.contains("On hold") {
-				status = MangaStatus::Hiatus;
-			}
-		}
+	let mut cover = data.get("thumbnail").as_string()?.read();
+	if !cover.contains("https") {
+		cover = format!("https://reaperscans.com/_next/image?url=https%3A%2F%2Fmedia.reaperscans.com%2Ffile%2F4SRBHm%2F{}&w=384&q=75", cover)
 	}
 
-	if age_rating.contains("18+") {
-		nsfw = MangaContentRating::Nsfw;
-	} else if age_rating.contains("16+") {
-		nsfw = MangaContentRating::Suggestive;
-	}
+	let title = data.get("title").as_string()?.read();
+	let description = data.get("description").as_string()?.read();
+	let id = data.get("series_slug").as_string()?.read();
+	let url = format!("{}/series/{}", base_url, id);
+	let status = data.get("status").as_string()?.read();
 
-	let viewer = match language.as_str() {
-		"Japanese" => MangaViewer::Rtl,
-		_ => MangaViewer::Scroll,
+	let manga_status = match status.as_str() {
+		"New" => MangaStatus::Unknown,
+		"Ongoing" => MangaStatus::Ongoing,
+		"Completed" => MangaStatus::Completed,
+		"Cancelled" => MangaStatus::Cancelled,
+		"Dropped" => MangaStatus::Cancelled,
+		"Hiatus" => MangaStatus::Hiatus,
+		_ => MangaStatus::Unknown,
 	};
 
+	let mut categories: Vec<String> = Vec::new();
+	let tags = data.get("tags").as_array()?;
+	for tag in tags {
+		let tag = tag.as_object()?;
+		categories.push(tag.get("name").as_string()?.read());
+	}
+
 	Ok(Manga {
-		id: manga_id,
+		id,
 		cover,
 		title,
 		description,
 		url,
-		status,
-		nsfw,
-		viewer,
+		categories,
+		status: manga_status,
+		viewer: MangaViewer::Scroll,
 		..Default::default()
 	})
 }
 
 pub fn parse_chapter_list(base_url: String, manga_id: String) -> Result<Vec<Chapter>> {
-	let mut chapters: Vec<Chapter> = Vec::new();
-	let url = get_manga_url(manga_id, base_url.clone());
-	let request = Request::new(url, HttpMethod::Get).header("User-Agent", USER_AGENT);
+	let url = format!("{}/series/{}", BASE_API_URL, manga_id);
+	let data = Request::new(url, HttpMethod::Get).json()?.as_object()?;
+	let manga_id = data.get("id").as_int()?.to_string();
 
-	let initial_request = request
-		.html()
-		.expect("Reaperscans: Could not retreive the chapter list html");
+	let url = format!(
+		"{}/chapter/query?page=1&perPage=30&series_id={}",
+		BASE_API_URL, manga_id
+	);
+	let data = Request::new(url, HttpMethod::Get).json()?.as_object()?;
+	let mut page = data.get("meta").as_object()?.get("first_page").as_int()?;
+	let last_page = data.get("meta").as_object()?.get("last_page").as_int()?;
 
-	parse_chapter_list_helper(initial_request.clone(), &mut chapters);
+	let mut all_chapters: Vec<Chapter> = Vec::new();
 
-	let mut page = 2;
-
-	let max_retries = 5;
-	let mut retries = 0;
-
-	while retries < max_retries {
-		// If let here gracefully handles the case where cloudflare blocks the request.
-		// If the request is blocked, then the loop will continue and try again, with
-		// a maximum of 5 retries.
-		// The cloudflare popup should appear and once validated, the next request will
-		// be successful.
-		if let Ok(response_html) = create_chapter_request_object(
-			initial_request.clone(),
-			base_url.clone(),
-			page.to_string(),
-		) {
-			parse_chapter_list_helper(response_html.clone(), &mut chapters);
-
-			// checks if next-page button exists.
-			if response_html
-				.select("button[wire:click*=nextPage]")
-				.html()
-				.read()
-				.is_empty()
-			{
-				break;
-			}
-
-			retries = 0;
-			page += 1;
-		} else {
-			retries += 1;
-			continue;
-		}
-	}
-	Ok(chapters)
-}
-
-pub fn parse_chapter_list_helper(html: Node, chapters: &mut Vec<Chapter>) {
-	for chapter in html.select("div[wire:id] div > ul > li").array() {
-		let chapter_node = chapter.as_node().expect(
-			"Reaperscans: Failed to parse a chapter node. The chapter list request was unsuccessful",
+	while page <= last_page {
+		let url = format!(
+			"{}/chapter/query?page={}&perPage=30&series_id={}",
+			BASE_API_URL, page, manga_id
 		);
+		let data = Request::new(url, HttpMethod::Get).json()?.as_object()?;
 
-		let mut title = String::new();
-		let chapter_number;
+		let chapters = data.get("data").as_array()?;
 
-		let parsed_title = chapter_node.select("div div p[class]").text().read();
+		for chapter in chapters {
+			let chapter = chapter.as_object()?;
 
-		// Only some titles have a chapter titles if they do
-		// they are in the format of "Chapter 1 - Chapter Title" else
-		// it's just "Chapter 1"
-		if parsed_title.contains('-') {
-			title = String::from(
-				parsed_title
-					.split('-')
-					.last()
-					.expect("Reaperscans: Failed to get chapter title")
-					.trim(),
-			);
-			chapter_number = parsed_title
-				.replace("Chapter", "")
-				.split('-')
-				.next()
-				.expect("Reaperscans: Failed to get chapter number")
-				.trim()
-				.parse::<f32>()
-				.expect("Reaperscans: Failed to parse chapter number");
-		} else {
-			chapter_number = parsed_title
-				.replace("Chapter", "")
-				.trim()
-				.parse::<f32>()
-				.expect("Reaperscans: Failed to parse chapter number");
+			let id = chapter.get("chapter_slug").as_string()?.read();
+
+			let index = id.split('-').collect::<Vec<&str>>();
+			let index = String::from(index[1]).parse::<f32>().unwrap_or(-1.0);
+
+			let url = format!("{}/series/{}/{}", base_url, manga_id, id);
+
+			let date_updated = chapter
+				.get("created_at")
+				.as_date("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Some("en_US"), None)
+				.unwrap_or(-1.0);
+
+			all_chapters.push(Chapter {
+				id,
+				chapter: index,
+				date_updated,
+				url,
+				..Default::default()
+			});
 		}
-
-		let chapter_id = get_chapter_id(chapter_node.select("a").attr("href").read());
-		let chapter_url = chapter_node.select("a").attr("href").read();
-
-		let date_updated = get_date(chapter_node.select("div div p:not([class])").text().read());
-
-		chapters.push(Chapter {
-			id: chapter_id,
-			title,
-			chapter: chapter_number,
-			date_updated,
-			url: chapter_url,
-			lang: String::from("en"),
-			..Default::default()
-		});
+		page += 1;
 	}
+
+	Ok(all_chapters)
 }
 
 pub fn parse_page_list(
@@ -328,68 +185,69 @@ pub fn parse_page_list(
 	manga_id: String,
 	chapter_id: String,
 ) -> Result<Vec<Page>> {
-	let url = get_chapter_url(chapter_id, manga_id, base_url);
+	let url = format!("{}/series/{}/{}", base_url, manga_id, chapter_id);
 
-	let html = Request::new(url, HttpMethod::Get)
-		.header("User-Agent", USER_AGENT)
-		.html()
-		.expect("Reaperscans: Failed to display the chapter. Check your internet connection");
+	let obj = Request::new(url, HttpMethod::Get).html()?;
 
-	let mut pages: Vec<Page> = Vec::new();
-	// Stores the indices of the pages so duplicates can be incremented.
-	let mut indices: Vec<i32> = Vec::new();
+	let mut page_list: Vec<Page> = Vec::new();
 
-	// Select all images that are not children of a noscript tag.
-	for page in html.select("main div > img:not(noscript *)").array() {
-		let page_node = page
-			.as_node()
-			.expect("Reaperscans: Failed to parse a chapter node. The chapter pages request was unsuccessful");
-
-		let url = {
-			let src = page_node.attr("src").read();
-			let data_cfsrc = page_node.attr("data-cfsrc").read();
-
-			if !src.is_empty() {
-				src
-			} else if !data_cfsrc.is_empty() {
-				data_cfsrc
-			} else {
-				panic!("Reaperscans: Failed to get the image url");
-			}
-		};
-
-		let image_name = url
-			.split('/')
-			.last()
-			.expect("Reaperscans: Could not get the image url for the chapter")
-			.split('.')
+	for (i, page) in obj.select(".container > .flex > img").array().enumerate() {
+		let obj = page.as_node().expect("node array");
+		let srcset = obj
+			.attr("srcset")
+			.read()
+			.split(',')
 			.next()
-			.expect("Reaperscans: Could not get the image url for the chapter");
+			.unwrap_or("")
+			.split(' ')
+			.next()
+			.unwrap_or("")
+			.to_string();
+		let mut url = format!("{}{}", base_url, srcset);
 
-		let mut index = *extract_f32_from_string(String::from(image_name))
-			.first()
-			.expect("Reaperscans: Failed to get index") as i32;
-
-		// ReaperScans sometimes has duplicate image numbers, so this will increment the
-		// index for pages that have the same index.
-		if indices.contains(&index) {
-			index += 1;
+		if srcset.is_empty() {
+			url = obj.attr("src").read();
 		}
-		indices.push(index);
 
-		let encoded_image_name = encode_uri_component(String::from(image_name));
-		let encoded_url = url.replace(image_name, &encoded_image_name);
-
-		pages.push(Page {
-			index,
-			url: encoded_url,
+		page_list.push(Page {
+			index: i as i32,
+			url,
 			..Default::default()
 		});
 	}
 
-	Ok(pages)
+	Ok(page_list)
 }
 
 pub fn modify_image_request(base_url: String, request: Request) {
 	request.header("Referer", &base_url);
+}
+
+fn parse_manga(base_url: &String, json: Request) -> Result<Vec<Manga>> {
+	let data = json.json()?.as_object()?.get("data").as_array()?;
+	let mut mangas: Vec<Manga> = Vec::new();
+
+	for manga in data {
+		let manga = manga.as_object()?;
+		let title = manga.get("title").as_string()?.read();
+		let mut cover = manga.get("thumbnail").as_string()?.read();
+
+		if !cover.contains("https") {
+			cover = format!("https://reaperscans.com/_next/image?url=https%3A%2F%2Fmedia.reaperscans.com%2Ffile%2F4SRBHm%2F{}&w=384&q=75", cover)
+		}
+
+		let id = manga.get("series_slug").as_string()?.read();
+
+		let url = format!("{}/series/{}", base_url, id);
+
+		mangas.push(Manga {
+			id,
+			cover,
+			title,
+			url,
+			..Default::default()
+		});
+	}
+
+	Ok(mangas)
 }
