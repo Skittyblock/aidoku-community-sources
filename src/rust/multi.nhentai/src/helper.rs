3155fd2a332e8440656dbe21e4f4da4e39c164cb
kantan <31490942+kkantan@users.noreply.github.com>
2025-01-29 21:28:16 -0600

    fix(nhentai): image loading (#865)

--- src/rust/multi.nhentai/src/helper.rs
+++ src/rust/multi.nhentai/src/helper.rs
@@ -1,53 +1,7 @@
-use aidoku::{error::Result, std::ArrayRef, std::String, std::ValueRef, std::Vec};
-
-pub fn urlencode(string: String) -> String {
-	let mut result: Vec<u8> = Vec::with_capacity(string.len() * 3);
-	let hex = "0123456789abcdef".as_bytes();
-	let bytes = string.as_bytes();
-
-	for byte in bytes {
-		let curr = *byte;
-		if curr.is_ascii_lowercase() || curr.is_ascii_uppercase() || curr.is_ascii_digit() {
-			result.push(curr);
-		} else {
-			result.push(b'%');
-			result.push(hex[curr as usize >> 4]);
-			result.push(hex[curr as usize & 15]);
-		}
-	}
-
-	String::from_utf8(result).unwrap_or_default()
-}
-
-pub fn i32_to_string(mut integer: i32) -> String {
-	if integer == 0 {
-		return String::from("0");
-	}
-	let mut string = String::with_capacity(11);
-	let pos = if integer < 0 {
-		string.insert(0, '-');
-		1
-	} else {
-		0
-	};
-	while integer != 0 {
-		let mut digit = integer % 10;
-		if pos == 1 {
-			digit *= -1;
-		}
-		string.insert(pos, char::from_u32((digit as u32) + ('0' as u32)).unwrap());
-		integer /= 10;
-	}
-	string
-}
-
-pub fn get_cover_url(id: String, filetype: String) -> String {
-	let mut string = String::from("https://t.nhentai.net/galleries/");
-	string.push_str(&id);
-	string.push_str("/cover.");
-	string.push_str(&filetype);
-	string
-}
+use aidoku::{
+	error::Result,
+	std::{ArrayRef, String, Vec},
+};
 
 pub fn get_details_url(id: String) -> String {
 	let mut string = String::from("https://nhentai.net/api/gallery/");
@@ -56,13 +10,13 @@ pub fn get_details_url(id: String) -> String {
 }
 
 pub fn get_file_type(filetype: String) -> String {
-	return match filetype.as_str() {
+	match filetype.as_str() {
 		"j" => String::from("jpg"),
 		"p" => String::from("png"),
 		"w" => String::from("webp"),
 		"g" => String::from("gif"),
 		_ => String::new(),
-	};
+	}
 }
 
 pub fn get_tag_names_by_type(tags: ArrayRef, tag_type: &str) -> Result<Vec<String>> {
@@ -79,15 +33,24 @@ pub fn get_tag_names_by_type(tags: ArrayRef, tag_type: &str) -> Result<Vec<Strin
 	Ok(names)
 }
 
-pub fn get_id(value: ValueRef) -> Result<String> {
-	let id = value.as_int().unwrap_or(0) as i32;
-	Ok(if id != 0 {
-		i32_to_string(id)
-	} else {
-		value.as_string()?.read()
-	})
-}
-
 pub fn is_number(s: &str) -> bool {
 	s.chars().all(|c| c.is_numeric())
+}
+
+pub fn find_media_server(big_string: &str) -> Option<&str> {
+	let begin_pattern = r#"window._n_app"#; // finds the script block that contains the media server
+	let start_pattern = r#"media_server: "#;
+	let end_pattern = r#","#;
+
+	if let Some(begin_index) = big_string.find(begin_pattern) {
+		if let Some(start_index) = big_string[begin_index..].find(start_pattern) {
+			let start_index = begin_index + start_index + start_pattern.len();
+			if let Some(end_index) = big_string[start_index..].find(end_pattern) {
+				let end_index = start_index + end_index;
+				return Some(&big_string[start_index..end_index]);
+			}
+		}
+	}
+
+	None
 }
--- src/rust/multi.nhentai/src/lib.rs
+++ src/rust/multi.nhentai/src/lib.rs
@@ -1,59 +1,55 @@
 #![no_std]
 use aidoku::{
-	error::Result, prelude::*, std::defaults::defaults_get, std::net::HttpMethod,
-	std::net::Request, std::ObjectRef, std::String, std::Vec, Chapter, DeepLink, Filter,
-	FilterType, Listing, Manga, MangaContentRating, MangaPageResult, MangaStatus, MangaViewer,
-	Page,
+	error::Result, helpers::uri::encode_uri_component, prelude::*, std::defaults::defaults_get,
+	std::net::HttpMethod, std::net::Request, std::ObjectRef, std::String, std::Vec, Chapter,
+	DeepLink, Filter, FilterType, Listing, Manga, MangaContentRating, MangaPageResult, MangaStatus,
+	MangaViewer, Page,
 };
 extern crate alloc;
 use alloc::{string::ToString, vec};
 
 mod helper;
 
+const BASE_URL: &str = "https://nhentai.net";
 const USER_AGENT: &str = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) GSA/300.0.598994205 Mobile/15E148 Safari/604";
 
 #[get_manga_list]
 fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
-	let mut query: String;
-	let mut is_sauce_code = false;
-	let mut sauce_code = String::from("0");
+	let mut sauce_code = Option::<String>::None;
 
-	if let Ok(languages) = defaults_get("languages")?.as_array() {
-		query = String::new();
+	let mut query = if let Ok(languages) = defaults_get("languages")?.as_array() {
 		if languages.is_empty() {
-			query.push_str("language:english")
+			String::from("language:english")
 		} else {
-			query.push_str(
-				&languages
-					.into_iter()
-					.filter_map(|lang| lang.as_string().ok())
-					.map(|lang| match lang.read().as_str() {
-						"en" => "language:english",
-						"ja" => "language:japanese",
-						"zh" => "language:chinese",
-						"All" => "\"\"",
-						_ => "",
-					})
-					.collect::<Vec<&str>>()
-					.join(" "),
-			);
+			languages
+				.into_iter()
+				.filter_map(|lang| lang.as_string().ok())
+				.map(|lang| match lang.read().as_str() {
+					"en" => "language:english",
+					"ja" => "language:japanese",
+					"zh" => "language:chinese",
+					"All" => "\"\"",
+					_ => "",
+				})
+				.collect::<Vec<&str>>()
+				.join(" ")
 		}
 	} else {
-		query = String::from("language:english")
-	}
+		String::from("language:english")
+	};
 
-	let mut sort = String::from("date");
+	let mut sort = "date";
 
 	for filter in filters {
 		match filter.kind {
 			FilterType::Title => {
 				let title = filter.value.as_string()?.read();
-				if helper::is_number(title.as_str()) {
-					is_sauce_code = true;
-					sauce_code = title.clone();
+				if helper::is_number(&title) {
+					sauce_code = Some(title.clone());
+				} else {
+					query.push(' ');
+					query.push_str(&title);
 				}
-				query.push(' ');
-				query.push_str(&title);
 			}
 			FilterType::Genre => {
 				match filter.value.as_int().unwrap_or(-1) {
@@ -77,110 +73,59 @@ fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
 					3 => "popular",
 					_ => continue,
 				};
-				sort = String::from(option)
+				sort = option;
 			}
 			_ => continue,
 		}
 	}
 
-	let mut manga_arr: Vec<Manga> = Vec::new();
-	let mut total: i32 = 1;
+	// if the user searches a code, just return the manga for that id
+	if let Some(sauce_code) = sauce_code {
+		let manga = get_manga_details(sauce_code)?;
 
-	if is_sauce_code {
-		let url = helper::get_details_url(sauce_code);
-		let request = Request::new(url, HttpMethod::Get).header("User-Agent", USER_AGENT);
-		let json = request.json()?.as_object()?;
+		return Ok(MangaPageResult {
+			manga: vec![manga],
+			has_more: false,
+		});
+	}
 
-		let id = helper::get_id(json.get("id"))?;
+	let url = format!(
+		"{BASE_URL}/search/?q={}&page={page}&sort={sort}",
+		encode_uri_component(query),
+	);
 
-		let media_id = json.get("media_id").as_string()?.read();
-		let cover_type = json
-			.get("images")
-			.as_object()?
-			.get("cover")
-			.as_object()?
-			.get("t")
-			.as_string()?
-			.read();
-		let cover = helper::get_cover_url(media_id, helper::get_file_type(cover_type));
+	let html = Request::new(&url, HttpMethod::Get)
+		.header("User-Agent", USER_AGENT)
+		.html()?;
 
-		let title = json
-			.get("title")
-			.as_object()?
-			.get("pretty")
-			.as_string()?
-			.read();
+	let mut manga: Vec<Manga> = Vec::new();
 
-		manga_arr.push(Manga {
+	for node in html
+		.select("#content .container:not(.index-popular) .gallery")
+		.array()
+	{
+		let node = node.as_node().expect("node array");
+
+		let rel_link = node.select("a").first().attr("href").read(); // /g/id/
+
+		let id = rel_link[3..rel_link.len() - 1].to_string();
+
+		let cover = node.select("img").first().attr("data-src").read();
+
+		let title = node.select(".caption").first().text().read();
+
+		manga.push(Manga {
 			id,
 			cover,
 			title,
-			author: String::new(),
-			artist: String::new(),
-			description: String::new(),
-			url: String::new(),
-			categories: Vec::new(),
-			status: MangaStatus::Completed,
 			nsfw: MangaContentRating::Nsfw,
-			viewer: MangaViewer::Rtl,
+			..Default::default()
 		});
-	} else {
-		let mut url = String::from("https://nhentai.net/api/galleries/search?query=");
-		url.push_str(&helper::urlencode(query));
-		url.push_str("&page=");
-		url.push_str(&helper::urlencode(page.to_string()));
-		url.push_str("&sort=");
-		url.push_str(&helper::urlencode(sort));
-
-		let request = Request::new(&url, HttpMethod::Get).header("User-Agent", USER_AGENT);
-		let json = request.json()?.as_object()?;
-
-		let data = json.get("result").as_array()?;
-
-		for manga in data {
-			let manga_obj = manga.as_object()?;
-
-			let id = helper::get_id(manga_obj.get("id"))?;
-
-			let media_id = manga_obj.get("media_id").as_string()?.read();
-			let cover_type = manga_obj
-				.get("images")
-				.as_object()?
-				.get("cover")
-				.as_object()?
-				.get("t")
-				.as_string()?
-				.read();
-			let cover = helper::get_cover_url(media_id, helper::get_file_type(cover_type));
-
-			let title = manga_obj
-				.get("title")
-				.as_object()?
-				.get("pretty")
-				.as_string()?
-				.read();
-
-			manga_arr.push(Manga {
-				id,
-				cover,
-				title,
-				author: String::new(),
-				artist: String::new(),
-				description: String::new(),
-				url: String::new(),
-				categories: Vec::new(),
-				status: MangaStatus::Completed,
-				nsfw: MangaContentRating::Nsfw,
-				viewer: MangaViewer::Rtl,
-			});
-		}
-		total = json.get("num_pages").as_int().unwrap_or(0) as i32;
 	}
 
-	Ok(MangaPageResult {
-		manga: manga_arr,
-		has_more: page < total,
-	})
+	let has_more = !manga.is_empty();
+
+	Ok(MangaPageResult { manga, has_more })
 }
 
 #[get_manga_listing]
@@ -196,7 +141,7 @@ fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
 			"Popular - Today" => 1i32.into(),
 			"Popular - This Week" => 2i32.into(),
 			"Popular - All Time" => 3i32.into(),
-			&_ => 0i32.into(),
+			_ => 0i32.into(),
 		},
 	);
 
@@ -212,41 +157,44 @@ fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
 
 #[get_manga_details]
 fn get_manga_details(id: String) -> Result<Manga> {
-	let request = Request::new(helper::get_details_url(id).as_str(), HttpMethod::Get)
-		.header("User-Agent", USER_AGENT);
-	let json = request.json()?.as_object()?;
+	let url = format!("{BASE_URL}/g/{id}/");
+	let html = Request::new(&url, HttpMethod::Get)
+		.header("User-Agent", USER_AGENT)
+		.html()?;
 
-	let id = helper::get_id(json.get("id"))?;
+	let cover = html.select("#cover img").first().attr("data-src").read();
 
-	let media_id = json.get("media_id").as_string()?.read();
-	let cover_type = json
-		.get("images")
-		.as_object()?
-		.get("cover")
-		.as_object()?
-		.get("t")
-		.as_string()?
-		.read();
-	let cover = helper::get_cover_url(media_id, helper::get_file_type(cover_type));
+	let title = html.select("#info h1.title").first().text().read();
 
-	let title = json
-		.get("title")
-		.as_object()?
-		.get("english")
-		.as_string()?
-		.read();
-
-	let tags = json.get("tags").as_array()?;
-	let author = String::from(helper::get_tag_names_by_type(tags.clone(), "artist")?[0].as_str());
+	let author = html
+		.select("#tags div:contains(Artists:)")
+		.array()
+		.map(|node| {
+			node.as_node()
+				.expect("node array")
+				.select(".name")
+				.first()
+				.text()
+				.read()
+		})
+		.collect::<Vec<_>>()
+		.join(", ");
 	let artist = author.clone();
 
-	let mut url = String::from("https://nhentai.net/g/");
-	url.push_str(&id);
+	let description = format!("#{id}");
 
-	let mut description = String::from("#");
-	description.push_str(&id);
-
-	let categories = helper::get_tag_names_by_type(tags, "tag")?;
+	let categories = html
+		.select("#tags div:contains(Tags:) a")
+		.array()
+		.map(|node| {
+			node.as_node()
+				.expect("node array")
+				.select(".name")
+				.first()
+				.text()
+				.read()
+		})
+		.collect();
 
 	Ok(Manga {
 		id,
@@ -265,44 +213,46 @@ fn get_manga_details(id: String) -> Result<Manga> {
 
 #[get_chapter_list]
 fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
-	let request = Request::new(
-		helper::get_details_url(id.clone()).as_str(),
-		HttpMethod::Get,
-	)
-	.header("User-Agent", USER_AGENT);
-	let json = request.json()?.as_object()?;
+	let json = Request::new(helper::get_details_url(id.clone()), HttpMethod::Get)
+		.header("User-Agent", USER_AGENT)
+		.json()?
+		.as_object()?;
 
-	let mut url = String::from("https://nhentai.net/g/");
-	url.push_str(&id);
+	let url = format!("{BASE_URL}/g/{id}/");
 
 	let date_updated = json.get("upload_date").as_float().unwrap_or(0.0);
 
 	let language = &helper::get_tag_names_by_type(json.get("tags").as_array()?, "artist")?[0];
 
 	let lang = match language.as_str() {
-		"english" => String::from("en"),
-		"japanese" => String::from("jp"),
-		"chinese" => String::from("zh"),
-		_ => String::new(),
-	};
+		"english" => "en",
+		"japanese" => "jp",
+		"chinese" => "zh",
+		_ => "",
+	}
+	.to_string();
 
 	Ok(vec![Chapter {
 		id,
-		title: String::from("Chapter 1"),
-		volume: -1.0,
 		chapter: 1.0,
 		date_updated,
-		scanlator: String::new(),
 		url,
 		lang,
+		..Default::default()
 	}])
 }
 
 #[get_page_list]
 fn get_page_list(_: String, id: String) -> Result<Vec<Page>> {
-	let request = Request::new(helper::get_details_url(id).as_str(), HttpMethod::Get)
-		.header("User-Agent", USER_AGENT);
-	let json = request.json()?.as_object()?;
+	let html = Request::new(format!("{BASE_URL}/g/{id}/"), HttpMethod::Get)
+		.header("User-Agent", USER_AGENT)
+		.string()?;
+	let media_server = helper::find_media_server(&html).unwrap_or("");
+
+	let json = Request::new(helper::get_details_url(id).as_str(), HttpMethod::Get)
+		.header("User-Agent", USER_AGENT)
+		.json()?
+		.as_object()?;
 
 	let images = json.get("images").as_object()?;
 	let pages_arr = images.get("pages").as_array()?;
@@ -315,18 +265,15 @@ fn get_page_list(_: String, id: String) -> Result<Vec<Page>> {
 		let media_id = json.get("media_id").as_string()?.read();
 		let file_type = helper::get_file_type(page_obj.get("t").as_string()?.read());
 
-		let mut url = String::from("https://i.nhentai.net/galleries/");
-		url.push_str(&media_id);
-		url.push('/');
-		url.push_str(&(i + 1).to_string());
-		url.push('.');
-		url.push_str(&file_type);
+		let url = format!(
+			"https://i{media_server}.nhentai.net/galleries/{media_id}/{}.{file_type}",
+			i + 1
+		);
 
 		pages.push(Page {
-			index: i.try_into().unwrap_or(-1),
+			index: i as i32,
 			url,
-			base64: String::new(),
-			text: String::new(),
+			..Default::default()
 		});
 	}
 
@@ -335,15 +282,15 @@ fn get_page_list(_: String, id: String) -> Result<Vec<Page>> {
 
 #[handle_url]
 pub fn handle_url(url: String) -> Result<DeepLink> {
-	let url = &url[20..]; // remove "https://nhentai.net/"
+	let url = &url[BASE_URL.len()..]; // remove "https://nhentai.net"
 
-	if let Some(id) = url.strip_prefix("g/") {
+	if let Some(id) = url.strip_prefix("/g/") {
 		let end = match id.find('/') {
 			Some(end) => end,
 			None => id.len(),
 		};
-		let manga_id = &id[..end];
-		let manga = get_manga_details(String::from(manga_id))?;
+		let manga_id = id[..end].to_string();
+		let manga = get_manga_details(manga_id)?;
 
 		return Ok(DeepLink {
 			manga: Some(manga),
