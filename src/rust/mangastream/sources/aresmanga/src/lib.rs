f5d4cbb85dba08732e953d566b273835cc8b1680
kantan <31490942+kkantan@users.noreply.github.com>
2025-01-31 11:58:24 -0600

    fix(aresmanga): parse base64 page data (#888)
    * fix(aresmanga): parse base64 page data
    
    * chore(aresmanga): fix lint

--- src/rust/mangastream/sources/aresmanga/src/lib.rs
+++ src/rust/mangastream/sources/aresmanga/src/lib.rs
@@ -3,16 +3,19 @@ use aidoku::{
 	error::Result, prelude::*, std::net::Request, std::String, std::Vec, Chapter, DeepLink, Filter,
 	Listing, Manga, MangaPageResult, Page,
 };
+use base64::prelude::*;
+use mangastream_template::template::{MangaStreamSource, USER_AGENT};
 
-use mangastream_template::template::MangaStreamSource;
+const BASE_URL: &str = "https://fl-ares.com";
 
 fn get_instance() -> MangaStreamSource {
 	MangaStreamSource {
+		base_url: BASE_URL.into(),
 		listing: ["الرائج", "آخر", "جَديد"],
-		base_url: String::from("https://en-aresmanga.com"),
 		last_page_text: "التالي",
 		traverse_pathname: "series",
 		manga_details_author: ".imptdt:contains(المؤلف) i",
+		manga_details_status: ".imptdt:contains(Status) i, .imptdt:contains(الحالة) i",
 		chapter_date_format: "MMMM d, yyyy",
 		locale: "ar_EH",
 		..Default::default()
@@ -39,9 +42,55 @@ fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
 	get_instance().parse_chapter_list(id)
 }
 
+// parse page json data in base64
 #[get_page_list]
 fn get_page_list(_manga_id: String, id: String) -> Result<Vec<Page>> {
-	get_instance().parse_page_list(id)
+	let url = format!("{BASE_URL}/{id}");
+
+	let mut pages: Vec<Page> = Vec::new();
+	let html = Request::get(url)
+		.header("Referer", BASE_URL)
+		.header("User-Agent", USER_AGENT)
+		.html()?;
+
+	let raw_data = html
+		.select("script[src^=data:text/javascript;base64,dHNfcmVhZGVyLnJ1bih7]")
+		.first()
+		.attr("src")
+		.read();
+
+	let start_pattern = "base64,";
+
+	let Some(start_idx) = raw_data.find(start_pattern) else {
+		return get_instance().parse_page_list(id);
+	};
+
+	let base64_data = &raw_data[start_idx + start_pattern.len()..];
+
+	let Some(json) = BASE64_STANDARD
+		.decode(base64_data)
+		.ok()
+		.and_then(|s| String::from_utf8(s).ok())
+	else {
+		return Ok(Vec::new());
+	};
+
+	let trimmed_json =
+		&json[json.find(r#":[{"s"#).unwrap_or(0) + 2..json.rfind("}],").unwrap_or(2) + 1];
+
+	let parsed_json = aidoku::std::json::parse(trimmed_json.as_bytes())?.as_object()?;
+	let images = parsed_json.get("images").as_array()?;
+
+	for (index, page) in images.enumerate() {
+		let url = mangastream_template::helper::urlencode(page.as_string()?.read());
+		pages.push(Page {
+			index: index as i32,
+			url,
+			..Default::default()
+		});
+	}
+
+	Ok(pages)
 }
 
 #[modify_image_request]
