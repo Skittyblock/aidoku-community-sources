3b691e9538638a7b0cc0511690a6193dbf3daeb2
kantan <31490942+kkantan@users.noreply.github.com>
2025-03-02 16:03:11 -0600

    fix(mangabox): update template and urls (#915)

--- src/rust/mangabox/template/src/helper.rs
+++ src/rust/mangabox/template/src/helper.rs
@@ -1,47 +1,11 @@
-use aidoku::{std::ArrayRef, std::String, std::Vec, MangaStatus};
+use aidoku::{
+	error::Result,
+	prelude::*,
+	std::{ArrayRef, String, Vec},
+	Filter, FilterType, MangaStatus,
+};
 
-pub fn urlencode(string: String) -> String {
-	let mut result: Vec<u8> = Vec::with_capacity(string.len() * 3);
-	let hex = "0123456789abcdef".as_bytes();
-	let bytes = string.as_bytes();
-
-	for byte in bytes {
-		let curr = *byte;
-		if curr.is_ascii_lowercase() || curr.is_ascii_uppercase() || curr.is_ascii_digit() {
-			result.push(curr);
-		} else {
-			result.push(b'%');
-			result.push(hex[curr as usize >> 4]);
-			result.push(hex[curr as usize & 15]);
-		}
-	}
-
-	String::from_utf8(result).unwrap_or_default()
-}
-
-pub fn i32_to_string(mut integer: i32) -> String {
-	if integer == 0 {
-		return String::from("0");
-	}
-	let mut string = String::with_capacity(11);
-	let pos = if integer < 0 {
-		string.insert(0, '-');
-		1
-	} else {
-		0
-	};
-	while integer != 0 {
-		let mut digit = integer % 10;
-		if pos == 1 {
-			digit *= -1;
-		}
-		string.insert(pos, char::from_u32((digit as u32) + ('0' as u32)).unwrap());
-		integer /= 10;
-	}
-	string
-}
-
-pub fn join_string_array(array: ArrayRef, delimeter: String) -> String {
+pub fn join_string_array(array: ArrayRef, delimeter: &str) -> String {
 	let mut string = String::new();
 	for (i, item) in array.enumerate() {
 		let node = match item.as_node() {
@@ -49,24 +13,25 @@ pub fn join_string_array(array: ArrayRef, delimeter: String) -> String {
 			Err(_) => continue,
 		};
 		if i != 0 {
-			string.push_str(&delimeter);
+			string.push_str(delimeter);
 		}
 		string.push_str(node.text().read().as_str());
 	}
 	string
 }
 
-pub fn status_from_string(status: String) -> MangaStatus {
-	if status == "Ongoing" {
-		MangaStatus::Ongoing
-	} else if status == "Completed" {
-		MangaStatus::Completed
-	} else if status == "Hiatus" {
-		MangaStatus::Hiatus
-	} else if status == "Cancelled" {
-		MangaStatus::Cancelled
-	} else {
-		MangaStatus::Unknown
+pub fn status_from_string(string: String) -> MangaStatus {
+	let string = string.trim();
+	let status = string
+		.rsplit_once(' ')
+		.map(|(_, last)| last)
+		.unwrap_or(string);
+	match status {
+		"Ongoing" => MangaStatus::Ongoing,
+		"Completed" => MangaStatus::Completed,
+		"Hiatus" => MangaStatus::Hiatus,
+		"Cancelled" => MangaStatus::Cancelled,
+		_ => MangaStatus::Unknown,
 	}
 }
 
@@ -74,63 +39,157 @@ pub fn is_numeric_char(c: char) -> bool {
 	c.is_ascii_digit() || c == '.'
 }
 
-pub fn get_chapter_number(id: String) -> f32 {
-	let mut number_string = String::new();
-	let mut i = id.len() - 1;
-	for c in id.chars().rev() {
-		if !is_numeric_char(c) {
-			number_string = String::from(&id[i + 1..]);
-			break;
+// parses ".../chapter-x" where x can be e.g. "3" or "0-2" for decimals
+pub fn get_chapter_number(s: &str) -> f32 {
+	const PREFIX: &str = "chapter-";
+
+	if let Some(pos) = s.find(PREFIX) {
+		let number_str = &s[pos + PREFIX.len()..];
+
+		let number_str = number_str.replace('-', ".");
+
+		if let Ok(number) = number_str.parse::<f32>() {
+			return number;
 		}
-		i -= 1;
 	}
-	if number_string.is_empty() {
-		return 0.0;
+
+	0.0
+}
+
+pub fn strip_default_chapter_title(s: String) -> String {
+	const PREFIX: &str = "Chapter ";
+
+	if let Some(pos) = s[PREFIX.len()..].find(' ') {
+		return s[PREFIX.len() + pos..].into();
 	}
-	number_string.parse::<f32>().unwrap_or(0.0)
+
+	String::default()
 }
 
 pub fn get_search_url(
-	base_url: String,
-	query: String,
+	base_url: &str,
 	page: i32,
-	include: Vec<String>,
-	exclude: Vec<String>,
-	sort: String,
-) -> String {
-	let mut url = String::new();
-	url.push_str(&base_url);
-	url.push_str("/advanced_search/?page=");
-	url.push_str(&i32_to_string(page));
-	if !query.is_empty() {
-		url.push_str("&keyw=");
-		url.push_str(&stupidencode(query));
-	}
-	if !include.is_empty() {
-		url.push_str("&g_i=");
-		for (i, tag) in include.iter().enumerate() {
-			if i == 0 {
+	filters: Vec<Filter>,
+	supports_advanced_search: bool,
+	search_path: Option<&str>,
+	genres: Option<&[&str]>,
+) -> Result<String> {
+	if supports_advanced_search {
+		// isn't used anymore (?)
+		let mut include: Vec<String> = Vec::new();
+		let mut exclude: Vec<String> = Vec::new();
+		let mut sort = String::new();
+		let mut query = String::new();
+		for filter in filters {
+			match filter.kind {
+				FilterType::Title => {
+					query = filter.value.as_string()?.read();
+				}
+				FilterType::Genre => match filter.value.as_int().unwrap_or(-1) {
+					0 => exclude.push(get_tag_id(&filter.name)),
+					1 => include.push(get_tag_id(&filter.name)),
+					_ => continue,
+				},
+				FilterType::Sort => {
+					let value = match filter.value.as_object() {
+						Ok(value) => value,
+						Err(_) => continue,
+					};
+					let index = value.get("index").as_int().unwrap_or(0);
+					let option = match index {
+						0 => "",
+						1 => "newest",
+						2 => "topview",
+						3 => "az",
+						_ => continue,
+					};
+					sort = String::from(option)
+				}
+				_ => continue,
+			}
+		}
+
+		let mut url = format!("{base_url}/advanced_search/?page={page}");
+		if !query.is_empty() {
+			url.push_str("&keyw=");
+			url.push_str(&stupidencode(query));
+		}
+		if !include.is_empty() {
+			url.push_str("&g_i=");
+			for (i, tag) in include.iter().enumerate() {
+				if i == 0 {
+					url.push('_');
+				}
+				url.push_str(tag.as_str());
 				url.push('_');
 			}
-			url.push_str(tag.as_str());
-			url.push('_');
 		}
-	}
-	if !exclude.is_empty() {
-		url.push_str("&g_e=");
-		for (i, tag) in exclude.iter().enumerate() {
-			if i == 0 {
+		if !exclude.is_empty() {
+			url.push_str("&g_e=");
+			for (i, tag) in exclude.iter().enumerate() {
+				if i == 0 {
+					url.push('_');
+				}
+				url.push_str(tag.as_str());
 				url.push('_');
 			}
-			url.push_str(tag.as_str());
-			url.push('_');
 		}
+		if !sort.is_empty() {
+			url.push_str("&orby=");
+			url.push_str(sort.as_str());
+		}
+
+		Ok(url)
+	} else {
+		let mut title = None;
+		let mut url_filter = 0;
+		let mut genre = String::new();
+
+		for filter in filters {
+			match filter.kind {
+				FilterType::Title => {
+					title = Some(filter.value.as_string()?.read());
+					break;
+				}
+				FilterType::Sort => {
+					let index = filter.value.as_object()?.get("index").as_int().unwrap_or(0);
+					url_filter += match index {
+						0 => 1,
+						1 => 2,
+						2 => 3,
+						_ => continue,
+					};
+				}
+				FilterType::Select => match filter.name.as_str() {
+					"Status" => match filter.value.as_int().unwrap_or(0) {
+						1 => url_filter += 1, // completed
+						2 => url_filter += 2, // ongoing
+						_ => continue,
+					},
+					"Genre" => {
+						if let Some(genres) = genres {
+							let index = filter.value.as_int().unwrap_or(0) as usize;
+							if index < genres.len() {
+								genre = genres[index].into();
+							}
+						}
+					}
+					_ => continue,
+				},
+				_ => continue,
+			}
+		}
+
+		Ok(if let Some(title) = title {
+			format!(
+				"{base_url}{}/{}?page={page}",
+				search_path.unwrap_or("/search"),
+				stupidencode(title)
+			)
+		} else {
+			format!("{base_url}/genre/{genre}?filter={url_filter}&page={page}")
+		})
 	}
-	if !sort.is_empty() {
-		url.push_str("&orby=");
-		url.push_str(sort.as_str());
-	}
-	url
 }
 
 pub fn string_replace(string: String, search: String, replace: String) -> String {
@@ -152,8 +211,8 @@ pub fn string_replace(string: String, search: String, replace: String) -> String
 	result
 }
 
-pub fn get_tag_id(tag: String) -> String {
-	let id = match tag.as_str() {
+pub fn get_tag_id(tag: &str) -> String {
+	let id = match tag {
 		"Action" => 2,
 		"Adult" => 3,
 		"Adventure" => 4,
@@ -196,14 +255,14 @@ pub fn get_tag_id(tag: String) -> String {
 		"Yuri" => 42,
 		_ => -1,
 	};
-	i32_to_string(id)
+	format!("{id}")
 }
 
 pub fn stupidencode(string: String) -> String {
 	let mut result = String::new();
 	for c in string.chars() {
 		if c.is_alphanumeric() {
-			result.push(c);
+			result.push(c.to_ascii_lowercase());
 		} else if c == ' ' {
 			result.push('_');
 		}
