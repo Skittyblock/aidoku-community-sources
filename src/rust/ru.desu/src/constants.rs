c7954b91e6331712db761578fbbe71c73bfe8cf9
MemesPwr <47759467+MemesPwr@users.noreply.github.com>
2025-04-02 20:04:59 +0300

    Initial commit for russian Desu.store source

---
+++ src/rust/ru.desu/src/constants.rs
@@ -0,0 +1,9 @@
+use const_format::formatcp;
+
+pub const BASE_URL: &str = "https://desu.store";
+pub const API_URL: &str = "manga/api";
+pub const BASE_API_URL: &str = formatcp!("{BASE_URL}/{API_URL}");
+pub const RATE_LIMIT: i32 = 3;
+pub const RATE_LIMIT_PERIOD: i32 = 1;
+pub const USER_AGENT: &str = "Aidoku";
+pub const PAGE_LIMIT: i32 = 20;
