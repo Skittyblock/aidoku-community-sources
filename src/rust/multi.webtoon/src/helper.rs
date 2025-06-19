9c917a395c8cc1d502bb8955ebdb28ef7e0875d8
kantan <31490942+kkantan@users.noreply.github.com>
2025-06-19 16:22:38 -0500

    fix(webtoon): update site parsing (#994)

--- src/rust/multi.webtoon/src/helper.rs
+++ src/rust/multi.webtoon/src/helper.rs
@@ -2,30 +2,30 @@ use aidoku::{
 	helpers::uri::encode_uri_component,
 	prelude::format,
 	std::defaults::defaults_get,
-	std::net::{HttpMethod, Request},
+	std::net::Request,
 	std::{String, Vec},
 	Filter, FilterType,
 };
 
+pub fn get_base_url_no_lang(mobile: bool) -> String {
+	if mobile {
+		String::from("https://m.webtoons.com")
+	} else {
+		String::from("https://www.webtoons.com")
+	}
+}
+
 /// Returns the correct url for the selected language and mobile/desktop
 pub fn get_base_url(mobile: bool) -> String {
-	let desktop_base_url = "https://www.webtoons.com";
-	let mobile_base_url = "https://m.webtoons.com";
+	let base = get_base_url_no_lang(mobile);
 	let lang = get_lang_code().unwrap_or(String::from("en"));
 
-	if mobile {
-		format!("{}/{}", mobile_base_url, lang)
-	} else {
-		format!("{}/{}", desktop_base_url, lang)
-	}
+	format!("{}/{}", base, lang)
 }
 
 /// Returns the currently selected language
 pub fn get_lang_code() -> Option<String> {
-	if let Ok(languages) = defaults_get("languages")
-		.expect("Could not read language")
-		.as_array()
-	{
+	if let Ok(languages) = defaults_get("languages").ok()?.as_array() {
 		if let Ok(language) = languages.get(0).as_string() {
 			return Some(language.read());
 		}
@@ -46,7 +46,7 @@ pub fn get_user_agent(mobile: bool) -> String {
 pub fn request(url: &String, mobile: bool) -> Request {
 	let locale = get_lang_code().unwrap_or(String::from("en"));
 	let age_gate_pass = true;
-	let need_gdpr = true;
+	let need_gdpr = false;
 	let need_ccpa = true;
 	let need_coppa = false;
 	let user_agent = get_user_agent(mobile);
@@ -56,14 +56,14 @@ pub fn request(url: &String, mobile: bool) -> Request {
 		locale, age_gate_pass, need_gdpr, need_ccpa, need_coppa
 	);
 
-	Request::new(url, HttpMethod::Get)
-		.header("Referer", &get_base_url(false))
+	Request::get(url)
+		.header("Referer", &get_base_url_no_lang(mobile))
 		.header("Cookie", &cookie_string)
 		.header("User-Agent", &user_agent)
 }
 
 /// Returns the ID of a manga from a URL.
-pub fn get_manga_id(url: String) -> String {
+pub fn get_manga_id(url: &str) -> String {
 	// Example Url: https://www.webtoons.com/en/action/ultra-alternate-character/list?title_no=3581
 	// Example Url: https://www.webtoons.com/episodeList?titleNo=3581
 	// parse "3581" from the url
@@ -105,7 +105,7 @@ pub fn get_manga_id(url: String) -> String {
 }
 
 /// Returns the ID of a chapter from a URL.
-pub fn get_chapter_id(url: String) -> String {
+pub fn get_chapter_id(url: &str) -> String {
 	// Example Url: https://www.webtoons.com/en/action/ultra-alternate-character/ep-1-healer-servant/viewer?title_no=3581&episode_no=1
 	// Example Url: https://www.webtoons.com/viewer?titleNo=3581&episodeNo=1
 	// parse "1" from the url
@@ -147,7 +147,7 @@ pub fn get_chapter_id(url: String) -> String {
 }
 
 /// Returns full URL of a manga from a manga ID.
-pub fn get_manga_url(manga_id: String, base_url: String) -> String {
+pub fn get_manga_url(manga_id: &str, base_url: String) -> String {
 	// Example manga id: 3581
 	// return "https://www.webtoons.com/episodeList?titleNo=3581"
 
@@ -159,14 +159,18 @@ pub fn get_manga_url(manga_id: String, base_url: String) -> String {
 	let mut split_url = base_url.split('/').collect::<Vec<&str>>();
 	split_url.pop();
 
-	let base_url = split_url.join("/");
+	let base_url_no_lang = split_url.join("/");
 
 	// Removing the "canvas" from the id if it's a canvas title
-	if manga_id.contains("-canvas") {
-		let manga_id = manga_id.replace("-canvas", "");
-		format!("{}/challenge/episodeList?titleNo={}", base_url, manga_id)
+	if let Some(canvas_id) = manga_id.strip_suffix("-canvas") {
+		format!(
+			"{}/challenge/episodeList?titleNo={}",
+			base_url_no_lang, canvas_id
+		)
+	// } else if base_url.contains("m.webtoons.com") {
+	// 	format!("{}/x/x/list?title_no={}", base_url, manga_id)
 	} else {
-		format!("{}/episodeList?titleNo={}", base_url, manga_id)
+		format!("{}/episodeList?titleNo={}", base_url_no_lang, manga_id)
 	}
 }
 
