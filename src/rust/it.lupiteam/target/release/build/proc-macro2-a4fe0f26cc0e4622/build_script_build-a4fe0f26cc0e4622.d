f2e91e64438c7928820aa898fe07c4cc403e296a
roguebankrupt <74917081+roguebankrupt@users.noreply.github.com>
2025-07-21 20:10:03 +0200

    Add files via upload

---
+++ src/rust/it.lupiteam/target/release/build/proc-macro2-a4fe0f26cc0e4622/build_script_build-a4fe0f26cc0e4622.d
@@ -0,0 +1,5 @@
+C:\Users\rober\Desktop\Github\aidoku\it.lupiteam\target\release\build\proc-macro2-a4fe0f26cc0e4622\build_script_build-a4fe0f26cc0e4622.d: C:\Users\rober\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.86\build.rs
+
+C:\Users\rober\Desktop\Github\aidoku\it.lupiteam\target\release\build\proc-macro2-a4fe0f26cc0e4622\build_script_build-a4fe0f26cc0e4622.exe: C:\Users\rober\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.86\build.rs
+
+C:\Users\rober\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.86\build.rs:
---
+++ src/rust/it.lupiteam/target/release/build/syn-d829f07b4b081348/build_script_build-d829f07b4b081348.d
@@ -0,0 +1,5 @@
+C:\Users\rober\Desktop\Github\aidoku\it.lupiteam\target\release\build\syn-d829f07b4b081348\build_script_build-d829f07b4b081348.d: C:\Users\rober\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs
+
+C:\Users\rober\Desktop\Github\aidoku\it.lupiteam\target\release\build\syn-d829f07b4b081348\build_script_build-d829f07b4b081348.exe: C:\Users\rober\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs
+
+C:\Users\rober\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs:
