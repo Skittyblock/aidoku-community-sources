41f52d0d93bca103388eeba81505368198d66bbf
SolsticeLeaf <me@sleaf.dev>
2025-09-22 13:10:31 +0300

    Updated urls (Mangaonelove & readmanga)

--- src/rust/ru.readmanga/src/constants.rs
+++ src/rust/ru.readmanga/src/constants.rs
@@ -1,6 +1,6 @@
 use const_format::formatcp;
 
-pub const BASE_URL: &str = "https://t.readmanga.io";
+pub const BASE_URL: &str = "https://3.readmanga.ru";
 pub const BASE_SEARCH_URL: &str = formatcp!("{}/{}", BASE_URL, "search/advancedResults?");
 
 pub const SEARCH_OFFSET_STEP: i32 = 50;
