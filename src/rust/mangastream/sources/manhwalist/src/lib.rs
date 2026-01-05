9935012b6a21af2a213781530406fcba4edeaced
kantan <31490942+kkantan@users.noreply.github.com>
2026-01-05 15:21:30 -0600

    fix(id.manhwalist): update url

--- src/rust/mangastream/sources/manhwalist/src/lib.rs
+++ src/rust/mangastream/sources/manhwalist/src/lib.rs
@@ -8,7 +8,7 @@ use mangastream_template::template::MangaStreamSource;
 
 fn get_instance() -> MangaStreamSource {
 	MangaStreamSource {
-		base_url: String::from("https://manhwalist.xyz"),
+		base_url: String::from("https://manhwalist02.site"),
 		chapter_date_format: "MMMM d, yyyy",
 		locale: "en",
 		alt_pages: true,
